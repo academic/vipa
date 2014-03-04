490843fb96568afa1c0323747825916bd80e72a9
hasantayyar <tayyar.besik@gmail.com>
2014-03-04 12:04:56 +0200

    no tests yet

--- src/Ojstr/JournalBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Ojstr/JournalBundle/Tests/Controller/DefaultControllerTest.php
@@ -4,12 +4,6 @@ namespace Ojstr\JournalBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
-{
-    public function testIndex()
-    {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/');
-        $this->assertTrue($crawler->filter('html:contains("Hello nobody")')->count() > 0);
-    }
+class DefaultControllerTest extends WebTestCase {
+    
 }
