6fc8c04d29ded8bd1e933249e59f1de320b10216
hasantayyar <tayyar.besik@gmail.com>
2014-03-06 14:21:54 +0200

    user role manage page

---
+++ src/Ojstr/UserBundle/Form/RoleType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Ojstr\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class RoleType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('role')
+            ->add('users')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Ojstr\UserBundle\Entity\Role'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ojstr_userbundle_role';
+    }
+}
