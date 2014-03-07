c49aabb21214d949a7879d6cef8c19400d31c2b5
Hüseyin Mert <huseyinin@gmail.com>
2014-03-07 10:50:15 +0200

    Keyword, Language, Subject Entities

---
+++ src/Ojstr/JournalBundle/Entity/Keyword.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Ojstr\JournalBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Keyword
+ */
+class Keyword
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $keyword;
+
+    /**
+     * @var integer
+     */
+    private $langId;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set keyword
+     *
+     * @param string $keyword
+     * @return Keyword
+     */
+    public function setKeyword($keyword)
+    {
+        $this->keyword = $keyword;
+
+        return $this;
+    }
+
+    /**
+     * Get keyword
+     *
+     * @return string 
+     */
+    public function getKeyword()
+    {
+        return $this->keyword;
+    }
+
+    /**
+     * Set langId
+     *
+     * @param integer $langId
+     * @return Keyword
+     */
+    public function setLangId($langId)
+    {
+        $this->langId = $langId;
+
+        return $this;
+    }
+
+    /**
+     * Get langId
+     *
+     * @return integer 
+     */
+    public function getLangId()
+    {
+        return $this->langId;
+    }
+}
---
+++ src/Ojstr/JournalBundle/Entity/Subject.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Ojstr\JournalBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Subject
+ */
+class Subject
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $subject;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set subject
+     *
+     * @param string $subject
+     * @return Subject
+     */
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    /**
+     * Get subject
+     *
+     * @return string 
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Subject
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
