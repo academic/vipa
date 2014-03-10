a6d3056f13fc2ac2dd87bba82a6120c84e8d7380
hasantayyar <tayyar.besik@gmail.com>
2014-03-10 15:59:52 +0200

    Base tests phpunit gets green
    Green is the colour of her kind
    Quickness of the eye deceives the mind

--- src/Ojstr/JournalBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Ojstr/JournalBundle/Tests/Controller/DefaultControllerTest.php
@@ -1,8 +1,15 @@
 <?php
 
 namespace Ojstr\JournalBundle\Tests\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase {
-    
+
+    public function testStatus() {
+        $client = static::createClient();
+        $client->request('GET', '/');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /");
+    }
+
 }
