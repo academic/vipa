55b4b64fc59e6ce64183b0cbf748029bd902a8c0
Hüseyin Mert <huseyinin@gmail.com>
2014-03-10 15:59:52 +0200

    Base tests phpunit gets green
    Green is the colour of her kind
    Quickness of the eye deceives the mind

--- src/Ojstr/JournalBundle/Tests/Controller/ContactControllerTest.php
+++ src/Ojstr/JournalBundle/Tests/Controller/ContactControllerTest.php
@@ -1,54 +1,15 @@
 <?php
 
 namespace Ojstr\JournalBundle\Tests\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class ContactControllerTest extends WebTestCase
-{
-    /*
-    public function testCompleteScenario()
-    {
-        // Create a new client to browse the application
+class ContactControllerTest extends WebTestCase {
+
+    public function testCompleteScenario() {
         $client = static::createClient();
-
-        // Create a new entry in the database
-        $crawler = $client->request('GET', '/contact/');
+        $client->request('GET', '/admin/contact/');
         $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /contact/");
-        $crawler = $client->click($crawler->selectLink('Create a new entry')->link());
-
-        // Fill in the form and submit it
-        $form = $crawler->selectButton('Create')->form(array(
-            'ojstr_journalbundle_contacttype[field_name]'  => 'Test',
-            // ... other fields to fill
-        ));
-
-        $client->submit($form);
-        $crawler = $client->followRedirect();
-
-        // Check data in the show view
-        $this->assertGreaterThan(0, $crawler->filter('td:contains("Test")')->count(), 'Missing element td:contains("Test")');
-
-        // Edit the entity
-        $crawler = $client->click($crawler->selectLink('Edit')->link());
-
-        $form = $crawler->selectButton('Edit')->form(array(
-            'ojstr_journalbundle_contacttype[field_name]'  => 'Foo',
-            // ... other fields to fill
-        ));
-
-        $client->submit($form);
-        $crawler = $client->followRedirect();
-
-        // Check the element contains an attribute with value equals "Foo"
-        $this->assertGreaterThan(0, $crawler->filter('[value="Foo"]')->count(), 'Missing element [value="Foo"]');
-
-        // Delete the entity
-        $client->submit($crawler->selectButton('Delete')->form());
-        $crawler = $client->followRedirect();
-
-        // Check the entity has been delete on the list
-        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
     }
 
-    */
 }
--- src/Ojstr/UserBundle/Tests/Controller/SecurityControllerTest.php
+++ src/Ojstr/UserBundle/Tests/Controller/SecurityControllerTest.php
@@ -1,9 +1,15 @@
 <?php
 
 namespace Ojstr\UserBundle\Tests\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
-{
-     
+class SecurityControllerTest extends WebTestCase {
+
+    public function testAuth() {
+        $client = static::createClient();
+        $client->request('GET', '/user/');
+        $this->assertEquals(302, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /user/");
+    }
+
 }
