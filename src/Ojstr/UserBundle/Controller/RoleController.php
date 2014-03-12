64797aab40ed6d5de16aa3b87a70ee7a78364383
Hüseyin Mert <huseyinin@gmail.com>
2014-03-12 16:29:23 +0200

    Role crud

---
+++ src/Ojstr/UserBundle/Controller/RoleController.php
@@ -0,0 +1,241 @@
+<?php
+
+namespace Ojstr\UserBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Ojstr\UserBundle\Entity\Role;
+use Ojstr\UserBundle\Entity\User;
+
+use Ojstr\UserBundle\Form\RoleType;
+
+/**
+ * Role controller.
+ *
+ * @Route("/admin/role")
+ */
+class RoleController extends Controller {
+
+    /**
+     * Lists all Role entities.
+     *
+     * @Route("/", name="admin_role")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction() {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('OjstrUserBundle:Role')->findAll();
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+
+    /**
+     * Creates a new Role entity.
+     *
+     * @Route("/", name="admin_role_create")
+     * @Method("POST")
+     * @Template("OjstrUserBundle:Role:new.html.twig")
+     */
+    public function createAction(Request $request) {
+        $entity = new Role();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('admin_role_show', array('id' => $entity->getId())));
+        }
+
+        return array(
+            'entity' => $entity,
+            'form' => $form->createView(),
+        );
+    }
+
+    /**
+     * Creates a form to create a Role entity.
+     *
+     * @param Role $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Role $entity) {
+        $form = $this->createForm(new RoleType(), $entity, array(
+            'action' => $this->generateUrl('admin_role_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Role entity.
+     *
+     * @Route("/new", name="admin_role_new")
+     * @Method("GET")
+     * @Template()
+     */
+    public function newAction() {
+        $entity = new Role();
+        $form = $this->createCreateForm($entity);
+
+        return array(
+            'entity' => $entity,
+            'form' => $form->createView(),
+        );
+    }
+
+    /**
+     * Finds and displays a Role entity.
+     *
+     * @Route("/{id}", name="admin_role_show")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction($id) {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('OjstrUserBundle:Role')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Role entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity' => $entity,
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to edit an existing Role entity.
+     *
+     * @Route("/{id}/edit", name="admin_role_edit")
+     * @Method("GET")
+     * @Template()
+     */
+    public function editAction($id) {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('OjstrUserBundle:Role')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Role entity.');
+        }
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity' => $entity,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Creates a form to edit a Role entity.
+     *
+     * @param Role $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createEditForm(Role $entity) {
+        $form = $this->createForm(new RoleType(), $entity, array(
+            'action' => $this->generateUrl('admin_role_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+        $form->add('submit', 'submit', array('attr'=>array('label ' => 'Update')));
+
+        return $form;
+    }
+
+    /**
+     * Edits an existing Role entity.
+     *
+     * @Route("/{id}", name="admin_role_update")
+     * @Method("PUT")
+     * @Template("OjstrUserBundle:Role:edit.html.twig")
+     */
+    public function updateAction(Request $request, $id) {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('OjstrUserBundle:Role')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Role entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('admin_role_edit', array('id' => $id)));
+        }
+
+        return array(
+            'entity' => $entity,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Deletes a Role entity.
+     *
+     * @Route("/{id}", name="admin_role_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, $id) {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('OjstrUserBundle:Role')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Role entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('admin_role'));
+    }
+
+    /**
+     * Creates a form to delete a Role entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id) {
+        return $this->createFormBuilder()
+                        ->setAction($this->generateUrl('admin_role_delete', array('id' => $id)))
+                        ->setMethod('DELETE')
+                        ->add('submit', 'submit', array('label' => 'Delete'))
+                        ->getForm()
+        ;
+    }
+
+}
--- src/Ojstr/UserBundle/Form/RoleType.php
+++ src/Ojstr/UserBundle/Form/RoleType.php
@@ -17,7 +17,6 @@ class RoleType extends AbstractType
         $builder
             ->add('name')
             ->add('role')
-            ->add('users')
         ;
     }
     
