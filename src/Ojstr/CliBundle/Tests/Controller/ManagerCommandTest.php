98f2b368851207de5dd35d4130b740fc8afd2495
hasantayyar <tayyar.besik@gmail.com>
2014-03-13 12:03:36 +0200

    Ojs cli bundle added

---
+++ src/Ojstr/CliBundle/Tests/Controller/ManagerCommandTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Ojstr\CliBundle\Tests\Command;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * @todo write tests
+ */
+class ManagerCommandTest extends WebTestCase {
+
+    public function testIndex() {
+
+        $this->assertTrue(TRUE, 1); // please forgive me
+    }
+
+}
