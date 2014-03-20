468528b7b97dfb3851e79cdcf3831b4b294d0b7e
hasantayyar <tayyar.besik@gmail.com>
2014-03-20 12:19:04 +0200

    User Journal Role mapping without ACL

---
+++ src/Ojstr/UserBundle/Entity/UserJournalRole.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace Ojstr\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * UserJournalRole
+ */
+class UserJournalRole {
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var integer
+     */
+    private $userId;
+
+    /**
+     * @var integer
+     */
+    private $journalId;
+
+    /**
+     * @var integer
+     */
+    private $roleId;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     * @return UserJournalRole
+     */
+    public function setUserId($userId) {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return integer 
+     */
+    public function getUserId() {
+        return $this->userId;
+    }
+
+    /**
+     * Set journalId
+     *
+     * @param integer $journalId
+     * @return UserJournalRole
+     */
+    public function setJournalId($journalId) {
+        $this->journalId = $journalId;
+
+        return $this;
+    }
+
+    /**
+     * Get journalId
+     *
+     * @return integer 
+     */
+    public function getJournalId() {
+        return $this->journalId;
+    }
+
+    /**
+     * Set roleId
+     *
+     * @param integer $roleId
+     * @return UserJournalRole
+     */
+    public function setRoleId($roleId) {
+        $this->roleId = $roleId;
+
+        return $this;
+    }
+
+    /**
+     * Get roleId
+     *
+     * @return integer 
+     */
+    public function getRoleId() {
+        return $this->roleId;
+    }
+
+    /**
+     * @var \Ojstr\UserBundle\Entity\User
+     */
+    private $user;
+
+    /**
+     * @var \Ojstr\JournalBundle\Entity\Journal
+     */
+    private $journal;
+
+    /**
+     * @var \Ojstr\UserBundle\Entity\Role
+     */
+    private $role;
+
+    /**
+     * Set user
+     *
+     * @param \Ojstr\UserBundle\Entity\User $user
+     * @return UserJournalRole
+     */
+    public function setUser(\Ojstr\UserBundle\Entity\User $user = null) {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \Ojstr\UserBundle\Entity\User 
+     */
+    public function getUser() {
+        return $this->user;
+    }
+
+    /**
+     * Set journal
+     *
+     * @param \Ojstr\UserBundle\Entity\Journal $journal
+     * @return UserJournalRole
+     */
+    public function setJournal(\Ojstr\UserBundle\Entity\Journal $journal = null) {
+        $this->journal = $journal;
+
+        return $this;
+    }
+
+    /**
+     * Get journal
+     *
+     * @return \Ojstr\UserBundle\Entity\Journal 
+     */
+    public function getJournal() {
+        return $this->journal;
+    }
+
+    /**
+     * Set role
+     *
+     * @param \Ojstr\UserBundle\Entity\Role $role
+     * @return UserJournalRole
+     */
+    public function setRole(\Ojstr\UserBundle\Entity\Role $role = null) {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    /**
+     * Get role
+     *
+     * @return \Ojstr\UserBundle\Entity\Role 
+     */
+    public function getRole() {
+        return $this->role;
+    }
+
+}
