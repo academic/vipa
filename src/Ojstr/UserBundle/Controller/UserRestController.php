6983ad08b7aba57b67845cc44f7cb8b8bcf0c578
hasantayyar <tayyar.besik@gmail.com>
2014-03-27 17:36:34 +0200

    user model modified

--- src/Ojstr/UserBundle/Controller/UserRestController.php
+++ src/Ojstr/UserBundle/Controller/UserRestController.php
@@ -2,6 +2,7 @@
 namespace Ojstr\UserBundle\Controller;
 
 use FOS\RestBundle\Controller\Annotations\View;
+use Ojstr\UserBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
@@ -14,5 +15,24 @@ class UserRestController extends Controller
             throw $this->createNotFoundException();
         }
         return $user;
+    }
+
+    public function deleteUserAction($user_id, $soft_delete = TRUE)
+    {
+        $em = $this->getDoctrine()->getManager();
+        /**
+         * @var User $user
+         */
+        $user = $this->getDoctrine()->getRepository('OjstrUserBundle:User')->findOneById($user_id);
+        if (!is_object($user)) {
+            throw $this->createNotFoundException();
+        }
+        if ($soft_delete) {
+            $user->setStatus(-1);
+        } else {
+            $em->remove($user);
+            $em->flush();
+        }
+        return array("user_id" => $user_id);
     }
 }
--- src/Ojstr/UserBundle/Entity/User.php
+++ src/Ojstr/UserBundle/Entity/User.php
@@ -8,7 +8,8 @@ use Doctrine\Common\Collections\ArrayCollection;
 /**
  * User
  */
-class User implements UserInterface, \Serializable {
+class User implements UserInterface, \Serializable
+{
 
     /**
      * @var integer
@@ -39,37 +40,46 @@ class User implements UserInterface, \Serializable {
     private $isActive;
 
     /**
-     * @var \Doctrine\Common\Collections\Collection     
+     * @var \Doctrine\Common\Collections\Collection
      */
     private $roles;
 
     /**
-     * @var \Doctrine\Common\Collections\Collection     
+     * @var \Doctrine\Common\Collections\Collection
      */
     private $journals;
 
-   
+    /**
+     * @var integer
+     *
+     */
+    private $status = 1;
 
-    public function __construct() {
+
+    public function __construct()
+    {
         $this->isActive = true;
         $this->roles = new ArrayCollection();
         $this->journals = new ArrayCollection();
     }
 
-    public function getRoles() {
+    public function getRoles()
+    {
         return $this->roles->toArray();
     }
 
-    public function getJournals() {
+    public function getJournals()
+    {
         return $this->journals->toArray();
     }
 
     /**
      * Get id
      *
-     * @return integer 
+     * @return integer
      */
-    public function getId() {
+    public function getId()
+    {
         return $this->id;
     }
 
@@ -79,19 +89,48 @@ class User implements UserInterface, \Serializable {
      * @param string $username
      * @return User
      */
-    public function setUsername($username) {
+    public function setUsername($username)
+    {
         $this->username = $username;
 
         return $this;
     }
 
+
+
     /**
      * Get username
      *
-     * @return string 
+     * @return string
      */
-    public function getUsername() {
+    public function getUsername()
+    {
         return $this->username;
+    }
+
+
+
+    /**
+     * Set status
+     *
+     * @param integer $status
+     * @return User
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return integer
+     */
+    public function getStatus()
+    {
+        return $this->status;
     }
 
     /**
@@ -100,7 +139,8 @@ class User implements UserInterface, \Serializable {
      * @param string $password
      * @return User
      */
-    public function setPassword($password) {
+    public function setPassword($password)
+    {
         $this->password = $password;
 
         return $this;
@@ -109,9 +149,10 @@ class User implements UserInterface, \Serializable {
     /**
      * Get password
      *
-     * @return string 
+     * @return string
      */
-    public function getPassword() {
+    public function getPassword()
+    {
         return $this->password;
     }
 
@@ -121,7 +162,8 @@ class User implements UserInterface, \Serializable {
      * @param string $email
      * @return User
      */
-    public function setEmail($email) {
+    public function setEmail($email)
+    {
         $this->email = $email;
 
         return $this;
@@ -130,9 +172,10 @@ class User implements UserInterface, \Serializable {
     /**
      * Get email
      *
-     * @return string 
+     * @return string
      */
-    public function getEmail() {
+    public function getEmail()
+    {
         return $this->email;
     }
 
@@ -142,7 +185,8 @@ class User implements UserInterface, \Serializable {
      * @param boolean $isActive
      * @return User
      */
-    public function setIsActive($isActive) {
+    public function setIsActive($isActive)
+    {
         $this->isActive = $isActive;
 
         return $this;
@@ -151,27 +195,31 @@ class User implements UserInterface, \Serializable {
     /**
      * Get isActive
      *
-     * @return boolean 
+     * @return boolean
      */
-    public function getIsActive() {
+    public function getIsActive()
+    {
         return $this->isActive;
     }
 
-    public function getSalt() {
+    public function getSalt()
+    {
         return null;
     }
 
     /**
      * @inheritDoc
      */
-    public function eraseCredentials() {
-        
+    public function eraseCredentials()
+    {
+
     }
 
     /**
      * @see \Serializable::serialize()
      */
-    public function serialize() {
+    public function serialize()
+    {
         return serialize(array(
             $this->id,
             $this->username,
@@ -182,12 +230,13 @@ class User implements UserInterface, \Serializable {
     /**
      * @see \Serializable::unserialize()
      */
-    public function unserialize($serialized) {
+    public function unserialize($serialized)
+    {
         list (
-                $this->id,
-                $this->username,
-                $this->password
-                ) = unserialize($serialized);
+            $this->id,
+            $this->username,
+            $this->password
+            ) = unserialize($serialized);
     }
 
     /**
@@ -196,7 +245,8 @@ class User implements UserInterface, \Serializable {
      * @param \Ojstr\UserBundle\Entity\Role $roles
      * @return User
      */
-    public function addRole(\Ojstr\UserBundle\Entity\Role $roles) {
+    public function addRole(\Ojstr\UserBundle\Entity\Role $roles)
+    {
         $this->roles[] = $roles;
 
         return $this;
@@ -207,7 +257,8 @@ class User implements UserInterface, \Serializable {
      *
      * @param \Ojstr\UserBundle\Entity\Role $roles
      */
-    public function removeRole(\Ojstr\UserBundle\Entity\Role $roles) {
+    public function removeRole(\Ojstr\UserBundle\Entity\Role $roles)
+    {
         $this->roles->removeElement($roles);
     }
 
@@ -217,7 +268,8 @@ class User implements UserInterface, \Serializable {
      * @param \Ojstr\JournalBundle\Entity\Journal $journals
      * @return User
      */
-    public function addJournal(\Ojstr\JournalBundle\Entity\Journal $journals) {
+    public function addJournal(\Ojstr\JournalBundle\Entity\Journal $journals)
+    {
         $this->journals[] = $journals;
 
         return $this;
@@ -228,7 +280,8 @@ class User implements UserInterface, \Serializable {
      *
      * @param \Ojstr\JournalBundle\Entity\Journal $journals
      */
-    public function removeJournal(\Ojstr\JournalBundle\Entity\Journal $journals) {
+    public function removeJournal(\Ojstr\JournalBundle\Entity\Journal $journals)
+    {
         $this->journals->removeElement($journals);
     }
 
