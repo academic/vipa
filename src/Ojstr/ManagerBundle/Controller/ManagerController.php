b7fa113373d415f2aa971c20adc7184f1e0905d8
Hüseyin Mert <huseyinin@gmail.com>
2014-04-24 17:38:16 +0300

    foundation.css removed bootstrap added

--- src/Ojstr/ManagerBundle/Controller/ManagerController.php
+++ src/Ojstr/ManagerBundle/Controller/ManagerController.php
@@ -7,6 +7,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class ManagerController extends Controller {
 
     public function indexAction() {
+        return $this->render('OjstrManagerBundle:Default:index.html.twig');
+    }
+    
+     public function getManagerAction() {
         return $this->render('OjstrManagerBundle:Manager:index.html.twig');
     }
 
