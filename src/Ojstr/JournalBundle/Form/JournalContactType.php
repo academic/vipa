facf4e8dfd495ed37f51bb1730eb485cc86a02bd
hasantayyar <tayyar.besik@gmail.com>
2014-05-29 16:31:26 +0300

    JournalContact entity fix formtype

--- src/Ojstr/JournalBundle/Form/JournalContactType.php
+++ src/Ojstr/JournalBundle/Form/JournalContactType.php
@@ -6,14 +6,24 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class JournalContactType extends AbstractType
-{
-    
+class JournalContactType extends AbstractType {
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+                ->add('journal')
+                ->add('contact')
+                ->add('contactType')
+        ;
+    }
+
     /**
      * @param OptionsResolverInterface $resolver
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'Ojstr\JournalBundle\Entity\JournalContact'
         ));
@@ -22,8 +32,8 @@ class JournalContactType extends AbstractType
     /**
      * @return string
      */
-    public function getName()
-    {
+    public function getName() {
         return 'ojstr_journalbundle_journalcontact';
     }
+
 }
