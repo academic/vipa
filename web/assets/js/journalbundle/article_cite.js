d162fbeaddbe70a5c21aef40b1f03db4da70b1a6
Hüseyin Mert <huseyinin@gmail.com>
2014-06-06 16:57:31 +0300

    layout improvements for article<->citation adding

--- web/assets/js/journalbundle/article_cite.js
+++ web/assets/js/journalbundle/article_cite.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
-    
     $("#addArticleCitationInline").click(function(e) {
         e.preventDefault();
         $("#citationContainer").append($("#citationInfoFields").html());
+        refreshCitationOrders();
     });
 
-    $("#citationContainer").on("click", "a.removeArticleCitationInline", function() {
+    $("#citationContainer").on("click", "a.removeArticleCitationInline", function(e) {
+        e.preventDefault();
         $(this).parent().remove();
+        refreshCitationOrders();
     });
+
+    function refreshCitationOrders() {
+        $("#citationContainer input[name=orderNum]").each(function(index) {
+            console.log(index + 1);
+            $(this).attr("value", index + 1);
+        });
+    }
 });
