f9af23f8bd67542d5d8d04db900890b36d764bf9
Hüseyin Mert <huseyinin@gmail.com>
2014-06-10 17:01:01 +0300

    js edits

--- web/assets/js/journalbundle/article_cite.js
+++ web/assets/js/journalbundle/article_cite.js
@@ -81,7 +81,7 @@ $(document).ready(function() {
             }
         });
         $.post(REST_API_BASEURL + "articles/" + articleId + "/bulkcitations", {cites: JSON.stringify(citeDetails)}, function(resp) {
-            resp = JSON.parse(resp);
+            //resp = JSON.parse(resp);
             consolelog(resp);
         });
     });
