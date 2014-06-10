95b6cb00c325e92d95fbcaea41c4e72f8e502137
hasantayyar <tayyar.besik@gmail.com>
2014-06-10 11:11:00 +0300

    citation types

--- src/Ojstr/JournalBundle/DependencyInjection/OjstrJournalExtension.php
+++ src/Ojstr/JournalBundle/DependencyInjection/OjstrJournalExtension.php
@@ -12,17 +12,17 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class OjstrJournalExtension extends Extension
-{
+class OjstrJournalExtension extends Extension {
+
     /**
      * {@inheritDoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
-    {
+    public function load(array $configs, ContainerBuilder $container) {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
     }
+
 }
