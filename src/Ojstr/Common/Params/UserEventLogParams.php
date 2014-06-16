255bf18233483feaa002c6e62781c736179556fb
Hüseyin Mert <huseyinin@gmail.com>
2014-06-16 15:30:59 +0300

    User Event Log Listener implemented

---
+++ src/Ojstr/Common/Params/UserEventLogParams.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Ojstr\Common\Params;
+
+class UserEventLogParams {
+
+    public static $USER_LOGIN = 'USER_LOGIN_EVENT';
+    public static $USER_LOGOUT = 'USER_LOGOUT_EVENT';
+    public static $USER_FAIL = 'USER_FAIL_EVENT';
+    public static $USER_ADD = 'USER_ADD_EVENT';
+    public static $USER_REMOVE = 'USER_REMOVE_EVENT';
+    public static $USER_ROLE_ADD = 'USER_ROLE_ADD_EVENT';
+    public static $USER_ROLE_REMOVE = 'USER_ROLE_REMOVE_EVENT';
+    public static $ROLE_ADD = 'ROLE_ADD_EVENT';
+    public static $ROLE_REMOVE = 'ROLE_REMOVE_EVENT';
+
+}
