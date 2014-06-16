25e770275baf41fbb56979cbcc0e02f80ced4345
Hüseyin Mert <huseyinin@gmail.com>
2014-06-16 12:06:26 +0300

    new Twig function ojsuser() to check user in template

--- src/Ojstr/Common/Twig/OjsExtension.php
+++ src/Ojstr/Common/Twig/OjsExtension.php
@@ -2,12 +2,39 @@
 
 namespace Ojstr\Common\Twig;
 
+use Symfony\Component\DependencyInjection\ContainerInterface as Container;
+
 class OjsExtension extends \Twig_Extension {
+
+    private $container;
+
+    public function __construct(Container $container = null) {
+        $this->container = $container;
+    }
 
     public function getFilters() {
         return array(
             new \Twig_SimpleFilter('issn', array($this, 'issnValidateFilter')),
         );
+    }
+
+    public function getFunctions() {
+        return array(
+            'ojsuser' => new \Twig_Function_Method($this, 'checkUser', array('is_safe' => array('html')))
+        );
+    }
+
+    /**
+     * Check osj user and return user data as array
+     * @return \Ojstr\UserBundle\Entity\User
+     */
+    public function checkUser() {
+        $securityContext = $this->container->get('security.context');
+        if ($securityContext->isGranted('IS_AUTHENTICATED_FULLY')) {
+            $user = $this->container->get('security.context')->getToken()->getUser();
+            return $user;
+        }
+        return FALSE;
     }
 
     /**
@@ -16,7 +43,7 @@ class OjsExtension extends \Twig_Extension {
      * @return string
      */
     public function issnValidateFilter($issn, $withErrors = FALSE) {
-        return $issn."---";
+        return $issn . "---";
     }
 
     public function getName() {
