f6756a9c7b999e5af6ca67205afc0071d31d7749
Hüseyin Mert <huseyinin@gmail.com>
2014-06-21 21:02:13 +0300

    Workflow Bundle added

--- src/Ojstr/WorkflowBundle/Controller/DefaultController.php
+++ src/Ojstr/WorkflowBundle/Controller/DefaultController.php
@@ -4,10 +4,10 @@ namespace Ojstr\WorkflowBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function indexAction($name)
-    {
-        return $this->render('OjstrWorkflowBundle:Default:index.html.twig', array('name' => $name));
+class DefaultController extends Controller {
+
+    public function indexAction() {
+        return $this->render('OjstrWorkflowBundle:Default:index.html.twig');
     }
+
 }
