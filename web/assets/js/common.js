3caede76a1472e6da1af4b7ed20fab04e6b79147
hasantayyar <tayyar.besik@gmail.com>
2014-07-02 16:23:54 +0300

    Article submission multi-language article data support #35

--- web/assets/js/common.js
+++ web/assets/js/common.js
@@ -1,3 +1,21 @@
+// to serialize form data as Json object
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
 
 var OjstrCommon = {
     inArray: function(needle, haystack) {
