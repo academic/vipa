da1b24586d4ce172027548ba5f73d7f4dc922aa1
hasantayyar <tayyar.besik@gmail.com>
2014-07-03 18:38:49 +0300

    Article submission form improvement #35

--- src/Ojstr/JournalBundle/Controller/ArticleSubmissionStepController.php
+++ src/Ojstr/JournalBundle/Controller/ArticleSubmissionStepController.php
@@ -5,8 +5,9 @@ namespace Ojstr\JournalBundle\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Ojstr\Common\Controller\OjsController as Controller;
 use Ojstr\JournalBundle\Entity\Article;
-use Ojstr\JournalBundle\Form\ArticleType;
-use Symfony\Component\HttpFoundation\Session\Session;
+//use Ojstr\JournalBundle\Form\ArticleType;
+//use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * Article submission step controller
@@ -18,15 +19,17 @@ class ArticleSubmissionStepController extends Controller {
      *
      */
     public function step1Action(Request $request, $locale) {
+        $em = $this->getDoctrine()->getManager();
         $article = new Article();
         $article->setTitle($request->get('title'));
         $article->setSubtitle($request->get('subtitle'));
         $article->setTitleTransliterated($request->get('titleTransliterated'));
         $article->setDoi($request->get('doi'));
-        $article->setKeywords(implode(",", $request->get('keywords')));
+        $article->setKeywords($request->get('keywords'));
         $article->setAbstract($request->get('abstract'));
         $article->setJournalId($request->get('journalId'));
-
+        $em->persist($article);
+        $em->flush();
         /* todo */
         return new JsonResponse(array('id' => $article->getId()));
     }
--- web/assets/js/journalbundle/article_submission.js
+++ web/assets/js/journalbundle/article_submission.js
@@ -1,5 +1,11 @@
 var OjsArticleSubmision = {
-    languages: ['en'],
+    languages: [],
+    activateFirstLanguageTab: function() {
+        firsttab = $("#langtabs .tab-pane").first().attr('id');
+        if (firsttab) {
+            $("li.lang a[href=#" + firsttab + "]").tab("show");
+        }
+    },
     step1: function(actionUrl, next) {
         forms = $(".tab-pane");
         if (forms.length === 0) {
@@ -8,10 +14,48 @@ var OjsArticleSubmision = {
         forms.each(function() {
             data = $("form", this).serializeObject();
             locale = $(this).attr('id');
-            postUrl = actionUrl.replace('locale', locale); 
+            postUrl = actionUrl.replace('locale', locale);
             $.post(postUrl, data, function(response) {
                 console.log(response);
             });
         });
     }
 };
+
+$(document).ready(function() {
+
+    $("ul#mainTabs li a").click(function(e) {
+        e.preventDefault();
+    });
+    $("ul#languageDropList li a").click(function(e) {
+        e.preventDefault();
+        langcode = $(this).attr('code');
+        langtitle = $(this).attr('lang');
+        // check if selected language tab already exists
+        if (OjstrCommon.inArray(langcode, OjsArticleSubmision.languages)) {
+            return false;
+        }
+        $("#langtabs").append('<div class="tab-pane" id="' + langcode + '">' + $("#formtpl").html() + '</div>');
+        OjsArticleSubmision.languages.push(langcode);
+        tabhtml = '<li class="lang" id="t_' + langcode + '"><a href="#' + langcode + '" role="tab" class="lang" data-toggle="tab">' + langtitle + '</a></li>';
+        $("ul#mainTabs li.lang").last().before(tabhtml);
+        OjsArticleSubmision.activateFirstLanguageTab();
+    });
+    $("#langtabs").on("click", "a.removelang", function(e) {
+        check = confirm("Are you sure to remove this language tab?");
+        if (!check) {
+            return false;
+        }
+        e.preventDefault();
+        langcode = $(this).parent().attr('id');
+        for (var i in OjsArticleSubmision.languages) {
+            console.log(OjsArticleSubmision.languages[i]);
+            if (OjsArticleSubmision.languages[i] === langcode) {
+                OjsArticleSubmision.languages.splice(i, 1);
+            }
+        }
+        $(this).parent().remove();
+        $('#t_' + langcode).remove();
+        OjsArticleSubmision.activateFirstLanguageTab();
+    });
+});
