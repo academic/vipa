e42e6ff80f1a8aee59a9030f962368abbd910ef1
Hüseyin Mert <huseyinin@gmail.com>
2014-07-09 15:53:35 +0300

    editor workflow pages

--- src/Ojstr/WorkflowBundle/Controller/EditorController.php
+++ src/Ojstr/WorkflowBundle/Controller/EditorController.php
@@ -14,17 +14,43 @@ class EditorController extends \Ojstr\Common\Controller\OjsController {
     }
 
     /**
-     * list in-review articles. 
+     * list published articles - Published
+     * see list of params at src/Ojstr/Common/Params/CommonParams.php
+     */
+    public function publishedArticlesAction() {
+        return $this->listArticles('articles_published', 2);
+    }
+
+    /**
+     * list in-review articles. - Reviewing
+     * see list of params at src/Ojstr/Common/Params/CommonParams.php
      */
     public function assignedArticlesAction() {
         return $this->listArticles('articles_assigned', 1);
     }
 
     /**
-     * list not assigned articles - waiting
+     * list not assigned articles - Waiting
+     * see list of params at src/Ojstr/Common/Params/CommonParams.php
      */
     public function waitingArticlesAction() {
         return $this->listArticles('articles_waiting', 0);
+    }
+
+    /**
+     * list not sumbitted articles - Not Submitted
+     * see list of params at src/Ojstr/Common/Params/CommonParams.php
+     */
+    public function uncompleteArticlesAction() {
+        return $this->listArticles('articles_uncomplete', -1);
+    }
+
+    public function unpublishedArticlesAction() {
+        return $this->listArticles('articles_unpublished', -2);
+    }
+
+    public function rejectedArticlesAction() {
+        return $this->listArticles('articles_rejected', -3);
     }
 
     private function listArticles($view, $status) {
