7b20a98fb1efca33d67f926829d26946c64db072
hasantayyar <tayyar.besik@gmail.com>
2014-07-15 14:49:51 +0300

    role based journal detail tpls

--- src/Ojstr/ManagerBundle/Controller/EditorController.php
+++ src/Ojstr/ManagerBundle/Controller/EditorController.php
@@ -22,4 +22,27 @@ class EditorController extends Controller {
         return $this->render('OjstrManagerBundle:Editor:dashboard.html.twig');
     }
 
+    public function myJournalsAction() {
+        $user_id = $this->container->get('security.context')->getToken()->getUser()->getId();
+        $em = $this->getDoctrine()->getManager();
+        $data = $em->createQuery(
+                        'SELECT  u  FROM OjstrUserBundle:UserJournalRole u WHERE u.userId = :user_id '
+                )->setParameter('user_id', $user_id)->getResult();
+        if ($data) {
+            $entities = array();
+            foreach ($data as $item) {
+                $entities[$item->getJournalId()]['journal'] = $item->getJournal();
+                $entities[$item->getJournalId()]['roles'][] = $item->getRole();
+            }
+        }
+
+        return $this->render('OjstrManagerBundle:Editor:myjournals.html.twig', array('entities' => $entities));
+    }
+
+    public function showJournalAction($id) {
+        $em = $this->getDoctrine()->getManager();
+        $journal = $em->getRepository('OjstrJournalBundle:Journal')->find($id);
+        return $this->render('OjstrJournalBundle:Journal:role_based/show_editor.html.twig', array('entity' => $journal));
+    }
+
 }
