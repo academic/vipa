aa9e5666809911f87feb2144a5aa82db5dfa9aa9
hasantayyar <tayyar.besik@gmail.com>
2014-07-17 00:02:15 +0300

    improvements for "domain based ojs" #61

--- src/Ojstr/ManagerBundle/Controller/AdminController.php
+++ src/Ojstr/ManagerBundle/Controller/AdminController.php
@@ -11,24 +11,30 @@ class AdminController extends Controller {
      * @return type
      */
     public function indexAction() {
+        /** @var $siteManager \KnpU\QADayBundle\Site\SiteManager */
+        $journalDomain = $this->container->get('journal_domain');
+        echo "<pre>--";
+        $currentJournal = $journalDomain->getCurrentJournal();
+        print_r( $currentJournal->getTitle());
+        exit();
+
         return $this->render('OjstrManagerBundle::index.html.twig');
     }
 
-    
     public function dashboardCheckAction() {
         $super_admin = $this->container->get('security.context')->isGranted('ROLE_SUPER_ADMIN');
         if ($super_admin) {
             return $this->redirect($this->generateUrl('dashboard_admin'));
-        }else{
+        } else {
             return $this->redirect($this->generateUrl('dashboard_editor'));
         }
     }
-    
+
     public function dashboardAction() {
         $super_admin = $this->container->get('security.context')->isGranted('ROLE_SUPER_ADMIN');
         if ($super_admin) {
             return $this->render('OjstrManagerBundle:Admin:dashboard.html.twig');
-        }else{
+        } else {
             return $this->redirect($this->generateUrl('dashboard_editor'));
         }
     }
