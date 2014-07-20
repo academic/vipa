5ad9530891ea4d94fb3711422c68cf185bffdd3e
hasantayyar <tayyar.besik@gmail.com>
2014-07-20 21:23:09 +0300

    created new bundle - search bundle #62

---
+++ src/Ojstr/SearchBundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ojstr\SearchBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        
+    }
+
+}
