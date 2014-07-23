b96a66931e136251557d6adad5eec08c558db849
Hüseyin Mert <huseyinin@gmail.com>
2014-07-23 09:57:13 +0300

    AnalyticsBundle mongodb documents updated improvements on issue #67

--- src/Ojstr/AnalyticsBundle/Document/ArticleView.php
+++ src/Ojstr/AnalyticsBundle/Document/ArticleView.php
@@ -5,14 +5,42 @@ namespace Ojstr\AnalyticsBundle\Document;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 
 /**
- * @MongoDb\Document(collection="analytics_views_article") 
+ * This collection keeps page information and total count *view action details*
+ * There will be one record for *each pages*
+ * @MongoDb\Document(collection="analytics_viewsum_article") 
  */
-class ArticleView extends ArticleStatsBase {
+class ArticleView {
 
     /**
      * @MongoDb\String
      */
     protected $pageUrl;
+
+    /**
+     * @MongoDb\String
+     */
+    protected $total;
+
+    /**
+     * Page full url with domain
+     * Set total
+     *
+     * @param string $total
+     * @return self
+     */
+    public function setTotal($total) {
+        $this->total = $total;
+        return $this;
+    }
+
+    /**
+     * Get total
+     *
+     * @return string $total
+     */
+    public function getTotal() {
+        return $this->total;
+    }
 
     /**
      * Page full url with domain
