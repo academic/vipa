a9ad39d49b83af5b61e22bd8c2be06ea31901e79
Hüseyin Mert <huseyinin@gmail.com>
2014-08-22 02:15:52 +0300

    major tests added

--- src/Ojstr/ManagerBundle/Tests/Controller/ManagerControllerTest.php
+++ src/Ojstr/ManagerBundle/Tests/Controller/ManagerControllerTest.php
@@ -8,6 +8,9 @@ class ManagerControllerTest extends \Ojstr\Common\Helper\TestHelper {
         $this->logIn('demo_editor', array('ROLE_EDITOR'));
         $this->client->request('GET', '/editor/dashboard');
         $this->assertTrue($this->client->getResponse()->isSuccessful());
+
+        $this->client->request('GET', '/editor/myjournals');
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
     }
 
 }
