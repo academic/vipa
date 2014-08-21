ffc4a0635352f2e8c288e2a1fddd8d68af26cb4b
Hüseyin Mert <huseyinin@gmail.com>
2014-08-22 02:02:24 +0300

    TestHelper Class updated - login method updated to set user object instead of username as session value

--- src/Ojstr/Common/Helper/TestHelper.php
+++ src/Ojstr/Common/Helper/TestHelper.php
@@ -13,6 +13,7 @@ session_start();
 class TestHelper extends WebTestCase {
 
     protected $client = null;
+    protected $em = null;
 
     function __construct() {
         parent::__construct();
@@ -20,6 +21,10 @@ class TestHelper extends WebTestCase {
 
     public function setUp() {
         $this->client = static::createClient();
+        $this->em = static::$kernel->getContainer()
+                ->get('doctrine')
+                ->getManager()
+        ;
     }
 
     public function testStatus() {
@@ -34,7 +39,9 @@ class TestHelper extends WebTestCase {
     protected function logIn($username = NULL, $role = NULL) {
         $session = $this->client->getContainer()->get('session');
         $firewall = 'main';
-        $token = new UsernamePasswordToken($username ? $username : 'admin', null, $firewall, $role ? $role : array('ROLE_SUPER_ADMIN'));
+        $username = $username ? $username : 'admin';
+        $user = $this->em->getRepository('OjstrUserBundle:User')->findOneByUsername($username);
+        $token = new UsernamePasswordToken($user, null, $firewall, $role ? $role : array('ROLE_SUPER_ADMIN'));
         $session->set('_security_' . $firewall, serialize($token));
         $session->save();
         $cookie = new Cookie($session->getName(), $session->getId());
