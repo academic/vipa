340ce3a789f70f3c84addb36414101d427b47d62
Hüseyin Mert <huseyinin@gmail.com>
2014-08-22 14:49:20 +0300

    some tests

--- src/Ojstr/JournalBundle/Tests/Controller/InstitutionTypesControllerTest.php
+++ src/Ojstr/JournalBundle/Tests/Controller/InstitutionTypesControllerTest.php
@@ -7,7 +7,13 @@ use \Ojstr\Common\Helper\TestHelper;
 class InstitutionTypesControllerTest extends TestHelper {
 
     public function testStatus() {
-        $this->assertEquals(TRUE, TRUE);
+        $this->logIn('admin', array('ROLE_SUPER_ADMIN'));
+
+        $this->client->request('GET', '/admin/institution_types/');
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+
+        $this->client->request('GET', '/admin/institution_types/new');
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
     }
 
 }
