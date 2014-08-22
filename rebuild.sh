18e0c3a039757cb1536d25740a3911e14490bf0c
Hüseyin Mert <huseyinin@gmail.com>
2014-08-22 15:58:38 +0300

    ignore rebuild.sh

--- rebuild.sh
+++
@@ -1,4 +0,0 @@
-mysql -uroot -proot -e "drop database ojssf;"
-sudo php app/console doctrine:database:create
-sudo php app/console ojs:install
-sudo php app/console doctrine:fixtures:load --append -v
