a604fb087d09eea34c589e62b9f9e12b8f9bd94b
hasantayyar <tayyar.besik@gmail.com>
2014-08-28 15:18:35 +0300

    article submission routes and controllers separated

---
+++ src/Ojstr/JournalBundle/Controller/ArticleSubmissionStep5Controller.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ojstr\JournalBundle\Controller;
+
+use Ojstr\Common\Controller\OjsController as Controller;
+
+class ArticleSubmissionStep5Controller extends Controller {
+    
+}
