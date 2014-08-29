0c48b59569c0fdd39d6c3c0bae1e7e756401aba3
Hüseyin Mert <huseyinin@gmail.com>
2014-08-29 12:10:24 +0300

    bower updated.
    - leanmodal removed
    - new  modal library added
    - common.js updated to use new modal library.

--- web/assets/js/jsglobals.js
+++ web/assets/js/jsglobals.js
@@ -2,27 +2,38 @@ var REST_API_BASEURL = "/api/";
 
 var OjstrApp;
 OjstrApp = OjstrApp || (function() {
-    var pleaseWaitDiv = $('<div class="modal" aria-hidden="true" \n\
-id="pleaseWaitDialog" data-backdrop="static" data-keyboard="false">\n\
-<div class="modal-dialog">\n\
-<div class="modal-content">\n\
-<div class="modal-header"><strong>Processing...</strong></div>\n\
-<div class="progress">\n\
-<div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="90" \n\
-aria-valuemin="0" aria-valuemax="100" style="width: 90%"><span class="sr-only">--</span></div></div>\n\
-</div></div>');
-    return {
-        showPleaseWait: function() {
-            pleaseWaitDiv.modal();
-        },
-        hidePleaseWait: function() {
-            pleaseWaitDiv.modal('hide');
-        }
-    };
+    
 })();
+
+var modalTypes = [];
+modalTypes.default = BootstrapDialog.TYPE_DEFAULT;
+modalTypes.info = BootstrapDialog.TYPE_INFO;
+modalTypes.primary = BootstrapDialog.TYPE_PRIMARY;
+modalTypes.success = BootstrapDialog.TYPE_SUCCESS;
+modalTypes.warning = BootstrapDialog.TYPE_WARNING;
+modalTypes.danger = BootstrapDialog.TYPE_DANGER;
+
 
 
 var OjstrCommon = {
+    errorModal: function(message, title) {
+        BootstrapDialog.show({
+            title: title ? title : 'Error',
+            message: message,
+            type: modalTypes.error
+        });
+    },
+    hideallModals: function() {
+        BootstrapDialog.closeAll();
+    },
+    waitModal: function(customWaitingMessage) {
+        BootstrapDialog.show({
+            title: 'Wait',
+            message: customWaitingMessage ? customWaitingMessage : 'Please wait...',
+            type: modalTypes.info,
+            closable: false
+        });
+    },
     inArray: function(needle, haystack) {
         var length = haystack.length;
         for (var i = 0; i < length; i++) {
