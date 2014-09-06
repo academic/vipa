e48c5e3cd2207e4c701d7aeeb1b3b1bf4c50652b
Hüseyin Mert <huseyinin@gmail.com>
2014-09-06 15:12:22 +0300

    travis goodness

--- src/Ojstr/UserBundle/Tests/Controller/ProxyControllerTest.php
+++ src/Ojstr/UserBundle/Tests/Controller/ProxyControllerTest.php
@@ -5,5 +5,10 @@ namespace Ojstr\UserBundle\Tests\Controller;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ProxyControllerTest extends WebTestCase {
-    
+    public function testStatus() {
+        $this->logIn('admin', array('ROLE_SUPER_ADMIN'));
+
+        $this->client->request('GET', '/parents');
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+    }
 }
