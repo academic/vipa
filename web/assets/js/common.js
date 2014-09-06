09eb79e3428d8b48684766d4e81a16026afae502
Hüseyin Mert <huseyinin@gmail.com>
2014-09-06 17:39:45 +0300

    draft #91

--- web/assets/js/common.js
+++ web/assets/js/common.js
@@ -46,6 +46,29 @@ $(document).ready(function() {
             $(this).removeClass('open');
         }
     });
+    
+    var $btnSets = $('#responsive'),
+    $btnLinks = $btnSets.find('a');
+ 
+    $btnLinks.click(function(e) {
+        e.preventDefault();
+        $(this).siblings('a.active').removeClass("active");
+        $(this).addClass("active");
+        var index = $(this).index();
+        $("div.user-menu>div.user-menu-content").removeClass("active");
+        $("div.user-menu>div.user-menu-content").eq(index).addClass("active");
+    });
+
+    $("[rel='tooltip']").tooltip();    
+ 
+    $('.view').hover(
+        function(){
+            $(this).find('.caption').slideDown(250); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.caption').slideUp(250); //.fadeOut(205)
+        }
+    ); 
 });
 
 
