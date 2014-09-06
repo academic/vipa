ff1cba17b25ac76587ebccb2b9b41e7724b2d80a
Hüseyin Mert <huseyinin@gmail.com>
2014-09-06 15:12:22 +0300

    travis goodness

--- src/Ojstr/JournalBundle/Tests/Controller/JournalSectionControllerTest.php
+++ src/Ojstr/JournalBundle/Tests/Controller/JournalSectionControllerTest.php
@@ -5,5 +5,11 @@ namespace Ojstr\JournalBundle\Tests\Controller;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class JournalSectionControllerTest extends WebTestCase {
+    public function testStatus() {
+        $this->logIn('admin', array('ROLE_SUPER_ADMIN'));
+
+        $this->client->request('GET', '/admin/journal/section');
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+    }
     
 }
