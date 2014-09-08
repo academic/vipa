f1563e9553b71d1b7d1020715cc2c4179b917f3f
Hüseyin Mert <huseyinin@gmail.com>
2014-09-08 23:39:11 +0300

    ScheduledTasks Command not implemented yet

--- src/Ojstr/CliBundle/Command/ScheduledTasksCommand.php
+++ src/Ojstr/CliBundle/Command/ScheduledTasksCommand.php
@@ -7,29 +7,28 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class IndexArticlesCommand extends ContainerAwareCommand {
+class cheduledTasksCommand extends ContainerAwareCommand {
 
     protected function configure() {
         $this
-                ->setName('search:index')
-                ->setDescription('Index all articles or given journal\'s articles to search engine ')
-                ->addArgument('journal-id', InputArgument::OPTIONAL, 'Journal id')
+                ->setName('scheduled:run')
+                ->setDescription('check and run all scheduled jobs. You can pass job-id if you know.')
+                ->addArgument('job-id', InputArgument::OPTIONAL, 'Spesific job id')
         ;
     }
 
     /**
      * Not implemented yet
-     *  @todo not implemented yet
+     * @todo not implemented yet
      * @param \Symfony\Component\Console\Input\InputInterface $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
      */
     protected function execute(InputInterface $input, OutputInterface $output) {
-        $journalId = $input->getArgument('journal-id');
-        $dialog = $this->getHelperSet()->get('dialog');
+        $jobId = $input->getArgument('job-id');
         $kernel = $this->getContainer()->get('kernel');
         $application = new \Symfony\Bundle\FrameworkBundle\Console\Application($kernel);
         $application->setAutoExit(false);
-        $output->writeln('<info>Index ' . ($journalId ? 'of journal#' . $journalId : 'all') . ' articles</info>');
+        $output->writeln('<info>Run Scheduled Jobs : ' . ($jobId ? ' job#' . $jobId : 'All') . '</info>');
 
 
 
