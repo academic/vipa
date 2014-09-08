aec493ad97bc9d16c436b93f3b7e6eca3c4b98cc
hasantayyar <tayyar.besik@gmail.com>
2014-09-09 00:58:27 +0300

    typo on ScheduledTasks classname

--- src/Ojstr/CliBundle/Command/ScheduledTasksCommand.php
+++ src/Ojstr/CliBundle/Command/ScheduledTasksCommand.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class cheduledTasksCommand extends ContainerAwareCommand {
+class ScheduledTasksCommand extends ContainerAwareCommand {
 
     protected function configure() {
         $this
@@ -18,7 +18,7 @@ class cheduledTasksCommand extends ContainerAwareCommand {
     }
 
     /**
-     * Not implemented yet
+     * Not implemented 
      * @todo not implemented yet
      * @param \Symfony\Component\Console\Input\InputInterface $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
