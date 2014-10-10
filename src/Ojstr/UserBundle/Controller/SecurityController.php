7b8d0024c0f1bd9a9a516d56d7557246d4e13c0d
Hüseyin Mert <huseyinin@gmail.com>
2014-10-10 17:56:17 +0300

    email confirmation fixes

--- src/Ojstr/UserBundle/Controller/SecurityController.php
+++ src/Ojstr/UserBundle/Controller/SecurityController.php
@@ -9,14 +9,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\SecurityContext;
 
-class SecurityController extends Controller
-{
+class SecurityController extends Controller {
 
     /**
      * Show unconfirmed user warning page
      */
-    public function unconfirmedAction()
-    {
+    public function unconfirmedAction() {
         $user = $this->getUser();
         if (!$user) {
             return $this->redirect($this->generateUrl('login'));
@@ -26,12 +24,11 @@ class SecurityController extends Controller
         }
 
         return $this->render(
-            'OjstrUserBundle:Security:unconfirmedUser.html.twig'
+                        'OjstrUserBundle:Security:unconfirmedUser.html.twig'
         );
     }
 
-    public function confirmEmailAction(Request $request, $code)
-    {
+    public function confirmEmailAction(Request $request, $code) {
         $session = $request->getSession();
         /**
          * @var User $user
@@ -42,34 +39,36 @@ class SecurityController extends Controller
 
             return $this->redirect($this->generateUrl('login'), 302);
         }
-        $do = $this->getDoctrine();
-        $em = $do->getRepository('OjstrUserBundle:User');
+        $do = $this->getDoctrine(); 
+        $em = $this->getDoctrine()->getManager();
         $flashBag = $request->getSession()->getFlashBag();
         //check confirmation code
         if ($user->getToken() == $code) {
             // add ROLE_USER
-            $role = $do->getRepository('OjstrUserBundle:Role')->getByRole('ROLE_USER');
-            $user->addRole($role);
-            $user->setToken(null);
-            $em->persist($user);
-            $flashBag->add('success', 'You\'ve confirmed your email successfully!');
-
-            return $this->redirect($this->generateUrl('myprofile'));
+            $role = $do->getRepository('OjstrUserBundle:Role')->findOneBy(array('role'=>'ROLE_USER'));
+            if ($role) {
+                $user->addRole($role);
+                $user->setToken(null);
+                $em->persist($user);
+                $flashBag->add('success', 'You\'ve confirmed your email successfully!');
+                return $this->redirect($this->generateUrl('myprofile'));
+            }
+            $flashBag->add('error', 'System error.');
+            return $this->redirect($this->generateUrl('login'));
         }
 
         $flashBag->add('error', 'There is an error while confirming your email address.' .
-            '<br>Your confirmation link may be expired.');
+                '<br>Your confirmation link may be expired.');
 
         return $this->redirect($this->generateUrl('confirm_email_warning'));
     }
 
-    public function loginAction(Request $request)
-    {
+    public function loginAction(Request $request) {
         $session = $request->getSession();
         // get the login error if there is one
         if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
             $error = $request->attributes->get(
-                SecurityContext::AUTHENTICATION_ERROR
+                    SecurityContext::AUTHENTICATION_ERROR
             );
         } else {
             $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
@@ -77,31 +76,28 @@ class SecurityController extends Controller
         }
 
         return $this->render(
-            'OjstrUserBundle:Security:login.html.twig', array(
-                // last username entered by the user
-                'last_username' => $session->get(SecurityContext::LAST_USERNAME),
-                'error' => $error,
-            )
+                        'OjstrUserBundle:Security:login.html.twig', array(
+                    // last username entered by the user
+                    'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+                    'error' => $error,
+                        )
         );
     }
 
-    private function encodePassword(User $user, $plainPassword)
-    {
+    private function encodePassword(User $user, $plainPassword) {
         $encoder = $this->container->get('security.encoder_factory')
-            ->getEncoder($user);
+                ->getEncoder($user);
 
         return $encoder->encodePassword($plainPassword, $user->getSalt());
     }
 
-    private function authenticateUser(User $user)
-    {
+    private function authenticateUser(User $user) {
         $providerKey = 'main'; //  firewall name
         $token = new UsernamePasswordToken($user, null, $providerKey, $user->getRoles());
         $this->container->get('security.context')->setToken($token);
     }
 
-    public function registerAction(Request $request)
-    {
+    public function registerAction(Request $request) {
         $session = $request->getSession();
         $error = null;
         $user = new User();
@@ -117,22 +113,21 @@ class SecurityController extends Controller
             $em->flush();
             //$this->authenticateUser($user); // auth. user
             $request->getSession()
-                ->getFlashBag()
-                ->add('success', 'Success. You are registered.');
+                    ->getFlashBag()
+                    ->add('success', 'Success. You are registered.');
 
             return $this->redirect($this->generateUrl('login'));
         }
 
         return $this->render(
-            'OjstrUserBundle:Security:register.html.twig', array(
-                'form' => $form->createView(),
-                'errors' => $form->getErrors(),
-            )
+                        'OjstrUserBundle:Security:register.html.twig', array(
+                    'form' => $form->createView(),
+                    'errors' => $form->getErrors(),
+                        )
         );
     }
 
-    public function createUserAction(Request $request)
-    {
+    public function createUserAction(Request $request) {
         $username = $request->get('_username');
         $email = $request->get('_email');
         $password = $request->get('_password');
