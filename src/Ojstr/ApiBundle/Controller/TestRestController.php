27219d4853a69497eb7628e95f1dea34231b1806
Hüseyin Mert <huseyinin@gmail.com>
2014-10-29 12:57:47 +0200

    test #13

--- src/Ojstr/ApiBundle/Controller/TestRestController.php
+++ src/Ojstr/ApiBundle/Controller/TestRestController.php
@@ -2,14 +2,26 @@
 
 namespace Ojstr\ApiBundle\Controller;
 
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations\Get;
 
-class TestRestController extends FOSRestController
-{
-    public function getTestAction()
-    {
-        $res = array("status" => "ok");
+class TestRestController extends FOSRestController {
+
+    /**
+     *
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Get Test Action",
+     *  filters={
+     *      {"name"="id", "dataType"="integer"}
+     *  }
+     * )
+     * @Get("/test/{id}")
+     */
+    public function getTestAction($id) {
+        $res = array("status" => $id);
 
         return $res;
     }
