94bc9ce2055d7b9f53073892ea07b4def9a292a5
Hüseyin Mert <huseyinin@gmail.com>
2014-11-07 19:37:56 +0200

    product touring logical assets method changed

---
+++ src/Ojs/JournalBundle/Resources/public/js/tour/article_submission_tour.js
@@ -0,0 +1,11 @@
+var tour = new Tour({
+    steps: [
+        {
+            element: "#article-submission-steps",
+            title: "Submission steps",
+            content: "You can watch your article submission proccess"
+        }
+    ]}
+);
+tour.init();
+tour.start();
