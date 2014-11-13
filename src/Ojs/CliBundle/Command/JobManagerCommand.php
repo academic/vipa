b65fbfc57b0162147a4162f94efb0d0e6d02d88c
hasantayyar <tayyar.besik@gmail.com>
2014-11-13 17:50:17 +0200

    Implementations for Job queue manager and scheduled jobs #219

---
+++ src/Ojs/CliBundle/Command/JobManagerCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Ojs\CliBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class JobManagerCommand extends ContainerAwareCommand
+{
+
+    protected function configure()
+    {
+        $this
+                ->setName('ojs:jobqueue:add')
+                ->setDescription('Add a new job to queue')
+                ->addArgument('action', InputArgument::OPTIONAL, 'app/console command')
+                ->addArgument('options', InputArgument::IS_ARRAY, 'options')
+        ;
+    }
+
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface   $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $command = $input->getArgument('action');
+        $options = $input->getArgument('options');
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $kernel = $this->getContainer()->get('kernel');
+        $application = new \Symfony\Bundle\FrameworkBundle\Console\Application($kernel);
+        $application->setAutoExit(false);
+        $output->writeln('<info>Running command ' . $command . '</info>');
+        $job = new \JMS\JobQueueBundle\Entity\Job($command, $options);
+        $em->persist($job);
+        $em->flush($job);
+
+        $output->writeln("\nDONE\n");
+    }
+
+}
