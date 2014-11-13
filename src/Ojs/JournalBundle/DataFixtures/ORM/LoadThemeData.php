686b9b10fb22f3c04a53e7e28628ac450b59ce53
Hüseyin Mert <huseyinin@gmail.com>
2014-11-14 00:31:18 +0200

    add default theme entry while installation

--- src/Ojs/JournalBundle/DataFixtures/ORM/LoadThemeData.php
+++ src/Ojs/JournalBundle/DataFixtures/ORM/LoadThemeData.php
@@ -4,17 +4,13 @@ namespace Ojs\JournalBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
-use Doctrine\Common\Persistence\ObjectManager;
-use Ojs\JournalBundle\Entity\Theme;
+use Doctrine\Common\Persistence\ObjectManager; 
 
 class LoadThemeData extends AbstractFixture implements OrderedFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        $theme = new Theme();
-        $theme->setName("default");
-        $manager->persist($theme);
-        $manager->flush();
+       
     }
 
     public function getOrder()
