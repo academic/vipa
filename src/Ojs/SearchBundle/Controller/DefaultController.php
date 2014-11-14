765ba6b2e7296948a17e0f90c37830147ff38749
Aybars Cengaver <aybarscengaver@gmail.com>
2014-11-14 13:36:15 +0200

    ojs-62 Search Bundle fixes #62

--- src/Ojs/SearchBundle/Controller/DefaultController.php
+++ src/Ojs/SearchBundle/Controller/DefaultController.php
@@ -3,12 +3,24 @@
 namespace Ojs\SearchBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request,$page=1)
     {
-        return $this->render('OjsSearchBundle:Default:index.html.twig');
+        $data =[];
+        $term = $request->get('q');
+        $searchManager = $this->get('ojs_search_manager');
+        $searchManager->addParam('term',$term);
+        $searchManager->setPage($page);
+        $result = $searchManager->search()->getResult();
+        $data['result'] = $result;
+        $data['total_count'] = $searchManager->getCount();
+        $data['page'] = $searchManager->getPageCount();
+        $data['page_count'] = $searchManager->getPageCount();
+        $data['term'] = $term;
+        return $this->render('OjsSearchBundle:Default:index.html.twig',$data);
     }
 
 }
