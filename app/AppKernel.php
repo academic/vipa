a1c6d73b7d406ca910658fffbb8c3802c426467b
Hüseyin Mert <huseyinin@gmail.com>
2014-11-20 11:36:42 +0200

    Feedback bundle created

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -43,6 +43,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\AopBundle\JMSAopBundle(),
             new Ojs\ReportBundle\OjsReportBundle(),
+            new Ojs\FeedbackBundle\OjsFeedbackBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
