14b4ef3bdc46f5d8e08a27e217347f7e2b1886e8
hasantayyar <tayyar.besik@gmail.com>
2014-11-21 11:29:41 +0200

    citation-parser api wrapper still has problems

--- src/Ojs/JournalBundle/Controller/ArticleSubmission/ArticleSubmissionStep2Controller.php
+++ src/Ojs/JournalBundle/Controller/ArticleSubmission/ArticleSubmissionStep2Controller.php
@@ -9,15 +9,16 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class ArticleSubmissionStep2Controller extends Controller
 {
 
-    public function addAuthorsAction(Request $request, $articleId)
+    public function addAuthorsAction(Request $request)
     {
         $authorsData = json_decode($request->request->get('authorsData'));
+        $submissionId = $request->get("submissionId");
         if (empty($authorsData)) {
             return new \Symfony\Component\HttpFoundation\Response('', 400);
         }
-        $dm = $this->get('doctrine_mongodb')->getManager(); 
-        $articleSubmission = $dm->getRepository('OjsJournalBundle:ArticleSubmission')
-                ->findByArticleId($articleId);
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $articleSubmission = $dm->getRepository('OjsJournalBundle:ArticleSubmissionProgress')
+                ->find($submissionId);
         $articleSubmission->setAuthors($authorsData);
         $dm->persist($articleSubmission);
         $dm->flush();
