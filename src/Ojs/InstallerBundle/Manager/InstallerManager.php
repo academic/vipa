e4f2464cd6b5e1e873a06394b13e3feea088cac8
Aybars Cengaver <aybarscengaver@gmail.com>
2014-12-03 23:44:42 +0200

    installer bundle tries. #256

---
+++ src/Ojs/InstallerBundle/Manager/InstallerManager.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Date: 3.12.14
+ * Time: 23:42
+ */
+
+namespace Ojs\InstallerBundle\Manager;
+
+
+class InstallerManager {
+    public function check()
+    {
+
+    }
+
+    public function configure()
+    {
+
+    }
+
+    public function runCommand()
+    {
+
+    }
+}
