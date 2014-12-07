b434116203eae93d7f69f3d281e1c1372800ff94
Hüseyin Mert <huseyinin@gmail.com>
2014-12-07 18:33:02 +0200

    general ui changes

--- web/assets/app/js/common.js
+++ web/assets/app/js/common.js
@@ -24,17 +24,6 @@ $(document).ready(function () {
         }
     });
 
-    $('a[href="#search"]').on('click', function (event) {
-        event.preventDefault();
-        $('#search').addClass('open');
-        $('#search > form > input[type="search"]').focus();
-    });
-
-    $('#search, #search button.close').on('click keyup', function (event) {
-        if (event.target == this || event.target.className === 'close' || event.keyCode === 27) {
-            $(this).removeClass('open');
-        }
-    });
 
     var $btnSets = $('#responsive'),
             $btnLinks = $btnSets.find('a');
