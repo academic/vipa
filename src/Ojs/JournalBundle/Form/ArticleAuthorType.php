b157ddac999d6a842ddb6a5af07ead6e8bb150c1
Aybars Cengaver <aybarscengaver@gmail.com>
2014-12-09 22:07:59 +0200

    Article author entity fields #265

--- src/Ojs/JournalBundle/Form/ArticleAuthorType.php
+++ src/Ojs/JournalBundle/Form/ArticleAuthorType.php
@@ -2,23 +2,36 @@
 
 namespace Ojs\JournalBundle\Form;
 
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class ArticleAuthorType extends AbstractType
 {
-        /**
+    /**
      * @param FormBuilderInterface $builder
-     * @param array                $options
+     * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $journal = $options['journal_id'];
         $builder
             ->add('authorOrder')
-            ->add('authorId')
-            ->add('articleId')
-        ;
+            ->add('author', 'entity', [
+                'class' => 'Ojs\JournalBundle\Entity\ArticleAuthor'
+            ])
+            ->add('article', 'entity', [
+                'class' => 'Ojs\JournalBundle\Entity\Article',
+                'query_builder' => function (EntityRepository $er) use ($journal) {
+                    $qb = $er->createQueryBuilder('a');
+                    $qb->where(
+                        $qb->expr()->eq('a.journalId', ':journal')
+                    )
+                        ->setParameter('journal', $journal);
+                    return $qb;
+                }
+            ]);
     }
 
     /**
@@ -27,7 +40,8 @@ class ArticleAuthorType extends AbstractType
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'Ojs\JournalBundle\Entity\ArticleAuthor'
+            'data_class' => 'Ojs\JournalBundle\Entity\ArticleAuthor',
+            'journal_id' => 0
         ));
     }
 
