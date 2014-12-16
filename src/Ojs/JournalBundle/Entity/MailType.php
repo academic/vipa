97fe0ad95042373bf53797f1b04722d5c758d4dd
Hüseyin Mert <huseyinin@gmail.com>
2014-12-16 13:31:57 +0200

    MailType entity and management pages added

---
+++ src/Ojs/JournalBundle/Entity/MailType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Ojs\JournalBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * MailType
+ */
+class MailType
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return MailType
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}
