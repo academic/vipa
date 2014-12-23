da6c6d778e4894b2ee5f9c5ca9a3115f2c13b8bc
Hüseyin Mert <huseyinin@gmail.com>
2014-12-23 17:39:14 +0200

    Article entity updated

--- src/Ojs/JournalBundle/Entity/Article.php
+++ src/Ojs/JournalBundle/Entity/Article.php
@@ -219,6 +219,11 @@ class Article extends \Ojs\Common\Entity\GenericExtendedEntity
     private $header;
 
     /**
+     * @var integer
+     */
+    private $order = 0;
+
+    /**
      * @return string
      */
     public function getHeader()
@@ -1029,6 +1034,24 @@ class Article extends \Ojs\Common\Entity\GenericExtendedEntity
     public function setSlug($slug)
     {
         $this->slug = $slug;
+        return $this;
+    }
+    
+    /**
+     * @return integer
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+
+    /**
+     * @param integer $order
+     * @return $this
+     */
+    public function setOrder($order)
+    {
+        $this->order= $order;
         return $this;
     }
 
