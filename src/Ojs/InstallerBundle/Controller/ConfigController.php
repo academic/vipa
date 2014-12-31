2154f32bc721495848248fdecfd10ff955054e10
Hüseyin Mert <huseyinin@gmail.com>
2014-12-31 15:04:02 +0200

    config from database

--- src/Ojs/InstallerBundle/Controller/ConfigController.php
+++ src/Ojs/InstallerBundle/Controller/ConfigController.php
@@ -3,11 +3,23 @@
 namespace Ojs\InstallerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Unifik\DatabaseConfigBundle\Entity\Parameter;
+use Doctrine\Common\Persistence\ObjectManager;
 
 class ConfigController extends Controller {
 
     public function configureAction() {
-        $data['page']='config';
+        $data['page'] = 'config';
+        
+
+//        $parameter = new Parameter();
+//        $parameter->setName('database_driver');
+//        $parameter->setValue('pdo_mysql');
+//        // ObjectManager $manager
+//        $manager->persist($parameter);
+//        $manager->flush();
+
+
         return $this->render("OjsInstallerBundle:Default:configure.html.twig", array('data' => $data));
     }
 
