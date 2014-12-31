e8cdb6e3f3dd579c5ae2c51ea52ad937020911ef
Aybars Cengaver <aybarscengaver@gmail.com>
2014-12-31 15:55:16 +0200

    Read parameters and put to configure form.

--- src/Ojs/InstallerBundle/Controller/ConfigController.php
+++ src/Ojs/InstallerBundle/Controller/ConfigController.php
@@ -2,25 +2,39 @@
 
 namespace Ojs\InstallerBundle\Controller;
 
+use Ojs\InstallerBundle\Entity\Config;
+use Ojs\InstallerBundle\Form\ConfigType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\DependencyInjection\Dumper\YamlDumper;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Yaml\Parser;
 use Unifik\DatabaseConfigBundle\Entity\Parameter;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class ConfigController extends Controller {
 
-    public function configureAction() {
-        $data['page'] = 'config';
-        
+    public function configureAction(Request $request) {
+        $data=[];
+        $data['data']['page'] = 'config';
+        $parametersFile = dirname($request->server->get('DOCUMENT_ROOT').'../')
+            .'/app/config/parameters.yml';
+        $parametersFileDist = $parametersFile.'.dist';
+        $formData = new Config();
+        $parser = new Parser();
+        if(file_exists($parametersFile)){
+            $formDataFile = $parser->parse(file_get_contents($parametersFile));
+        }else{
+            $formDataFile = $parser->parse(file_get_contents($parametersFileDist));
 
-//        $parameter = new Parameter();
-//        $parameter->setName('database_driver');
-//        $parameter->setValue('pdo_mysql');
-//        // ObjectManager $manager
-//        $manager->persist($parameter);
-//        $manager->flush();
-
-
-        return $this->render("OjsInstallerBundle:Default:configure.html.twig", array('data' => $data));
+        }
+        foreach($formDataFile['parameters'] as $key=>$value){
+            $setter = 'set'.join('',array_map(function($s){return ucfirst($s);},explode('_',$key)));
+            $formData->{$setter}($value);
+        }
+        $form = $this->createForm(new ConfigType(),$formData,[]);
+        $data['form'] = $form->createView();
+        return $this->render("OjsInstallerBundle:Default:configure.html.twig", $data);
     }
 
 }
