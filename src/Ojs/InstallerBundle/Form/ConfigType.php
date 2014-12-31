e0670e56be02aed34ef6cd5a887d8b5461728daa
Aybars Cengaver <aybarscengaver@gmail.com>
2014-12-31 15:58:29 +0200

    ConfigType and Config entity forgetten.

---
+++ src/Ojs/InstallerBundle/Form/ConfigType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Ojs\InstallerBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ConfigType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('database_driver')
+            ->add('database_host')
+            ->add('database_port')
+            ->add('database_name')
+            ->add('database_user')
+            ->add('database_password')
+            ->add('system_email')
+            ->add('mailer_transport')
+            ->add('mailer_host')
+            ->add('mailer_user')
+            ->add('mailer_password')
+            ->add('locale')
+            ->add('secret')
+            ->add('base_host')
+            ->add('post_types')
+            ->add('elasticsearch_host')
+            ->add('mongodb_host')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Ojs\InstallerBundle\Entity\Config'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ojs_installerbundle_config';
+    }
+}
