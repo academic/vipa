808fbeb39a069a39ec1458d3fa51a4b7b3442c6b
Aybars Cengaver <aybarscengaver@gmail.com>
2015-01-01 17:38:10 +0200

    Installer Setup page completed. related #256

--- src/Ojs/InstallerBundle/Controller/ConfigController.php
+++ src/Ojs/InstallerBundle/Controller/ConfigController.php
@@ -5,38 +5,38 @@ namespace Ojs\InstallerBundle\Controller;
 use Ojs\InstallerBundle\Entity\Config;
 use Ojs\InstallerBundle\Form\ConfigType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\DependencyInjection\Dumper\YamlDumper;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Yaml\Dumper;
 use Symfony\Component\Yaml\Parser;
-use Unifik\DatabaseConfigBundle\Entity\Parameter;
-use Doctrine\Common\Persistence\ObjectManager;
 
-class ConfigController extends Controller {
+class ConfigController extends Controller
+{
 
-    public function configureAction(Request $request) {
-        $data=[];
+    public function configureAction(Request $request)
+    {
+        $data = [];
         $data['data']['page'] = 'config';
-        $parametersFile = __DIR__.'/../../../../app/config/parameters.yml';
-        $parametersFileDist = $parametersFile.'.dist';
+        $parametersFile = __DIR__ . '/../../../../app/config/parameters.yml';
+        $parametersFileDist = $parametersFile . '.dist';
         $formData = new Config();
         $parser = new Parser();
-        if(file_exists($parametersFile)){
+        if (file_exists($parametersFile)) {
             $formDataFile = $parser->parse(file_get_contents($parametersFile));
-        }else{
+        } else {
             $formDataFile = $parser->parse(file_get_contents($parametersFileDist));
 
         }
-        foreach($formDataFile['parameters'] as $key=>$value){
-            $setter = 'set'.join('',array_map(function($s){return ucfirst($s);},explode('_',$key)));
+        foreach ($formDataFile['parameters'] as $key => $value) {
+            $setter = 'set' . join('', array_map(function ($s) {
+                    return ucfirst($s);
+                }, explode('_', $key)));
             $formData->{$setter}($value);
         }
-        $form = $this->createForm(new ConfigType(),$formData,[
-            'method'=>'post',
-            'action'=>$this->get('router')->generate('ojs_installer_save_configure')
+        $form = $this->createForm(new ConfigType(), $formData, [
+            'method' => 'post',
+            'action' => $this->get('router')->generate('ojs_installer_save_configure')
         ]);
         $data['form'] = $form->createView();
         return $this->render("OjsInstallerBundle:Default:configure.html.twig", $data);
@@ -45,18 +45,18 @@ class ConfigController extends Controller {
     public function saveAction(Request $request)
     {
         $entity = new Config();
-        $form = $this->createForm(new ConfigType(),$entity,[
-            'method'=>'post',
-            'action'=>$this->get('router')->generate('ojs_installer_save_configure')
+        $form = $this->createForm(new ConfigType(), $entity, [
+            'method' => 'post',
+            'action' => $this->get('router')->generate('ojs_installer_save_configure')
         ]);
         $form->handleRequest($request);
         if ($form->isValid()) {
 
             $dumper = new Dumper();
-            $yaml = $dumper->dump($entity->toArray(),2,0);
-            $parametersFile = __DIR__.'/../../../../app/config/parameters.yml';
+            $yaml = $dumper->dump($entity->toArray(), 2, 0);
+            $parametersFile = __DIR__ . '/../../../../app/config/parameters.yml';
             $fs = new Filesystem();
-            $fs->dumpFile($parametersFile,$yaml);
+            $fs->dumpFile($parametersFile, $yaml);
             return new RedirectResponse('/install/setup');
         }
 
