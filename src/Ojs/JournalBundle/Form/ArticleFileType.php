0fca425eb176334626c7ad1ac109c2aed9400748
Aybars Cengaver <aybarscengaver@gmail.com>
2015-01-03 21:07:43 +0200

    Edit completed #265

--- src/Ojs/JournalBundle/Form/ArticleFileType.php
+++ src/Ojs/JournalBundle/Form/ArticleFileType.php
@@ -2,7 +2,10 @@
 
 namespace Ojs\JournalBundle\Form;
 
+use Doctrine\ORM\EntityRepository;
 use Ojs\Common\Params\ArticleFileParams;
+use Ojs\UserBundle\Entity\Role;
+use Ojs\UserBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
@@ -15,6 +18,7 @@ class ArticleFileType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $user = $options['user'];
         $builder
             ->add('title')
             ->add('keywords')
@@ -22,11 +26,31 @@ class ArticleFileType extends AbstractType
             ->add('type','choice',[
                 'choices'=>ArticleFileParams::$FILE_TYPES
             ])
-            ->add('article','entity')
+            ->add('article', 'entity', array(
+                'attr' => array('class' => ' form-control select2'),
+                'class' => 'Ojs\JournalBundle\Entity\Article',
+                'query_builder' => function(EntityRepository $er)use($user) {
+                    /** @var User $user $qb */
+                    $qb = $er->createQueryBuilder('a');
+                    foreach ($user->getRoles() as $role) {
+                        /** @var Role $role */
+                        if ($role->getRole() == 'ROLE_SUPER_ADMIN') {
+                            return $qb;
+                            break;
+                        }
+                    }
+
+                    $qb
+                        ->join('a.journal.userRoles', 'user_role', 'WITH', 'user_role.user=:user')
+                        ->setParameter('user', $user)
+                    ;
+                    return $qb;
+                }
+            )
+            )
             ->add('langCode')
             ->add('version')
             ->add('article')
-            ->add('file')
         ;
     }
     
@@ -36,7 +60,8 @@ class ArticleFileType extends AbstractType
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'Ojs\JournalBundle\Entity\ArticleFile'
+            'data_class' => 'Ojs\JournalBundle\Entity\ArticleFile',
+            'user'=> null 
         ));
     }
 
