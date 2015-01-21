312b487ea5c194c07f42d346f52ff22530ab63cd
hasantayyar <tayyar.besik@gmail.com>
2015-01-21 15:17:25 +0200

    ReviewFormRepositıry should be empty for now

--- src/Ojs/WorkflowBundle/Repository/ReviewFormRepository.php
+++ src/Ojs/WorkflowBundle/Repository/ReviewFormRepository.php
@@ -10,41 +10,5 @@ use Doctrine\ODM\MongoDB\DocumentRepository;
  */
 class ReviewFormRepository extends DocumentRepository
 {
-    /**
-     *
-     * $firstStep = $this->get('doctrine_mongodb')
-     *  ->getManager()
-     *  ->getRepository('OjsWorkflowBundle:JournalWorkflowStep')
-     *  ->findFirstStep();
-     *
-     * @return \Ojs\WorkflowBundle\Document\JournalWorkflowStep
-     */
-    public function findFirstStep()
-    {
-        return $this->createQueryBuilder()
-                        ->field('firststep')->equals(true)
-                        ->getQuery()
-                        ->getSingleResult();
-    }
-
-    /**
-     *
-     * @return \Ojs\WorkflowBundle\Document\JournalWorkflowStep
-     */
-    public function findLastStepStep()
-    {
-        return $this->createQueryBuilder()
-                        ->field('lastStep')->equals(true)
-                        ->getQuery()
-                        ->getSingleResult();
-    }
-
-    public function findAllOrderedByTitle()
-    {
-        return $this->createQueryBuilder()
-                        ->sort('title', 'ASC')
-                        ->getQuery()
-                        ->execute();
-    }
-
+    
 }
