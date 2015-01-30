42d20748687973f51b50f6a50d8639dc433562da
Hüseyin Mert <huseyinin@gmail.com>
2015-01-30 17:54:12 +0200

    second workflow template loaded #70

--- src/Ojs/WorkflowBundle/DataFixtures/MongoDB/LoadWorkflowTemplateData.php
+++ src/Ojs/WorkflowBundle/DataFixtures/MongoDB/LoadWorkflowTemplateData.php
@@ -80,7 +80,7 @@ class LoadWorkflowTemplateData extends AbstractFixture implements FixtureInterfa
         $step3->setLaststep(true);
         $step3->setStatus('Ready to publish');
         $step3->setTitle('Publish');
-        $step1->setRoles(array(
+        $step3->setRoles(array(
             json_decode($serializer->serialize($roleEditor, 'json')),
             json_decode($serializer->serialize($roleJournalManager, 'json'))
         ));
@@ -128,7 +128,91 @@ class LoadWorkflowTemplateData extends AbstractFixture implements FixtureInterfa
      */
     private function insertSecondTemplate($dm)
     {
-        
+        $roleRepo = $this->container->get('doctrine')->getRepository('OjsUserBundle:Role');
+        $roleEditor = $roleRepo->findOneByRole('ROLE_EDITOR');
+        $roleAuthor = $roleRepo->findOneByRole('ROLE_AUTHOR');
+        $roleJournalManager = $roleRepo->findOneByRole('ROLE_JOURNAL_MANAGER');
+        $roleReviewer = $roleRepo->findOneByRole('ROLE_REVIEWER');
+        $serializer = $this->container->get('serializer');
+
+        $step1 = new JournalWorkflowTemplateStep();
+        $step1->setFirststep(true);
+        $step1->setStatus('Editor is reviewing');
+        $step1->setTitle('Editor Review');
+        $step1->setRoles(array(
+            json_decode($serializer->serialize($roleEditor, 'json')),
+            json_decode($serializer->serialize($roleJournalManager, 'json'))
+        ));
+
+        $step2 = new JournalWorkflowTemplateStep();
+        $step2->setStatus('Author is updating');
+        $step2->setTitle('Author Revision');
+        $step2->setRoles(array(
+            json_decode($serializer->serialize($roleAuthor, 'json')),
+            json_decode($serializer->serialize($roleEditor, 'json')),
+            json_decode($serializer->serialize($roleJournalManager, 'json'))
+        ));
+
+
+        $step3 = new JournalWorkflowTemplateStep();
+        $step3->setOnlyreply(true);
+        $step3->setStatus('In Review');
+        $step3->setTitle('Review');
+        $step3->setRoles(array(
+            json_decode($serializer->serialize($roleReviewer, 'json')),
+            json_decode($serializer->serialize($roleEditor, 'json')),
+            json_decode($serializer->serialize($roleJournalManager, 'json'))
+        ));
+
+
+        $step4 = new JournalWorkflowTemplateStep();
+        $step4->setLaststep(true);
+        $step4->setStatus('Ready to publish');
+        $step4->setTitle('Publish');
+        $step4->setRoles(array(
+            json_decode($serializer->serialize($roleEditor, 'json')),
+            json_decode($serializer->serialize($roleJournalManager, 'json'))
+        ));
+
+        $dm->persist($step1);
+        $dm->persist($step2);
+        $dm->persist($step3);
+        $dm->persist($step4);
+
+        $step1->setNextsteps(
+                array(
+                    array('id' => $step2->getId(), 'title' => $step2->gettitle()),
+                    array('id' => $step3->getId(), 'title' => $step3->gettitle()),
+                    array('id' => $step4->getId(), 'title' => $step4->gettitle())
+                )
+        );
+        $step2->setNextsteps(
+                array(
+                    array('id' => $step1->getId(), 'title' => $step1->gettitle())
+                )
+        );
+
+        $step4->setNextsteps(
+                array(
+                    array('id' => $step1->getId(), 'title' => $step1->gettitle()),
+                    array('id' => $step2->getId(), 'title' => $step2->gettitle())
+                )
+        );
+
+        $dm->persist($step1);
+        $dm->persist($step2);
+        $dm->persist($step3);
+        $dm->persist($step4);
+
+        $dm->flush();
+
+        $template = new JournalWorkflowTemplate();
+        $template->setTitle('Basic Academic Workflow');
+        $template->setFirstNode($step1);
+
+        $dm->persist($template);
+        $dm->flush();
+        return $template;
     }
 
     /**
