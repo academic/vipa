1717cbaa22a85ad0fb4e70a8727ce31f539a8428
Hüseyin Mert <huseyinin@gmail.com>
2015-02-02 15:46:17 +0200

    workflow template use action (unfinished)

--- src/Ojs/WorkflowBundle/Controller/WorkflowTemplateController.php
+++ src/Ojs/WorkflowBundle/Controller/WorkflowTemplateController.php
@@ -30,8 +30,40 @@ class WorkflowTemplateController extends \Ojs\Common\Controller\OjsController
                 ->field('template.$id')
                 ->equals(new \MongoId($template->getId()))
                 ->getQuery()
-                ->execute(); 
+                ->execute();
         return $this->render('OjsWorkflowBundle:WorkflowStep:template.html.twig', array('template' => $template, 'steps' => $steps));
+    }
+
+    /**
+     * 
+     * @param string $id template document id
+     * @return RedirectResponse
+     */
+    public function useAction($id)
+    {
+        $selectedJournal = $this->get("ojs.journal_service")->getSelectedJournal();
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $template = $dm->getRepository('OjsWorkflowBundle:JournalWorkflowTemplate')->find($id);
+
+        // remove old steps!
+        /**
+         * @todo
+         * check each step and move all articles in review to first node
+         */
+        $collection = $dm->getDocumentCollection('OjsWorkflowBundle:JournalWorkflowStep');
+        $collection->remove(array('journalid' => $selectedJournal->getId()));
+
+        $steps = $dm->getRepository('OjsWorkflowBundle:JournalWorkflowTemplateStep')->createQueryBuilder()
+                ->field('template.$id')
+                ->equals(new \MongoId($template->getId()))
+                ->getQuery()
+                ->execute();
+
+        /**
+         * @todo
+         * clone new steps and relate them 
+         */
+        return $this->redirect($this->generateUrl('ojs_workflow_homepage'));
     }
 
 }
