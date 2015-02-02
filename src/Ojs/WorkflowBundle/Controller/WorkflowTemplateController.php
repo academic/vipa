3e9d6b6e5f2918ad18a3c2914aeb90badb200a61
hasantayyar <tayyar.besik@gmail.com>
2015-02-02 12:10:12 +0200

    Workflow template detail pages

--- src/Ojs/WorkflowBundle/Controller/WorkflowTemplateController.php
+++ src/Ojs/WorkflowBundle/Controller/WorkflowTemplateController.php
@@ -8,22 +8,30 @@ class WorkflowTemplateController extends \Ojs\Common\Controller\OjsController
 {
 
     /**
-     * 
      * @return Response
      */
     public function indexAction()
     {
-        $templates = $this->get('doctrine_mongodb')
-                        ->getRepository('OjsWorkflowBundle:JournalWorkflowTemplate')->findAll();
+        $templates = $this->get('doctrine_mongodb')->getRepository('OjsWorkflowBundle:JournalWorkflowTemplate')->findAll();
         return $this->render('OjsWorkflowBundle:WorkflowStep:templates.html.twig', array('templates' => $templates));
     }
 
+    /**
+     * 
+     * @param string $id template document id
+     * @return Response
+     */
     public function showAction($id)
     {
         $dm = $this->get('doctrine_mongodb')->getManager();
         $template = $dm->getRepository('OjsWorkflowBundle:JournalWorkflowTemplate')->find($id);
 
-        return $this->render('OjsWorkflowBundle:WorkflowStep:template.html.twig', array('template' => $template));
+        $steps = $dm->getRepository('OjsWorkflowBundle:JournalWorkflowTemplateStep')->createQueryBuilder()
+                ->field('template.$id')
+                ->equals(new \MongoId($template->getId()))
+                ->getQuery()
+                ->execute(); 
+        return $this->render('OjsWorkflowBundle:WorkflowStep:template.html.twig', array('template' => $template, 'steps' => $steps));
     }
 
 }
