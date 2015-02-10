9d00b592a05adb394095399ba37a8d1a3c086a00
aybarscengaver <aybarscengaver@gmail.com>
2015-02-10 03:07:12 +0200

    related #318 institute detail admin. / Journal list admin.

--- src/Ojs/JournalBundle/Controller/SuggestionController.php
+++ src/Ojs/JournalBundle/Controller/SuggestionController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Ojs\Common\Controller\OjsController as Controller;
 use Ojs\JournalBundle\Entity\Institution;
 use Ojs\JournalBundle\Form\InstitutionType;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * Institution controller.
@@ -21,12 +22,73 @@ class SuggestionController extends Controller
     public function instituteAction()
     {
         $dm = $this->get('doctrine.odm.mongodb.document_manager');
-        $entities = $dm->getRepository('OjsJournalBundle:InstituteSuggestion')->findBy(['merged'=>null]);
+        $entities = $dm->getRepository('OjsJournalBundle:InstituteSuggestion')->findBy(['merged' => null]);
         return $this->render('OjsJournalBundle:Suggestion:institute.html.twig', array(
             'entities' => $entities,
         ));
     }
 
+    public function journalAction()
+    {
+        $dm = $this->get('doctrine.odm.mongodb.document_manager');
+        $entities = $dm->getRepository('OjsJournalBundle:JournalSuggestion')->findBy(['merged' => null]);
+        return $this->render('OjsJournalBundle:Suggestion:journal.html.twig', array(
+            'entities' => $entities,
+        ));
+    }
+
+    public function journalDetailAction($id)
+    {
+        $dm = $this->get('doctrine.odm.mongodb.document_manager');
+        $entity = $dm->find('OjsJournalBundle:JournalSuggestion', $id);
+        if (!$entity) {
+            throw new NotFoundHttpException;
+        }
+        $data = [];
+        $data['entity'] = $entity;
+
+        return $this->render('OjsJournalBundle:Suggestion:institute_detail.html.twig', $data);
+    }
+
+    public function instituteDetailAction($id)
+    {
+        $dm = $this->get('doctrine.odm.mongodb.document_manager');
+        $entity = $dm->find('OjsJournalBundle:InstituteSuggestion', $id);
+        if (!$entity) {
+            throw new NotFoundHttpException;
+        }
+
+        $data = [];
+        $data['entity'] = $entity;
+
+        return $this->render('OjsJournalBundle:Suggestion:institute_detail.html.twig', $data);
+
+    }
+
+    public function journalDeleteAction($id)
+    {
+
+        $dm = $this->get('doctrine.odm.mongodb.document_manager');
+        $entity = $dm->find('OjsJournalBundle:JournalSuggestion', $id);
+        if (!$entity) {
+            throw new NotFoundHttpException;
+        }
+        $dm->remove($entity);
+        $dm->flush();
+        return $this->redirectToRoute('journal_suggestion');
+    }
+
+    public function instituteDeleteAction($id)
+    {
+        $dm = $this->get('doctrine.odm.mongodb.document_manager');
+        $entity = $dm->find('OjsJournalBundle:InstituteSuggestion', $id);
+        if (!$entity) {
+            throw new NotFoundHttpException;
+        }
+        $dm->remove($entity);
+        $dm->flush();
+        return $this->redirectToRoute('institute_suggestion');
+    }
 
 
 }
