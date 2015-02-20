6a20214c5c5bbe9a18e90f11fea63893c2cb3545
behram çelen <behramcelen@gmail.com>
2015-02-20 19:41:38 +0200

    new user register event log to event log

--- src/Ojs/UserBundle/Listeners/UserListener.php
+++ src/Ojs/UserBundle/Listeners/UserListener.php
@@ -1,13 +1,41 @@
 <?php
-/** 
- * Date: 26.01.15
- * Time: 20:44
- */
-
 namespace Ojs\UserBundle\Listeners;
 
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Ojs\UserBundle\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class UserListener {
+class UserListener
+{
+    protected $container;
 
+    public function __construct(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
 
+    /**
+     * Every new user log to event log
+     * @param LifecycleEventArgs|Request $args
+     * @link http://docs.doctrine-project.org/en/latest/reference/events.html#postupdate-postremove-postpersist
+     * @return Response never null
+     */
+    public function postPersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        $entityManager = $args->getEntityManager();
+
+        // perhaps you only want to act on some "User" entity
+        if ($entity instanceof User) {
+
+            //log as eventlog
+            $event = new \Ojs\UserBundle\Entity\EventLog();
+            $event->setEventInfo(\Ojs\Common\Params\UserEventLogParams::$USER_ADD);
+            $event->setIp($this->container->get('request')->getClientIp());
+            $event->setUserId($entity->getId());
+            $entityManager->persist($event);
+
+            $entityManager->flush();
+        }
+    }
 }
