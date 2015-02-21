6af397db9944423538f8504c764fb0a5e7a33973
behram çelen <behramcelen@gmail.com>
2015-02-21 23:53:04 +0200

    add event log flush system for admin,translate addition

--- src/Ojs/UserBundle/Controller/EventLogController.php
+++ src/Ojs/UserBundle/Controller/EventLogController.php
@@ -2,6 +2,7 @@
 
 namespace Ojs\UserBundle\Controller;
 
+use GuzzleHttp\Subscriber\Redirect;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Ojs\UserBundle\Entity\EventLog;
 
@@ -42,4 +43,32 @@ class EventLogController extends Controller
                     'entity' => $entity));
     }
 
+    /**
+     * Removes all EventLog records
+     *
+     * @return mixed
+     */
+    public function flushAction(){
+
+        /**
+         * All entities delete. Function not truncate table only removes all entry, not resets FOREIGN_KEY.
+         *
+         * Later this event can be log another super user log table.
+         *
+         * For Truncating you can use this(http://stackoverflow.com/a/9710383/2438520) link
+         */
+        $em = $this->getDoctrine()->getManager();
+        $entities = $em->getRepository('OjsUserBundle:EventLog')->findAll();
+
+        foreach($entities as $entity){
+
+            $em->remove($entity);
+        }
+        $em->flush();
+
+        $this->get('session')->getFlashBag()->add('success', $this->get('translator')->trans('All records removed successfully!'));
+
+
+        return $this->redirect($this->generateUrl('eventlog'));
+    }
 }
