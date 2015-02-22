07a30858a49238dfa7e5bc7c1104320ac3ca34b8
behram çelen <behramcelen@gmail.com>
2015-02-22 02:30:21 +0200

    Article submission,remove event listener for activity log

---
+++ src/Ojs/Common/Params/ArticleEventLogParams.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ojs\Common\Params;
+
+class ArticleEventLogParams
+{
+
+    public static $ARTICLE_SUBMISSION = 'ARTICLE_SUBMISSION';
+    public static $ARTICLE_UPDATE = 'ARTICLE_UPDATE';
+    public static $ARTICLE_REMOVE = 'ARTICLE_REMOVE';
+    public static $ARTICLE_APPROVE = 'ARTICLE_APPROVE';
+}
