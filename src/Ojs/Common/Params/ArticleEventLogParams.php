238b3f45676741cd1350d3031aa0eea45b1c79c0
behram çelen <behramcelen@gmail.com>
2015-02-25 14:42:27 +0200

    activity log for every user by own role

--- src/Ojs/Common/Params/ArticleEventLogParams.php
+++ src/Ojs/Common/Params/ArticleEventLogParams.php
@@ -9,4 +9,5 @@ class ArticleEventLogParams
     public static $ARTICLE_UPDATE = 'ARTICLE_UPDATE';
     public static $ARTICLE_REMOVE = 'ARTICLE_REMOVE';
     public static $ARTICLE_APPROVE = 'ARTICLE_APPROVE';
+
 }
