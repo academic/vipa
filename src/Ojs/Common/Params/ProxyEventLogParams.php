918fa9d080f08823ca0d1815bfb6d6a958a70815
behram çelen <behramcelen@gmail.com>
2015-02-25 14:42:27 +0200

    activity log for every user by own role

---
+++ src/Ojs/Common/Params/ProxyEventLogParams.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ojs\Common\Params;
+
+class ProxyEventLogParams
+{
+
+    public static $PROXY_CREATE = 'PROXY_CREATE_EVENT';
+    public static $PROXY_DROP = 'PROXY_DROP_EVENT';
+
+}
