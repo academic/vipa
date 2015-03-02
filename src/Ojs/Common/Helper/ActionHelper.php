faab50fecc845f4f7432045a0e39efd77167ae30
aybarscengaver <aybarscengaver@gmail.com>
2015-03-02 14:29:50 +0200

    anonymlogin added to actionhelpers

--- src/Ojs/Common/Helper/ActionHelper.php
+++ src/Ojs/Common/Helper/ActionHelper.php
@@ -15,26 +15,27 @@ use Symfony\Bundle\FrameworkBundle\Routing\Router;
 
 class ActionHelper
 {
-    public static function deleteAction($route,$key)
+    public static function deleteAction($route, $key)
     {
-        $rowAction = new RowAction("",$route);
+        $rowAction = new RowAction("", $route);
         $rowAction->setTitle('<i class="fa fa-trash-o"></i>');
-        $rowAction->setAttributes(['class'=>'btn btn-danger btn-xs  ']);
+        $rowAction->setAttributes(['class' => 'btn btn-danger btn-xs  ']);
         $rowAction->setRouteParameters($key);
         $rowAction->setConfirm(true);
         $rowAction->setConfirmMessage("Do you want delete this row?");
         return $rowAction;
     }
+
     public static function userBanAction()
     {
-        $rowAction = new RowAction("",'user_block');
+        $rowAction = new RowAction("", 'user_block');
         $rowAction->setTitle('<i class="fa fa-ban"></i>');
-        $rowAction->setAttributes(['class'=>'btn btn-warning btn-xs  ']);
+        $rowAction->setAttributes(['class' => 'btn btn-warning btn-xs  ']);
         $rowAction->setRouteParameters('id');
         $rowAction->setConfirm(true);
         $rowAction->setConfirmMessage("Do you want ban this user?");
-        $rowAction->manipulateRender(function(RowAction $action, Row $row){
-            if(!$row->getField('status')){
+        $rowAction->manipulateRender(function (RowAction $action, Row $row) {
+            if (!$row->getField('status')) {
                 $action->setRoute('user_unblock');
                 $action->setTitle('<i class="fa fa-check"></i>');
                 $action->setConfirmMessage("Do you want unban this user?");
@@ -46,30 +47,50 @@ class ActionHelper
 
     public static function switchUserAction($route, $key)
     {
-        $rowAction = new RowAction("",$route);
+        $rowAction = new RowAction("", $route);
         $rowAction->setTitle('<i class="fa fa-sign-in"></i>');
-        $rowAction->setAttributes(['class'=>'btn btn-info btn-xs  ']);
+        $rowAction->setAttributes(['class' => 'btn btn-info btn-xs  ']);
         $rowAction->setRouteParameters($key);
-        $rowAction->setRouteParametersMapping(['username'=>'_su']);
+        $rowAction->setRouteParametersMapping(['username' => '_su']);
         $rowAction->setRole('ROLE_SUPER_ADMIN');
         return $rowAction;
     }
 
     public static function showAction($route, $key)
     {
-        $rowAction = new RowAction("",$route);
+        $rowAction = new RowAction("", $route);
         $rowAction->setTitle('<i class="fa fa-info-circle"></i>');
-        $rowAction->setAttributes(['class'=>'btn btn-success btn-xs  ']);
+        $rowAction->setAttributes(['class' => 'btn btn-success btn-xs  ']);
         $rowAction->setRouteParameters($key);
         return $rowAction;
     }
 
     public static function editAction($route, $key)
     {
-        $rowAction = new RowAction("",$route);
+        $rowAction = new RowAction("", $route);
         $rowAction->setTitle('<i class="fa fa-pencil"></i>');
-        $rowAction->setAttributes(['class'=>'btn btn-warning btn-xs  ']);
+        $rowAction->setAttributes(['class' => 'btn btn-warning btn-xs  ']);
         $rowAction->setRouteParameters($key);
+        return $rowAction;
+    }
+
+    public static  function userAnonymLoginAction()
+    {
+        global $kernel;
+        $route = 'user_create_anonym_login';
+
+        $rowAction = new RowAction("", $route);
+        $rowAction->setTitle('<i class="fa fa-users"></i>');
+        $rowAction->setAttributes(['class' => 'btn btn-warning btn-xs  ']);
+        $postExtension = $kernel->getContainer()->get('okulbilisimcmsbundle.twig.post_extension');
+        $rowAction->setRouteParameters(['id', 'object']);
+        $rowAction->setRoute($route);
+        $rowAction->manipulateRender(function (RowAction $action, Row $row) use ($postExtension) {
+            $entity = $row->getEntity();
+            $object = $postExtension->cmsobject($entity);
+            $action->setRouteParameters(['id','object'=>$object]);
+            return $action;
+        });
         return $rowAction;
     }
 } 
