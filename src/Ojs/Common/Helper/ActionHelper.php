26245c00db05a5be15fc38ae24f0e3f2f458c15d
aybarscengaver <aybarscengaver@gmail.com>
2015-03-02 13:42:57 +0200

    ActionHelper for action buttons on grid bundle.

--- src/Ojs/Common/Helper/ActionHelper.php
+++ src/Ojs/Common/Helper/ActionHelper.php
@@ -10,24 +10,66 @@ namespace Ojs\Common\Helper;
 
 
 use APY\DataGridBundle\Grid\Action\RowAction;
+use APY\DataGridBundle\Grid\Row;
 use Symfony\Bundle\FrameworkBundle\Routing\Router;
 
 class ActionHelper
 {
     public static function deleteAction($route,$key)
     {
-        global $kernel;
-        $container = $kernel->getContainer();
-        /** @var Router $router */
-        $html = <<<HTML
-             <i class="fa fa-trash-o"></i>
-HTML;
         $rowAction = new RowAction("",$route);
-        $rowAction->setTitle($html);
+        $rowAction->setTitle('<i class="fa fa-trash-o"></i>');
         $rowAction->setAttributes(['class'=>'btn btn-danger btn-xs  ']);
         $rowAction->setRouteParameters($key);
         $rowAction->setConfirm(true);
         $rowAction->setConfirmMessage("Do you want delete this row?");
+        return $rowAction;
+    }
+    public static function userBanAction()
+    {
+        $rowAction = new RowAction("",'user_block');
+        $rowAction->setTitle('<i class="fa fa-ban"></i>');
+        $rowAction->setAttributes(['class'=>'btn btn-warning btn-xs  ']);
+        $rowAction->setRouteParameters('id');
+        $rowAction->setConfirm(true);
+        $rowAction->setConfirmMessage("Do you want ban this user?");
+        $rowAction->manipulateRender(function(RowAction $action, Row $row){
+            if(!$row->getField('status')){
+                $action->setRoute('user_unblock');
+                $action->setTitle('<i class="fa fa-check"></i>');
+                $action->setConfirmMessage("Do you want unban this user?");
+            }
+            return $action;
+        });
+        return $rowAction;
+    }
+
+    public static function switchUserAction($route, $key)
+    {
+        $rowAction = new RowAction("",$route);
+        $rowAction->setTitle('<i class="fa fa-sign-in"></i>');
+        $rowAction->setAttributes(['class'=>'btn btn-info btn-xs  ']);
+        $rowAction->setRouteParameters($key);
+        $rowAction->setRouteParametersMapping(['username'=>'_su']);
+        $rowAction->setRole('ROLE_SUPER_ADMIN');
+        return $rowAction;
+    }
+
+    public static function showAction($route, $key)
+    {
+        $rowAction = new RowAction("",$route);
+        $rowAction->setTitle('<i class="fa fa-info-circle"></i>');
+        $rowAction->setAttributes(['class'=>'btn btn-success btn-xs  ']);
+        $rowAction->setRouteParameters($key);
+        return $rowAction;
+    }
+
+    public static function editAction($route, $key)
+    {
+        $rowAction = new RowAction("",$route);
+        $rowAction->setTitle('<i class="fa fa-pencil"></i>');
+        $rowAction->setAttributes(['class'=>'btn btn-warning btn-xs  ']);
+        $rowAction->setRouteParameters($key);
         return $rowAction;
     }
 } 
