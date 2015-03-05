5f6e97548002ca89deb66f0a13b71f7cad19b8a9
Hasan Tayyar BESIK <tayyar.besik@gmail.com>
2015-03-05 17:23:55 +0200

    fix htmltree of subject list - format subjectcontroller - edit subject view

--- src/Ojs/JournalBundle/Controller/SubjectController.php
+++ src/Ojs/JournalBundle/Controller/SubjectController.php
@@ -35,161 +35,154 @@ class SubjectController extends Controller {
         $data['grid'] = $grid;
         $repo = $this->getDoctrine()->
                 getRepository('OjsJournalBundle:Subject');
-        $options = array(
+        $options =  array(
             'decorate' => true,
             'rootOpen' => '<ul>',
             'rootClose' => '</ul>',
-            'childOpen' => '<ul><li>',
-            'childClose' => '</li></ul>',
+            'childOpen' => '<li>',
+            'childClose' => '</li>',
             'idField' => true,
             'nodeDecorator' => function ($node) {
                 return '<a href="' . $this->generateUrl('subject_show', array('id' => $node['id'])) . '">' . $node['subject'] . '</a>';
-            }
-                );
-                $data['htmlTree'] = $repo->childrenHierarchy(
-                        null
-                        , false
-                        , $options
-                );
+            });
+        $data['htmlTree'] = $repo->childrenHierarchy(null, false, $options);
 
+        return $grid->getGridResponse('OjsJournalBundle:Subject:index.html.twig', $data);
+    }
 
-                return $grid->getGridResponse('OjsJournalBundle:Subject:index.html.twig', $data);
-            }
+    /**
+     * Creates a new Subject entity.
+     *
+     */
+    public function createAction(Request $request) {
+        $entity = new Subject();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity->setTranslatableLocale($request->getLocale());
+            $em->persist($entity);
+            $em->flush();
 
-            /**
-             * Creates a new Subject entity.
-             *
-             */
-            public function createAction(Request $request) {
-                $entity = new Subject();
-                $form = $this->createCreateForm($entity);
-                $form->handleRequest($request);
-                if ($form->isValid()) {
-                    $em = $this->getDoctrine()->getManager();
-                    $entity->setTranslatableLocale($request->getLocale());
-                    $em->persist($entity);
-                    $em->flush();
-
-                    return $this->redirect($this->generateUrl('subject_show', array('id' => $entity->getId())));
-                }
-
-                return $this->render('OjsJournalBundle:Subject:new.html.twig', array(
-                            'entity' => $entity,
-                            'form' => $form->createView(),
-                ));
-            }
-
-            /**
-             * Creates a form to create a Subject entity.
-             *
-             * @param Subject $entity The entity
-             *
-             * @return \Symfony\Component\Form\Form The form
-             */
-            private function createCreateForm(Subject $entity) {
-                $form = $this->createForm(new SubjectType(), $entity, array(
-                    'action' => $this->generateUrl('subject_create'),
-                    'method' => 'POST',
-                ));
-                $form->add('submit', 'submit', array('label' => 'Create'));
-
-                return $form;
-            }
-
-            /**
-             * Displays a form to create a new Subject entity.
-             *
-             */
-            public function newAction() {
-                $entity = new Subject();
-                $form = $this->createCreateForm($entity);
-
-                return $this->render('OjsJournalBundle:Subject:new.html.twig', array(
-                            'entity' => $entity,
-                            'form' => $form->createView(),
-                ));
-            }
-
-            /**
-             * Finds and displays a Subject entity.
-             *
-             */
-            public function showAction($id) {
-                $em = $this->getDoctrine()->getManager();
-                $entity = $em->getRepository('OjsJournalBundle:Subject')->find($id);
-                $this->throw404IfNotFound($entity);
-                return $this->render('OjsJournalBundle:Subject:show.html.twig', array(
-                            'entity' => $entity,));
-            }
-
-            /**
-             * Displays a form to edit an existing Subject entity.
-             *
-             */
-            public function editAction($id) {
-                $em = $this->getDoctrine()->getManager();
-                $entity = $em->getRepository('OjsJournalBundle:Subject')->find($id);
-                $this->throw404IfNotFound($entity);
-                $editForm = $this->createEditForm($entity);
-                return $this->render('OjsJournalBundle:Subject:edit.html.twig', array(
-                            'entity' => $entity,
-                            'edit_form' => $editForm->createView(),
-                ));
-            }
-
-            /**
-             * Creates a form to edit a Subject entity.
-             *
-             * @param Subject $entity The entity
-             *
-             * @return \Symfony\Component\Form\Form The form
-             */
-            private function createEditForm(Subject $entity) {
-                $form = $this->createForm(new SubjectType(), $entity, array(
-                    'action' => $this->generateUrl('subject_update', array('id' => $entity->getId())),
-                    'method' => 'PUT',
-                ));
-                $form->add('submit', 'submit', array('label' => 'Update'));
-
-                return $form;
-            }
-
-            /**
-             * Edits an existing Subject entity.
-             *
-             */
-            public function updateAction(Request $request, $id) {
-                $em = $this->getDoctrine()->getManager();
-                $entity = $em->getRepository('OjsJournalBundle:Subject')->find($id);
-                $this->throw404IfNotFound($entity);
-                $editForm = $this->createEditForm($entity);
-                $editForm->handleRequest($request);
-                if ($editForm->isValid()) {
-                    $em->flush();
-
-                    return $this->redirect($this->generateUrl('subject_edit', array('id' => $id)));
-                }
-
-                return $this->render('OjsJournalBundle:Subject:edit.html.twig', array(
-                            'entity' => $entity,
-                            'edit_form' => $editForm->createView(),
-                ));
-            }
-
-            /**
-             * Deletes a Subject entity.
-             *
-             */
-            public function deleteAction(Request $request, $id) {
-
-                $em = $this->getDoctrine()->getManager();
-                $entity = $em->getRepository('OjsJournalBundle:Subject')->find($id);
-                $this->throw404IfNotFound($entity);
-                $em->remove($entity);
-                $em->flush();
-
-                return $this->redirect($this->generateUrl('subject'));
-            }
-
+            return $this->redirect($this->generateUrl('subject_show', array('id' => $entity->getId())));
         }
-        
+
+        return $this->render('OjsJournalBundle:Subject:new.html.twig', array(
+                    'entity' => $entity,
+                    'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Creates a form to create a Subject entity.
+     *
+     * @param Subject $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Subject $entity) {
+        $form = $this->createForm(new SubjectType(), $entity, array(
+            'action' => $this->generateUrl('subject_create'),
+            'method' => 'POST',
+        ));
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Subject entity.
+     *
+     */
+    public function newAction() {
+        $entity = new Subject();
+        $form = $this->createCreateForm($entity);
+
+        return $this->render('OjsJournalBundle:Subject:new.html.twig', array(
+                    'entity' => $entity,
+                    'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Subject entity.
+     *
+     */
+    public function showAction($id) {
+        $em = $this->getDoctrine()->getManager();
+        $entity = $em->getRepository('OjsJournalBundle:Subject')->find($id);
+        $this->throw404IfNotFound($entity);
+        return $this->render('OjsJournalBundle:Subject:show.html.twig', array(
+                    'entity' => $entity,));
+    }
+
+    /**
+     * Displays a form to edit an existing Subject entity.
+     *
+     */
+    public function editAction($id) {
+        $em = $this->getDoctrine()->getManager();
+        $entity = $em->getRepository('OjsJournalBundle:Subject')->find($id);
+        $this->throw404IfNotFound($entity);
+        $editForm = $this->createEditForm($entity);
+        return $this->render('OjsJournalBundle:Subject:edit.html.twig', array(
+                    'entity' => $entity,
+                    'edit_form' => $editForm->createView(),
+        ));
+    }
+
+    /**
+     * Creates a form to edit a Subject entity.
+     *
+     * @param Subject $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createEditForm(Subject $entity) {
+        $form = $this->createForm(new SubjectType(), $entity, array(
+            'action' => $this->generateUrl('subject_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+
+    /**
+     * Edits an existing Subject entity.
+     *
+     */
+    public function updateAction(Request $request, $id) {
+        $em = $this->getDoctrine()->getManager();
+        $entity = $em->getRepository('OjsJournalBundle:Subject')->find($id);
+        $this->throw404IfNotFound($entity);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('subject_edit', array('id' => $id)));
+        }
+
+        return $this->render('OjsJournalBundle:Subject:edit.html.twig', array(
+                    'entity' => $entity,
+                    'edit_form' => $editForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Subject entity.
+     *
+     */
+    public function deleteAction(Request $request, $id) {
+
+        $em = $this->getDoctrine()->getManager();
+        $entity = $em->getRepository('OjsJournalBundle:Subject')->find($id);
+        $this->throw404IfNotFound($entity);
+        $em->remove($entity);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('subject'));
+    }
+
+}
