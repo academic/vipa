c8a3b04d227908b0971292f983dbd180c017daef
Hüseyin Mert <huseyinin@gmail.com>
2015-03-06 10:30:00 +0200

    remove tabbed language view #375

--- src/Ojs/JournalBundle/Resources/public/js/article_submission.js
+++ src/Ojs/JournalBundle/Resources/public/js/article_submission.js
@@ -37,12 +37,6 @@ var OjsArticleSubmission = {
     hideStep: function (step) {
         $("#step" + step + "-container").slideUp("fast", 200, "easeInBack");
     },
-    activateFirstLanguageTab: function () {
-        firsttab = $("#step1 .tab-pane").first().attr('id');
-        if (firsttab) {
-            $("li.lang a[href=#" + firsttab + "]").tab("show");
-        }
-    },
     addAuthorForm: function (params) {
         $("#step2").append(Mustache.render($("#step2_tpl").html(), params));
     },
@@ -53,48 +47,9 @@ var OjsArticleSubmission = {
     removeAuthor: function ($el) {
         $el.parents(".author-item").first().remove();
     },
-    step1AddLanguageForm: function (langcode, langtitle, params) {
-        // check if selected language tab already exists
-        if (OjsCommon.inArray(langcode, OjsArticleSubmission.languages)) {
-            return false;
-        }
-        $tpl = Mustache.render($('#step1_tpl').html(), params);
-        $("#step1").append('<div class="tab-pane step1" id="' + langcode + '">' + $tpl);
-
-        OjsArticleSubmission.languages.push(langcode);
-        $("div#" + langcode + " textarea.editor").wysihtml5({
-            toolbar: {"font-styles": false, "emphasis": true, "lists": false, "html": false, "link": true, "image": false, "color": false, "blockquote": true}
-        });
-        tabhtml = '<li class="lang" id="t_' + langcode + '"><a href="#' +
-                langcode + '" role="tab" class="lang" data-toggle="tab">' +
-                langtitle + '<span class="removelang btn btn-sm btn-default"><i class="fa fa-trash-o"></i>' +
-                '</span></a></li>';
-        $("ul#mainTabs li.lang").last().before(tabhtml);
-        OjsArticleSubmission.activateFirstLanguageTab();
-        OjsArticleSubmission.setupUi();
-    },
-    step1RemoveLanguageForm: function (langcode, $tab) {
-        check = confirm("Are you sure to remove this language tab?");
-        if (!check) {
-            return false;
-        }
-        for (var i in this.languages) {
-            if (this.languages[i] === langcode) {
-                this.languages.splice(i, 1);
-            }
-        }
-        $tab.remove();
-        $('#t_' + langcode).remove();
-        this.activateFirstLanguageTab();
-    },
     step1: function (actionUrl) {
-        forms = $("#step1 .tab-pane");
-        if (forms.length === 0) {
-            OjsCommon.errorModal("Add at least one language to your submission.");
-            return false;
-        }
+        forms = $("#step1");
         $primaryLang = $("select[name=primaryLanguage] option:selected").val();
-
         // prepare post params
         articleParams = false;
         translationParams = [];
@@ -264,33 +219,33 @@ var OjsArticleSubmission = {
     setupUi: function () {
         this.bindFileUploader();
         $primaryLang = $("select[name=primaryLanguage] option:selected").val();
-        OjsArticleSubmission.step1AddLanguageForm($primaryLang, $("#langitem_" + $primaryLang).attr("lang"));
         $('input[name=keywords], input[name=subjects]').tagsinput({
             tagClass: 'label label-info'
         });
-
         $('#changeSelectedJournal').on("select2-selecting", function (e) {
             window.location.href = "" + e.val;
-        })
+        });
         $('.select2-element').select2({placeholder: '', allowClear: true, closeOnSelect: false});
+        $("div.editor").wysihtml5({
+            toolbar: {
+                "font-styles": false,
+                "emphasis": true,
+                "lists": false,
+                "html": false,
+                "link": true,
+                "image": false,
+                "color": false,
+                "blockquote": true}
+        });
+    },
+    step1ScrollToLangPanel: function (lang) {
+        $('html, body').animate({
+            scrollTop: $("#lang_"+lang).offset().top
+        }, 500);
     }
 };
 
 $(document).ready(function () {
     OjsArticleSubmission.setupUi();
-    $("ul#mainTabs li a").click(function (e) {
-        e.preventDefault();
-    });
-    $("ul#languageDropList li a").click(function (e) {
-        e.preventDefault();
-        langcode = $(this).attr('code');
-        langtitle = $(this).attr('lang');
-        OjsArticleSubmission.step1AddLanguageForm(langcode, langtitle);
-    });
-    $("body").on("click", "span.removelang", function (e) {
-        e.preventDefault();
-        langcode = $(this).parent().attr("href").replace("#", "");
-        $tab = $("#" + langcode);
-        OjsArticleSubmission.step1RemoveLanguageForm(langcode, $tab);
-    });
+
 });
