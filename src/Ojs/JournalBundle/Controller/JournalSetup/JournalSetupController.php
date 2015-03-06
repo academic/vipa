95b1d38714d15db2e7501a071ec53b47641e5454
behram çelen <behramcelen@gmail.com>
2015-03-06 15:27:56 +0200

    journal setup wizard create routes,classes

---
+++ src/Ojs/JournalBundle/Controller/JournalSetup/JournalSetupController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ojs\JournalBundle\Controller\JournalSetup;
+
+use Ojs\Common\Controller\OjsController as Controller;
+
+/**
+ * Journal Setup Wizard controller.
+ *
+ */
+class JournalSetupController extends Controller
+{
+    /**
+     *
+     */
+    public function indexAction(){
+
+    }
+
+    /**
+     *
+     */
+    public function resumeAction(){
+
+    }
+
+    /**
+     *
+     */
+    public function previewAction(){
+
+    }
+
+    /**
+     *
+     */
+    public function finishction(){
+
+    }
+}
---
+++ src/Ojs/JournalBundle/Controller/JournalSetup/JournalSetupStep5Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Ojs\JournalBundle\Controller\JournalSetup;
+
+use Ojs\Common\Controller\OjsController as Controller;
+
+/**
+ * Journal Setup Wizard Step controller.
+ *
+ */
+class JournalSetupStep5Controller extends Controller
+{
+    /**
+     *
+     */
+    public function addIndexAction(){
+
+    }
+}
