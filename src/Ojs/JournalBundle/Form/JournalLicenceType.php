ae1ad7378eb1628c8ca3f5a40008bd2a56d4a581
aybarscengaver <aybarscengaver@gmail.com>
2015-03-10 12:52:07 +0200

    Journal Licence crud generate & modify #361

---
+++ src/Ojs/JournalBundle/Form/JournalLicenceType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Ojs\JournalBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Class JournalLicenceType
+ * @package Ojs\JournalBundle\Form
+ */
+class JournalLicenceType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('label')
+            ->add('licence')
+            ->add('visible')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Ojs\JournalBundle\Entity\JournalLicence'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ojs_journalbundle_journallicence';
+    }
+}
