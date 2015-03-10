1b7d2873c560e70700f3a21cd2111ec27c98b2fb
Hüseyin Mert <huseyinin@gmail.com>
2015-03-10 14:07:57 +0200

    New workflow (pre draft) #376

--- src/Ojs/WorkflowBundle/DataFixtures/MongoDB/LoadWorkflowTemplateData.php
+++ src/Ojs/WorkflowBundle/DataFixtures/MongoDB/LoadWorkflowTemplateData.php
@@ -35,7 +35,7 @@ class LoadWorkflowTemplateData extends AbstractFixture implements FixtureInterfa
     {
         $em = $this->container->get('doctrine')->getManager();
         $journal = $em->createQuery('SELECT c FROM OjsJournalBundle:Journal c')
-                        ->setMaxResults(1)->getResult();
+            ->setMaxResults(1)->getResult();
         if (!isset($journal)) {
             return;
         }
@@ -43,10 +43,12 @@ class LoadWorkflowTemplateData extends AbstractFixture implements FixtureInterfa
         $this->insertFirstTemplate($dm);
         $this->insertSecondTemplate($dm);
         $this->insertThirdTemplate($dm);
+        $this->insertOJSTemplate($dm);
+        $this->insertAdvancedEditorTemplate($dm);
     }
 
     /**
-     * 
+     *
      * @param mixed $dm doctrine mongodb manager
      */
     private function insertFirstTemplate($dm)
@@ -97,22 +99,22 @@ class LoadWorkflowTemplateData extends AbstractFixture implements FixtureInterfa
         $dm->persist($step2);
         $dm->persist($step3);
         $step1->setNextsteps(
-                array(
-                    array('id' => $step2->getId(), 'title' => $step2->gettitle()),
-                    array('id' => $step3->getId(), 'title' => $step3->gettitle())
-                )
+            array(
+                array('id' => $step2->getId(), 'title' => $step2->gettitle()),
+                array('id' => $step3->getId(), 'title' => $step3->gettitle())
+            )
         );
         $step2->setNextsteps(
-                array(
-                    array('id' => $step1->getId(), 'title' => $step1->gettitle())
-                )
+            array(
+                array('id' => $step1->getId(), 'title' => $step1->gettitle())
+            )
         );
 
         $step3->setNextsteps(
-                array(
-                    array('id' => $step1->getId(), 'title' => $step1->gettitle()),
-                    array('id' => $step2->getId(), 'title' => $step2->gettitle())
-                )
+            array(
+                array('id' => $step1->getId(), 'title' => $step1->gettitle()),
+                array('id' => $step2->getId(), 'title' => $step2->gettitle())
+            )
         );
 
         $dm->persist($step1);
@@ -191,18 +193,18 @@ class LoadWorkflowTemplateData extends AbstractFixture implements FixtureInterfa
         $dm->persist($step4);
 
         $step1->setNextsteps(
-                array(
-                    array('id' => $step2->getId(), 'title' => $step2->gettitle()),
-                    array('id' => $step3->getId(), 'title' => $step3->gettitle()),
-                    array('id' => $step4->getId(), 'title' => $step4->gettitle())
-                )
+            array(
+                array('id' => $step2->getId(), 'title' => $step2->gettitle()),
+                array('id' => $step3->getId(), 'title' => $step3->gettitle()),
+                array('id' => $step4->getId(), 'title' => $step4->gettitle())
+            )
         );
 
         $step4->setNextsteps(
-                array(
-                    array('id' => $step1->getId(), 'title' => $step1->gettitle()),
-                    array('id' => $step2->getId(), 'title' => $step2->gettitle())
-                )
+            array(
+                array('id' => $step1->getId(), 'title' => $step1->gettitle()),
+                array('id' => $step2->getId(), 'title' => $step2->gettitle())
+            )
         );
 
         $dm->persist($step1);
@@ -303,30 +305,282 @@ class LoadWorkflowTemplateData extends AbstractFixture implements FixtureInterfa
 
 
         $step1->setNextsteps(
-                array(
-                    array('id' => $step2->getId(), 'title' => $step2->gettitle()),
-                    array('id' => $step3->getId(), 'title' => $step3->gettitle()),
-                    array('id' => $step4->getId(), 'title' => $step4->gettitle()),
-                    array('id' => $step5->getId(), 'title' => $step5->gettitle())
-                )
+            array(
+                array('id' => $step2->getId(), 'title' => $step2->gettitle()),
+                array('id' => $step3->getId(), 'title' => $step3->gettitle()),
+                array('id' => $step4->getId(), 'title' => $step4->gettitle()),
+                array('id' => $step5->getId(), 'title' => $step5->gettitle())
+            )
         );
 
         $step3->setNextsteps(
-                array(
-                    array('id' => $step1->getId(), 'title' => $step1->gettitle())
-                )
+            array(
+                array('id' => $step1->getId(), 'title' => $step1->gettitle())
+            )
         );
 
         $step4->setNextsteps(
-                array(
-                    array('id' => $step1->getId(), 'title' => $step1->gettitle())
-                )
+            array(
+                array('id' => $step1->getId(), 'title' => $step1->gettitle())
+            )
         );
 
         $step6->setNextsteps(
-                array(
-                    array('id' => $step1->getId(), 'title' => $step1->gettitle())
-                )
+            array(
+                array('id' => $step1->getId(), 'title' => $step1->gettitle())
+            )
+        );
+
+        $dm->persist($step1);
+        $dm->persist($step2);
+        $dm->persist($step3);
+        $dm->persist($step4);
+        $dm->persist($step5);
+        $dm->persist($step6);
+
+        $dm->flush();
+        return $template;
+    }
+
+    /**
+     * @TODO
+     * @param type $dm
+     */
+    private function insertOJSTemplate($dm)
+    {
+        $roleRepo = $this->container->get('doctrine')->getRepository('OjsUserBundle:Role');
+        $roleEditor = $roleRepo->findOneByRole('ROLE_EDITOR');
+        $roleAuthor = $roleRepo->findOneByRole('ROLE_AUTHOR');
+        $roleJournalManager = $roleRepo->findOneByRole('ROLE_JOURNAL_MANAGER');
+        $roleReviewer = $roleRepo->findOneByRole('ROLE_REVIEWER');
+        $serializer = $this->container->get('serializer');
+
+        $template = new JournalWorkflowTemplate();
+        $template->setTitle('OJS Workflow');
+        $template->setDescription('PKP/OJS workflow.');
+
+        $dm->persist($template);
+
+        $step1 = new JournalWorkflowTemplateStep();
+        $step1->setFirststep(true);
+        $step1->setStatus('Editor is reviewing');
+        $step1->setTitle('Editor Review');
+        $step1->setTemplate($template);
+        $step1->setRoles(array(
+            json_decode($serializer->serialize($roleEditor, 'json')),
+            json_decode($serializer->serialize($roleJournalManager, 'json'))
+        ));
+
+        $step2 = new JournalWorkflowTemplateStep();
+        $step2->setStatus('Author is updating');
+        $step2->setTitle('Author Revision');
+        $step2->setOnlyreply(true);
+        $step2->setTemplate($template);
+        $step2->setRoles(array(
+            json_decode($serializer->serialize($roleAuthor, 'json')),
+            json_decode($serializer->serialize($roleEditor, 'json')),
+            json_decode($serializer->serialize($roleJournalManager, 'json'))
+        ));
+
+        $step3 = new JournalWorkflowTemplateStep();
+        $step3->setStatus('Redaction');
+        $step3->setTitle('Redaction');
+        $step3->setTemplate($template);
+        $step3->setRoles(array(
+            json_decode($serializer->serialize($roleEditor, 'json')),
+            json_decode($serializer->serialize($roleJournalManager, 'json'))
+        ));
+
+        $step4 = new JournalWorkflowTemplateStep();
+        $step4->setStatus('Language Review');
+        $step4->setTitle('Language Review');
+        $step4->setTemplate($template);
+        $step4->setRoles(array(
+            json_decode($serializer->serialize($roleEditor, 'json')),
+            json_decode($serializer->serialize($roleJournalManager, 'json'))
+        ));
+
+
+        $step5 = new JournalWorkflowTemplateStep();
+        $step5->setOnlyreply(true);
+        $step5->setStatus('In Review');
+        $step5->setTitle('Review');
+        $step5->setTemplate($template);
+        $step5->setRoles(array(
+            json_decode($serializer->serialize($roleReviewer, 'json')),
+            json_decode($serializer->serialize($roleEditor, 'json')),
+            json_decode($serializer->serialize($roleJournalManager, 'json'))
+        ));
+
+
+        $step6 = new JournalWorkflowTemplateStep();
+        $step6->setLaststep(true);
+        $step6->setStatus('Ready to publish');
+        $step6->setTitle('Publish');
+        $step6->setRoles(array(
+            json_decode($serializer->serialize($roleEditor, 'json')),
+            json_decode($serializer->serialize($roleJournalManager, 'json'))
+        ));
+
+        $dm->persist($step1);
+        $dm->persist($step2);
+        $dm->persist($step3);
+        $dm->persist($step4);
+        $dm->persist($step5);
+        $dm->persist($step6);
+
+
+        $step1->setNextsteps(
+            array(
+                array('id' => $step2->getId(), 'title' => $step2->gettitle()),
+                array('id' => $step3->getId(), 'title' => $step3->gettitle()),
+                array('id' => $step4->getId(), 'title' => $step4->gettitle()),
+                array('id' => $step5->getId(), 'title' => $step5->gettitle())
+            )
+        );
+
+        $step3->setNextsteps(
+            array(
+                array('id' => $step1->getId(), 'title' => $step1->gettitle())
+            )
+        );
+
+        $step4->setNextsteps(
+            array(
+                array('id' => $step1->getId(), 'title' => $step1->gettitle())
+            )
+        );
+
+        $step6->setNextsteps(
+            array(
+                array('id' => $step1->getId(), 'title' => $step1->gettitle())
+            )
+        );
+
+        $dm->persist($step1);
+        $dm->persist($step2);
+        $dm->persist($step3);
+        $dm->persist($step4);
+        $dm->persist($step5);
+        $dm->persist($step6);
+
+        $dm->flush();
+        return $template;
+    }
+
+    /**
+     * @TODO
+     * @param type $dm
+     */
+    private function insertAdvancedEditorTemplate($dm)
+    {
+        $roleRepo = $this->container->get('doctrine')->getRepository('OjsUserBundle:Role');
+        $roleEditor = $roleRepo->findOneByRole('ROLE_EDITOR');
+        $roleAuthor = $roleRepo->findOneByRole('ROLE_AUTHOR');
+        $roleJournalManager = $roleRepo->findOneByRole('ROLE_JOURNAL_MANAGER');
+        $roleReviewer = $roleRepo->findOneByRole('ROLE_REVIEWER');
+        $serializer = $this->container->get('serializer');
+
+        $template = new JournalWorkflowTemplate();
+        $template->setTitle('AdvancedEditor Workflow');
+        $template->setDescription('Academic Workflow with extra steps. Contains "Redaction" and "Language Review" steps.');
+
+        $dm->persist($template);
+
+        $step1 = new JournalWorkflowTemplateStep();
+        $step1->setFirststep(true);
+        $step1->setStatus('Editor is reviewing');
+        $step1->setTitle('Editor Review');
+        $step1->setTemplate($template);
+        $step1->setRoles(array(
+            json_decode($serializer->serialize($roleEditor, 'json')),
+            json_decode($serializer->serialize($roleJournalManager, 'json'))
+        ));
+
+        $step2 = new JournalWorkflowTemplateStep();
+        $step2->setStatus('Author is updating');
+        $step2->setTitle('Author Revision');
+        $step2->setOnlyreply(true);
+        $step2->setTemplate($template);
+        $step2->setRoles(array(
+            json_decode($serializer->serialize($roleAuthor, 'json')),
+            json_decode($serializer->serialize($roleEditor, 'json')),
+            json_decode($serializer->serialize($roleJournalManager, 'json'))
+        ));
+
+        $step3 = new JournalWorkflowTemplateStep();
+        $step3->setStatus('Redaction');
+        $step3->setTitle('Redaction');
+        $step3->setTemplate($template);
+        $step3->setRoles(array(
+            json_decode($serializer->serialize($roleEditor, 'json')),
+            json_decode($serializer->serialize($roleJournalManager, 'json'))
+        ));
+
+        $step4 = new JournalWorkflowTemplateStep();
+        $step4->setStatus('Language Review');
+        $step4->setTitle('Language Review');
+        $step4->setTemplate($template);
+        $step4->setRoles(array(
+            json_decode($serializer->serialize($roleEditor, 'json')),
+            json_decode($serializer->serialize($roleJournalManager, 'json'))
+        ));
+
+
+        $step5 = new JournalWorkflowTemplateStep();
+        $step5->setOnlyreply(true);
+        $step5->setStatus('In Review');
+        $step5->setTitle('Review');
+        $step5->setTemplate($template);
+        $step5->setRoles(array(
+            json_decode($serializer->serialize($roleReviewer, 'json')),
+            json_decode($serializer->serialize($roleEditor, 'json')),
+            json_decode($serializer->serialize($roleJournalManager, 'json'))
+        ));
+
+
+        $step6 = new JournalWorkflowTemplateStep();
+        $step6->setLaststep(true);
+        $step6->setStatus('Ready to publish');
+        $step6->setTitle('Publish');
+        $step6->setRoles(array(
+            json_decode($serializer->serialize($roleEditor, 'json')),
+            json_decode($serializer->serialize($roleJournalManager, 'json'))
+        ));
+
+        $dm->persist($step1);
+        $dm->persist($step2);
+        $dm->persist($step3);
+        $dm->persist($step4);
+        $dm->persist($step5);
+        $dm->persist($step6);
+
+
+        $step1->setNextsteps(
+            array(
+                array('id' => $step2->getId(), 'title' => $step2->gettitle()),
+                array('id' => $step3->getId(), 'title' => $step3->gettitle()),
+                array('id' => $step4->getId(), 'title' => $step4->gettitle()),
+                array('id' => $step5->getId(), 'title' => $step5->gettitle())
+            )
+        );
+
+        $step3->setNextsteps(
+            array(
+                array('id' => $step1->getId(), 'title' => $step1->gettitle())
+            )
+        );
+
+        $step4->setNextsteps(
+            array(
+                array('id' => $step1->getId(), 'title' => $step1->gettitle())
+            )
+        );
+
+        $step6->setNextsteps(
+            array(
+                array('id' => $step1->getId(), 'title' => $step1->gettitle())
+            )
         );
 
         $dm->persist($step1);
