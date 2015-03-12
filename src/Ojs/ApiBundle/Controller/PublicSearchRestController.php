26fa045c3ba8f40439316a8ba575004206d9b21e
Hüseyin Mert <huseyinin@gmail.com>
2015-03-12 22:31:21 +0200

    Intitution suggester added to submission form for Authors
    - Author and Instiution entities updates -  **requires `doctrine:scheme:update --force`**
    - article_submission.js updated  -  **requires  `app/console assetic:dump`**
    - elastica configuration changed : Added new fields for institution map

--- src/Ojs/ApiBundle/Controller/PublicSearchRestController.php
+++ src/Ojs/ApiBundle/Controller/PublicSearchRestController.php
@@ -8,9 +8,9 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\View as RestView;
- 
-use Elastica\Query;   
+use Elastica\Query;
 use FOS\ElasticaBundle\Doctrine\ORM\ElasticaToModelTransformer;
+use Symfony\Component\HttpFoundation\Request;
 
 class PublicSearchRestController extends FOSRestController {
 
@@ -18,22 +18,48 @@ class PublicSearchRestController extends FOSRestController {
      *
      * @ApiDoc(
      *  resource=true,
-     *  description="search Institutions"
+     *  description="search Institutions",
+     *  parameters={
+     * {
+     *          "name"="q",
+     *          "dataType"="string",
+     *          "required"="true",
+     *          "description"="search term"
+     *      },
+     *      {
+     *          "name"="verified",
+     *          "dataType"="boolean",
+     *          "required"="false",
+     *          "description"="list only verified or not"
+     *      },
+     *      {
+     *          "name"="page",
+     *          "dataType"="integer",
+     *          "required"="false",
+     *          "description"="limit"
+     *      }
+     *  }
      * )
-     * @Get("/public/search/institution/{q}/{page}/{limit}")
+     * @Get("/public/search/institution")
      */
-    public function getInstitutionsAction($q, $page = 0, $limit = 50)
+    public function getInstitutionsAction(Request $request )
     {
+        $limit = $request->get('limit');
+        $verified = $request->get('verified');
+        $q = $request->get('q');
         $search = $this->container->get('fos_elastica.index.search.institution');
         $query = new Query\Bool();
-        $should = new Query\Match();
-        $should->setField('name', $q);  
-        $query->addShould($should); 
-         $should2 = new Query\Match();
-        $should2->setField('tags', $q);
-        $query->addShould($should2); 
+        $must1 = new Query\Match();
+        $must1->setField('name', $q);
+        $query->addMust($must1); 
+ 
+        if ($verified !== null) {
+            $must2 = new Query\Match();
+            $must2->setField('verified', $verified);
+            $query->addMust($must2);
+        }
         $results = $search->search($query);
-       $data = [];
+        $data = [];
         foreach ($results as $result) {
             $data[] = array_merge(array('id' => $result->getId()), $result->getData());
         }
