4014a64634d2a7255588f4e8b21d0ff46c3da436
Hasan Tayyar BESIK <tayyar.besik@gmail.com>
2015-03-19 16:42:19 +0200

    journal setup ui fixes

--- src/Ojs/JournalBundle/Resources/public/js/tour/journal_setup_tour.js
+++ src/Ojs/JournalBundle/Resources/public/js/tour/journal_setup_tour.js
@@ -1,9 +1,9 @@
 var tour = new Tour({
         steps: [
             {
-                element: "#article-submission-steps",
-                title: "Submission steps",
-                content: "You can watch your article submission proccess",
+                element: "#journal-setup-steps",
+                title: "Journal Setup Steps",
+                content: "You can watch your setup proccess",
                 placement: "bottom"
             }
         ]}
