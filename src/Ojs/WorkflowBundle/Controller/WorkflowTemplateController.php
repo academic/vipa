baa724026a0e716ac49a9d9e560bd067d1fad33f
behram çelen <behramcelen@gmail.com>
2015-03-20 06:50:52 +0200

    create workflow template controllers #406

--- src/Ojs/WorkflowBundle/Controller/WorkflowTemplateController.php
+++ src/Ojs/WorkflowBundle/Controller/WorkflowTemplateController.php
@@ -2,41 +2,63 @@
 
 namespace Ojs\WorkflowBundle\Controller;
 
+use Ojs\WorkflowBundle\Form\TemplateType;
+use Ojs\WorkflowBundle\Document\JournalWorkflowTemplate;
 use \Symfony\Component\HttpFoundation\Request;
+use Ojs\WorkflowBundle\Document\JournalWorkflowTemplateStep;
 
-class WorkflowTemplateController extends \Ojs\Common\Controller\OjsController {
+class WorkflowTemplateController extends \Ojs\Common\Controller\OjsController
+{
 
     /**
      * @return Response
      */
-    public function indexAction() {
-        $templates = $this->get('doctrine_mongodb')->getRepository('OjsWorkflowBundle:JournalWorkflowTemplate')->findAll();
-        return $this->render('OjsWorkflowBundle:WorkflowStep:templates.html.twig', array('templates' => $templates));
+    public function indexAction()
+    {
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $selectedJournal = $this->get("ojs.journal_service")->getSelectedJournal();
+
+        /**
+         * Get system and journal's templates
+         * Query references below
+         * @link http://doctrine-mongodb-odm.readthedocs.org/en/latest/reference/query-builder-api.html#map-reduce
+         * @link http://docs.mongodb.org/manual/reference/operator/query/and/#op._S_and
+         */
+        $templates = $dm->getRepository('OjsWorkflowBundle:JournalWorkflowTemplate')->createQueryBuilder()
+            ->where("function() { return this.isSystemTemplate == true || this.journalId == ".$selectedJournal->getId()." }")
+            ->getQuery()
+            ->execute();
+
+        return $this->render('OjsWorkflowBundle:WorkflowStep:Template/templates.html.twig', array(
+            'templates' => $templates
+        ));
     }
 
     /**
-     * 
+     *
      * @param string $id template document id
      * @return Response
      */
-    public function showAction($id) {
+    public function showAction($id)
+    {
         $dm = $this->get('doctrine_mongodb')->getManager();
         $template = $dm->getRepository('OjsWorkflowBundle:JournalWorkflowTemplate')->find($id);
 
         $steps = $dm->getRepository('OjsWorkflowBundle:JournalWorkflowTemplateStep')->createQueryBuilder()
-                ->field('template.$id')
-                ->equals(new \MongoId($template->getId()))
-                ->getQuery()
-                ->execute();
-        return $this->render('OjsWorkflowBundle:WorkflowStep:template.html.twig', array('template' => $template, 'steps' => $steps));
+            ->field('template.$id')
+            ->equals(new \MongoId($template->getId()))
+            ->getQuery()
+            ->execute();
+        return $this->render('OjsWorkflowBundle:WorkflowStep:Template/template.html.twig', array('template' => $template, 'steps' => $steps));
     }
 
     /**
-     * 
+     *
      * @param string $id template document id
      * @return RedirectResponse
      */
-    public function useAction($id) {
+    public function useAction($id)
+    {
         $selectedJournal = $this->get("ojs.journal_service")->getSelectedJournal();
         $dm = $this->get('doctrine_mongodb')->getManager();
         $template = $dm->getRepository('OjsWorkflowBundle:JournalWorkflowTemplate')->find($id);
@@ -50,38 +72,39 @@ class WorkflowTemplateController extends \Ojs\Common\Controller\OjsController {
         $collection->remove(array('journalid' => $selectedJournal->getId()));
 
         $steps = $dm->getRepository('OjsWorkflowBundle:JournalWorkflowTemplateStep')->createQueryBuilder()
-                ->field('template.$id')
-                ->equals(new \MongoId($template->getId()))
-                ->getQuery()
-                ->execute();
+            ->field('template.$id')
+            ->equals(new \MongoId($template->getId()))
+            ->getQuery()
+            ->execute();
         $newSteps = [];
         foreach ($steps as $step) {
             $newSteps[$step->getId()] = $this->cloneStep($step, $newSteps);
         }
         // add nextstep relations
         foreach ($steps as $step) {
-            $entity = $newSteps[$step->getId()]; 
+            $entity = $newSteps[$step->getId()];
             $nextSteps = $step->getNextSteps();
             foreach ($nextSteps as $nStep) {
                 $entity->addNextStep($newSteps[$nStep->getId()]);
-            } 
+            }
             $dm->persist($entity);
             $dm->flush();
         }
         /**
          * @todo
-         * clone new steps and relate them 
+         * clone new steps and relate them
          */
         return $this->redirect($this->generateUrl('ojs_workflow_homepage'));
     }
 
     /**
-     * 
+     *
      * @param \Ojs\WorkflowBundle\Document\JournalWorkflowTemplateStep $tplStep
      * @param array $newSteps
      * @return \Ojs\WorkflowBundle\Document\JournalWorkflowStep
      */
-    protected function cloneStep(\Ojs\WorkflowBundle\Document\JournalWorkflowTemplateStep $tplStep, $newSteps) {
+    protected function cloneStep(\Ojs\WorkflowBundle\Document\JournalWorkflowTemplateStep $tplStep, $newSteps)
+    {
         if (in_array($tplStep->getId(), $newSteps)) {
             return $newSteps[$tplStep->getId()];
         }
@@ -107,6 +130,166 @@ class WorkflowTemplateController extends \Ojs\Common\Controller\OjsController {
         $dm->persist($step);
         $dm->flush();
         return $step;
+    }
+
+    /**
+     * New workflow template create
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function newAction()
+    {
+        $entity = new JournalWorkflowTemplate();
+
+        $form = $this->createForm(new TemplateType(), $entity, array(
+            'action' => $this->generateUrl('workflow_template_create'),
+            'method' => 'POST',
+        ));
+
+        return $this->render('OjsWorkflowBundle:WorkflowStep:Template/new.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    /**
+     * Insert template data that comes from post data
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function createAction(Request $request)
+    {
+        $selectedJournal = $this->get("ojs.journal_service")->getSelectedJournal();
+        $session = $request->getSession();
+        $entity = new JournalWorkflowTemplate();
+        $form = $this->createForm(new TemplateType(), $entity);
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $dm = $this->get('doctrine_mongodb')->getManager();
+            //Template created by user
+            $entity->setIsSystemTemplate(0);
+            $entity->setJournalId($selectedJournal->getId());
+            $dm->persist($entity);
+            $dm->flush();
+
+            //add success message
+            $session->getFlashBag()->add(
+                'success',
+                'Template created successfully. Now you can add steps'
+            );
+            return $this->redirect($this->generateUrl('workflow_template_new_step', array('templateId' => $entity->getId())));
+        }
+
+        //add error message
+        $session->getFlashBag()->add(
+            'error',
+            'Please recheck your submission'
+        );
+        return $this->redirect($this->generateUrl('workflow_template_new'));
+    }
+
+    /**
+     * add new step to created workflow template
+     * @param $templateId
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function newStepAction($templateId){
+
+        $selectedJournal = $this->get("ojs.journal_service")->getSelectedJournal();
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $template = $dm->getRepository('OjsWorkflowBundle:JournalWorkflowTemplate')->find($templateId);
+
+        $em = $this->getDoctrine();
+        $roles = $em->getRepository('OjsUserBundle:Role')->findAll();
+
+        $nextSteps = $dm->getRepository('OjsWorkflowBundle:JournalWorkflowTemplateStep')->createQueryBuilder()
+            ->field('template.$id')
+            ->equals(new \MongoId($template->getId()))
+            ->getQuery()
+            ->execute();
+
+        $journalReviewForms = $dm->getRepository('OjsWorkflowBundle:ReviewForm')->getJournalForms($selectedJournal->getId());
+
+        return $this->render('OjsWorkflowBundle:WorkflowStep/Template:new_step.html.twig', array(
+            'roles' => $roles,
+            'nextSteps' => $nextSteps,
+            'journal' => $selectedJournal,
+            'forms' => $journalReviewForms,
+            'template' => $template
+        ));
+    }
+
+    /**
+     * @param Request $request
+     * @param $templateId
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function createStepAction(Request $request, $templateId){
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $template = $dm->getRepository('OjsWorkflowBundle:JournalWorkflowTemplate')->find($templateId);
+        $step = new JournalWorkflowTemplateStep();
+        $step->setTemplate($template);
+        $step->setMaxdays($request->get('maxdays'));
+        $step->setFirststep($request->get('firstStep') ? true : false);
+        $step->setLaststep($request->get('lastStep') ? true : false);
+        $step->setJournalid($request->get('journalId'));
+        $step->setRoles($this->prepareRoles($request->get('roles')));
+        if(is_array($request->get('nextSteps')))
+            foreach($request->get('nextSteps') as $nId){
+                $nextStep = $dm->getRepository('OjsWorkflowBundle:JournalWorkflowTemplateStep')->find($nId);
+                $step->addNextStep($nextStep);
+            }
+        $step->setOnlyreply($request->get('onlyreply') ? true : false);
+        $step->setStatus($request->get('status'));
+        $step->setTitle($request->get('title'));
+        $step->setColor($request->get('color'));
+        $step->setIsVisible($request->get('isVisible') ? true : false);
+        $step->setMustBeAssigned($request->get('mustBeAssigned') ? true : false);
+        $step->setCanEdit($request->get('canEdit') ? true : false);
+        $step->setCanRejectSubmission($request->get('canRejectSubmission') ? true : false);
+        $step->setCanReview($request->get('canReview') ? true : false);
+        $step->setCanSeeAuthor($request->get('canSeeAuthor') ? true : false);
+        $reviewFormIds = $request->get('reviewforms');
+        if (!empty($reviewFormIds)) {
+            foreach ($reviewFormIds as $formId) {
+                $form = $dm->getRepository('OjsWorkflowBundle:ReviewForm')->find($formId);
+                $form && $step->addReviewForm($form);
+            }
+        }
+        $dm->persist($step);
+        $dm->flush();
+
+        return $this->redirect($this->generateUrl('workflow_template_show', array('id' => $templateId)));
+    }
+
+    public function showStepAction($stepId){
+        $step = $this->get('doctrine_mongodb')->getManager()
+            ->getRepository('OjsWorkflowBundle:JournalWorkflowTemplateStep')->find($stepId);
+
+        return $this->render('OjsWorkflowBundle:WorkflowStep/Template:show_step.html.twig', array('step' => $step));
+    }
+
+    /**
+     * prepare an array given form values for JournalWorkflowTemplate $roles atrribute
+     * @param  array $roleIds
+     * @return array
+     */
+    public function prepareRoles($roleIds) {
+        $serializer = $this->get('serializer');
+        $em = $this->get('doctrine')->getManager();
+        $roles = array();
+        $rolesArray = array();
+        if ($roleIds) {
+            foreach ($roleIds as $roleId) {
+                $roles[] = $em->getRepository("OjsUserBundle:Role")->findOneById($roleId);
+            }
+        }
+        if ($roles) {
+            foreach ($roles as $role) {
+                $rolesArray[] = json_decode(
+                    $serializer->serialize($role, 'json'));
+            }
+        }
+        return $rolesArray;
     }
 
 }
