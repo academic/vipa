c16d60eafcb2d114fe57c4c4f42c03dade759562
aybarscengaver <aybarscengaver@gmail.com>
2015-03-26 14:39:31 +0200

    Institution select .

--- src/Ojs/JournalBundle/Form/JournalType.php
+++ src/Ojs/JournalBundle/Form/JournalType.php
@@ -7,92 +7,99 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class JournalType extends AbstractType {
+class JournalType extends AbstractType
+{
 
     /**
      * @param FormBuilderInterface $builder
-     * @param array                $options
+     * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options) {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
         $builder
-                ->add('title')
-                ->add('titleAbbr')
-                ->add('titleTransliterated')
-                ->add('languages', 'entity', array(
+            ->add('title')
+            ->add('titleAbbr')
+            ->add('titleTransliterated')
+            ->add('institution',null,[
+                'attr'=>[
+                    'class'=>'select2-element'
+                ]
+            ])
+            ->add('languages', 'entity', array(
                     'class' => 'Ojs\JournalBundle\Entity\Lang',
                     'property' => 'name',
                     'multiple' => true,
                     'expanded' => false,
                     'required' => false
-                        )
                 )
-                ->add('subjects','entity',[
-                    'class'=>'Ojs\JournalBundle\Entity\Subject',
-                    'property'=>'subject',
-                    'multiple'=>true,
-                    'attr'=>[
-                        'class'=>'select2-element'
+            )
+            ->add('subjects', 'entity', [
+                'class' => 'Ojs\JournalBundle\Entity\Subject',
+                'property' => 'subject',
+                'multiple' => true,
+                'attr' => [
+                    'class' => 'select2-element'
+                ]
+            ])
+            ->add('submitRoles', 'entity', array(
+                    'class' => 'Ojs\UserBundle\Entity\Role',
+                    'property' => 'name',
+                    'multiple' => true,
+                    'expanded' => false,
+                    'required' => false,
+                    'attr' => [
+                        'class' => 'select2-element'
                     ]
-                ])
-                ->add('submitRoles', 'entity', array(
-                        'class' => 'Ojs\UserBundle\Entity\Role',
-                        'property' => 'name',
-                        'multiple' => true,
-                        'expanded' => false,
-                        'required' => false,
-                        'attr'=>[
-                            'class'=>'select2-element'
-                        ]
-                    )
                 )
-                ->add('subtitle')
-                ->add('path')
-                ->add('domain')
-                ->add('issn', 'text',array('label' => 'ISSN'))
-                ->add('eissn','text', array('label' => 'e-ISSN'))
-                ->add('firstPublishDate', 'datetime', array(
-                    'date_widget' => 'single_text',
-                    'time_widget' => 'single_text',
-                    'date_format' => 'yyyy-MM-dd',
-                    'with_seconds' => true,
-                    'data' => new \DateTime()
-                ))
-                ->add('period')
-                ->add('url')
-                ->add('country','entity',[
-                    'class'=>'Okulbilisim\LocationBundle\Entity\Country',
-                    'attr'=>[
-                        'class'=>'select2-element'
-                    ]
-                ])
-                ->add('published')
-                ->add('status','choice',[
-                    'choices'=>CommonParams::getStatusTexts()
-                ])
-                ->add('image', 'hidden')
-                ->add('header', 'hidden')
-                ->add('logo', 'hidden')
-                ->add('slug')
-                ->add('tags','text',['attr'=>['class'=>'select2-tags','data-role'=>'']])
-                ->add('description','textarea')
-                ->add('theme', 'entity', array(
+            )
+            ->add('subtitle')
+            ->add('path')
+            ->add('domain')
+            ->add('issn', 'text', array('label' => 'ISSN'))
+            ->add('eissn', 'text', array('label' => 'e-ISSN'))
+            ->add('firstPublishDate', 'datetime', array(
+                'date_widget' => 'single_text',
+                'time_widget' => 'single_text',
+                'date_format' => 'yyyy-MM-dd',
+                'with_seconds' => true,
+                'data' => new \DateTime()
+            ))
+            ->add('period')
+            ->add('url')
+            ->add('country', 'entity', [
+                'class' => 'Okulbilisim\LocationBundle\Entity\Country',
+                'attr' => [
+                    'class' => 'select2-element'
+                ]
+            ])
+            ->add('published')
+            ->add('status', 'choice', [
+                'choices' => CommonParams::getStatusTexts()
+            ])
+            ->add('image', 'hidden')
+            ->add('header', 'hidden')
+            ->add('logo', 'hidden')
+            ->add('slug')
+            ->add('tags', 'text', ['attr' => ['class' => 'select2-tags', 'data-role' => '']])
+            ->add('description', 'textarea')
+            ->add('theme', 'entity', array(
                     'class' => 'Ojs\JournalBundle\Entity\Theme',
                     'property' => 'title',
                     'multiple' => false,
                     'expanded' => false,
                     'required' => false,
-                    'query_builder' => function(\Doctrine\ORM\EntityRepository $er) {
+                    'query_builder' => function (\Doctrine\ORM\EntityRepository $er) {
                         return $er->createQueryBuilder('t')
-                                ->where('t.isPublic IS NULL OR t.isPublic = TRUE');
+                            ->where('t.isPublic IS NULL OR t.isPublic = TRUE');
                     })
-                )
-        ;
+            );
     }
 
     /**
      * @param OptionsResolverInterface $resolver
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
         $resolver->setDefaults(array(
             'data_class' => 'Ojs\JournalBundle\Entity\Journal'
         ));
@@ -101,7 +108,8 @@ class JournalType extends AbstractType {
     /**
      * @return string
      */
-    public function getName() {
+    public function getName()
+    {
         return 'ojs_journalbundle_journal';
     }
 
