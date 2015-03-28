9d62c4ea9b6596f61c4073572b401a12beaef0bb
behram çelen <behramcelen@gmail.com>
2015-03-28 03:25:48 +0200

    most viewed, downloaded article stats #414

--- src/Ojs/Common/Params/ArticleEventLogParams.php
+++ src/Ojs/Common/Params/ArticleEventLogParams.php
@@ -9,5 +9,7 @@ class ArticleEventLogParams
     public static $ARTICLE_UPDATE = 'ARTICLE_UPDATE';
     public static $ARTICLE_REMOVE = 'ARTICLE_REMOVE';
     public static $ARTICLE_APPROVE = 'ARTICLE_APPROVE';
+    public static $ARTICLE_VIEW = 'ARTICLE_VIEW';
+    public static $ARTICLE_DOWNLOAD = 'ARTICLE_DOWNLOAD';
 
 }
--- src/Ojs/ManagerBundle/Controller/EditorController.php
+++ src/Ojs/ManagerBundle/Controller/EditorController.php
@@ -3,6 +3,7 @@
 namespace Ojs\ManagerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use \DateTime;
 
 class EditorController extends Controller
 {
@@ -22,7 +23,7 @@ class EditorController extends Controller
     public function dashboardAction()
     {
         return $this->render('OjsManagerBundle:Editor:dashboard.html.twig',[
-            'counts' => $this->counts()
+            'stats' => $this->getStats()
         ]);
     }
 
@@ -48,23 +49,68 @@ class EditorController extends Controller
         return $this->render('OjsJournalBundle:Journal:role_based/show_editor.html.twig', array('entity' => $journal));
     }
 
-    private function counts()
+    /**
+     * Returns general stats;
+     * - Journal user count
+     * - Journal article count
+     * - Journal issue count
+     * - Last 30 day most viewed article
+     * - Last 30 day most viewed article view count
+     * - Last 30 day most downloaded article
+     * - Last 30 day most downloaded article download count
+     * @return mixed
+     */
+    private function getStats()
     {
         $em = $this->getDoctrine()->getEntityManager();
         $journal = $this->get("ojs.journal_service")->getSelectedJournal();
-        $counts['userCount'] = $em
+        $stats['userCount'] = $em
             ->createQuery('SELECT COUNT(a) FROM OjsUserBundle:UserJournalRole a WHERE a.journalId = :journal_id')
             ->setParameter('journal_id', $journal->getId())
             ->getSingleScalarResult();
-        $counts['articleCount'] = $em
+        $stats['articleCount'] = $em
             ->createQuery('SELECT COUNT(a) FROM OjsJournalBundle:Article a WHERE a.journalId = :journal_id')
             ->setParameter('journal_id', $journal->getId())
             ->getSingleScalarResult();
-        $counts['issueCount'] = $em
+        $stats['issueCount'] = $em
             ->createQuery('SELECT COUNT(a) FROM OjsJournalBundle:Issue a WHERE a.journalId = :journal_id')
             ->setParameter('journal_id', $journal->getId())
             ->getSingleScalarResult();
-        return $counts;
+
+        /**
+         * get most common value from article_event_log
+         * for query {@link http://stackoverflow.com/a/7693627/2438520}
+         * @todo query result can set session or memcache for more performance.
+         */
+        $now = new DateTime('-30 days');
+        $last30Day = $now->format("Y-m-d H:i:s");
+        $mostViewedArticleLog = $em
+            ->createQuery('SELECT a.articleId,COUNT(a) AS viewCount FROM OjsJournalBundle:ArticleEventLog a WHERE a.eventInfo = :event_info AND a.eventDate > :date GROUP BY a.articleId ORDER BY viewCount DESC')
+            ->setParameter('event_info', \Ojs\Common\Params\ArticleEventLogParams::$ARTICLE_VIEW)
+            ->setParameter('date', $last30Day)
+            ->setMaxResults(1)
+            ->getResult();
+        if(isset($mostViewedArticleLog[0])){
+            $stats['article']['mostViewedArticle'] = $em
+                ->getRepository('OjsJournalBundle:Article')
+                ->find($mostViewedArticleLog[0]['articleId']);
+            $stats['article']['mostViewedArticleCount'] = $mostViewedArticleLog[0]['viewCount'];
+        }
+
+        $mostDownloadedArticleLog = $em
+            ->createQuery('SELECT a.articleId,COUNT(a) AS downloadCount FROM OjsJournalBundle:ArticleEventLog a WHERE a.eventInfo = :event_info AND a.eventDate > :date GROUP BY a.articleId ORDER BY downloadCount DESC')
+            ->setParameter('event_info', \Ojs\Common\Params\ArticleEventLogParams::$ARTICLE_DOWNLOAD)
+            ->setParameter('date', $last30Day)
+            ->setMaxResults(1)
+            ->getResult();
+        if(isset($mostDownloadedArticleLog[0])){
+            $stats['article']['mostDownloadedArticle'] = $em
+                ->getRepository('OjsJournalBundle:Article')
+                ->find($mostDownloadedArticleLog[0]['articleId']);
+            $stats['article']['mostDownloadedArticleCount'] = $mostDownloadedArticleLog[0]['downloadCount'];
+        }
+
+        return $stats;
     }
 
 }
