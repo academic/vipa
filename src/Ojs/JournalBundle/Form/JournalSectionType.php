a5fda29144d7eb358b1d3e58be23af918a20bcb2
Hasan Tayyar BESIK <tayyar.besik@gmail.com>
2015-03-30 11:23:29 +0300

    fix #415
    - Journal section managements paths fixed
    - Journal section views liposuction

--- src/Ojs/JournalBundle/Form/JournalSectionType.php
+++ src/Ojs/JournalBundle/Form/JournalSectionType.php
@@ -6,27 +6,39 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class JournalSectionType extends AbstractType
-{
+class JournalSectionType extends AbstractType {
+
     /**
      * @param FormBuilderInterface $builder
      * @param array                $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $journal = $options['journal'];
+        $user = $options['user'];
         $builder
                 ->add('title')
                 ->add('allowIndex')
                 ->add('hideTitle')
                 ->add('journal', 'entity', array(
+                    'attr' => array('class' => 'select2-element'),
                     'class' => 'Ojs\JournalBundle\Entity\Journal',
-                    'property' => 'title',
-                    'multiple' => false,
-                    'expanded' => false,
-                    'required' => true,
-                        'attr'=>['class'=>'select2-element']
-                        )
-                )
+                    'query_builder' => function(\Doctrine\ORM\EntityRepository $er)use($user, $journal) {
+                $qb = $er->createQueryBuilder('j');
+                if ($user && !$user->hasRole('ROLE_SUPER_ADMIN')) {
+                    // if user is super admin get all journals
+                    $qb
+                    ->join('j.userRoles', 'user_role', 'WITH', 'user_role.user=:user')
+                    ->setParameter('user', $user);
+                }
+                if ($journal) {
+                    $qb
+                    ->where($qb->expr()->eq('j.id', ':journal'))
+                    ->setParameter('journal', $journal->getId());
+                }
+                return $qb;
+            }
+                ))
         ;
     }
 
@@ -36,7 +48,9 @@ class JournalSectionType extends AbstractType
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'Ojs\JournalBundle\Entity\JournalSection'
+            'data_class' => 'Ojs\JournalBundle\Entity\JournalSection',
+            'user' => null,
+            'journal' => null
         ));
     }
 
