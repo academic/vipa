5bd1b4438741a94191f20da98863df525d6fd4d3
aybarscengaver <aybarscengaver@gmail.com>
2015-04-09 16:16:27 +0300

    Default data.

--- src/Ojs/WorkflowBundle/DataFixtures/MongoDB/LoadWorkflowData.php
+++ src/Ojs/WorkflowBundle/DataFixtures/MongoDB/LoadWorkflowData.php
@@ -32,8 +32,7 @@ class LoadWorkflowData extends AbstractFixture implements FixtureInterface, Cont
     public function load(ObjectManager $dm)
     {
         $em = $this->container->get('doctrine')->getManager();
-        $journal = $em->createQuery('SELECT c FROM OjsJournalBundle:Journal c')
-                        ->setMaxResults(1)->getResult();
+        $journal = $em->getRepository("OjsJournalBundle:Journal")->find(1);
         if (!isset($journal)) {
             return;
         }
@@ -45,7 +44,7 @@ class LoadWorkflowData extends AbstractFixture implements FixtureInterface, Cont
 
         $step1 = new JournalWorkflowStep();
         $step1->setFirststep(true);
-        $step1->setJournalid($journal[0]->getId());
+        $step1->setJournalid($journal->getId());
         $step1->setMaxdays(15);
         $step1->setStatus('First Review');
         $step1->setTitle('First Review');
@@ -58,7 +57,7 @@ class LoadWorkflowData extends AbstractFixture implements FixtureInterface, Cont
 
         $step2 = new JournalWorkflowStep();
         $step2->setLaststep(true);
-        $step2->setJournalid($journal[0]->getId());
+        $step2->setJournalid($journal->getId());
         $step2->setMaxdays(15);
         $step2->setStatus('Publish');
         $step2->setTitle('Publish');
