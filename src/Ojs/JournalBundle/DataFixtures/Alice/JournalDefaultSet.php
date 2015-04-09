c72ba767e21b01273d8e95753cdddd402158ee4d
Hüseyin Mert <huseyinin@gmail.com>
2015-04-09 18:26:24 +0300

    faker date removed & added real data as inital-data

--- src/Ojs/JournalBundle/DataFixtures/Alice/JournalDefaultSet.php
+++ src/Ojs/JournalBundle/DataFixtures/Alice/JournalDefaultSet.php
@@ -13,7 +13,7 @@ $set = new \h4cc\AliceFixturesBundle\Fixtures\FixtureSet([
 
 $set->addFile(__DIR__ . '/lang.yml', 'yaml');
 
-$set->addFile(__DIR__ . '/i_type.yml', 'yaml');
+$set->addFile(__DIR__ . '/institution_type.yml', 'yaml');
 $set->addFile(__DIR__ . '/institution.yml', 'yaml');
 
 $set->addFile(__DIR__ . '/subject.yml', 'yaml');
@@ -24,21 +24,19 @@ $set->addFile(__DIR__ . '/journal_section.yml', 'yaml');
 $set->addFile(__DIR__ . '/issue.yml', 'yaml');
 
 $set->addFile(__DIR__ . '/article.yml', 'yaml');
-//$set->addFile(__DIR__ . '/a_attribute.yml', 'yaml'); //Article Attribute
-
+//$set->addFile(__DIR__ . '/article_attribute.yml', 'yaml'); //Article Attribute
 $set->addFile(__DIR__ . '/author.yml', 'yaml');
-$set->addFile(__DIR__ . '/a_author.yml', 'yaml'); //Article Author
+$set->addFile(__DIR__ . '/article_author.yml', 'yaml'); //Article Author
+$set->addFile(__DIR__ . '/file.yml', 'yaml');
+$set->addFile(__DIR__ . '/article_file.yml', 'yaml'); //Article File
 
 $set->addFile(__DIR__ . '/citation.yml', 'yaml');
-//$set->addFile(__DIR__ . '/c_setting.yml', 'yaml'); //Citation Settings
+//$set->addFile(__DIR__ . '/citation_setting.yml', 'yaml'); //Citation Settings
 
-//$set->addFile(__DIR__ . '/contact.yml', 'yaml');
-//$set->addFile(__DIR__ . '/c_type.yml', 'yaml'); //Contact Type
-//$set->addFile(__DIR__ . '/j_contact.yml', 'yaml'); //Journal Contact
+$set->addFile(__DIR__ . '/contact.yml', 'yaml');
+$set->addFile(__DIR__ . '/contact_type.yml', 'yaml'); //Contact Types
+//$set->addFile(__DIR__ . '/journal_contact.yml', 'yaml'); //Journal Contacts
 
-$set->addFile(__DIR__ . '/file.yml', 'yaml');
-
-$set->addFile(__DIR__ . '/a_file.yml', 'yaml'); //Article File
 
 $set->addFile(__DIR__ . '/theme.yml', 'yaml'); //themes
 $set->addFile(__DIR__ . '/journal_licence.yml', 'yaml'); //licences
