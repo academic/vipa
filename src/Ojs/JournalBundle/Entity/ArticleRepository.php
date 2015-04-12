ffb56fd38ee804a830d36b47847a78d6875f40ac
Hüseyin Mert <huseyinin@gmail.com>
2015-04-13 00:22:16 +0300

    article pdf url function fixed. added new meta tags

--- src/Ojs/JournalBundle/Entity/ArticleRepository.php
+++ src/Ojs/JournalBundle/Entity/ArticleRepository.php
@@ -4,34 +4,7 @@ namespace Ojs\JournalBundle\Entity;
 
 use Doctrine\ORM\EntityRepository;
 
-class ArticleRepository extends EntityRepository
-{
-
-    /**
-     * Get Article full text file. 
-     * @param  integer $articleId
-     * @return ArticleFile
-     * @throws NoResultException
-     */
-    public function getArticleFullTextFile($articleId)
-    {
-        $q = $this
-                ->createQueryBuilder('a')
-                ->select('a, f')
-                ->leftJoin('a.articleFiles', 'f')
-                ->where('a.article_id = :article_id AND f.type= 0')
-                ->setParameter('article_id', $articleId)
-                ->getQuery();
-        try {
-            // The Query::getSingleResult() method throws an exception
-            // if there is no record matching the criteria.
-            $article = $q->getSingleResult();
-            return $article;
-        } catch (NoResultException $e) {
-            $message = sprintf('There is no full text file for this article.');
-            throw new UsernameNotFoundException($message, 0, $e);
-        }
-    }
+class ArticleRepository extends EntityRepository {
 
     /**
      * Get articles that has no issue_id
