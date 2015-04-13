0c1ee6e04ae4359b5833aa720118c8084edb7915
aybarscengaver <aybarscengaver@gmail.com>
2015-04-13 12:09:04 +0300

    fixed #442

--- tools/batch_import.sh
+++ tools/batch_import.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 IDS=dergipark.csv
 
+contents=()
+i=0
 while read id
 do
-    echo "Adding $id"
-    php ../app/console ojs:import:journal $id
+    #echo "Adding $id"
+    #php ../app/console ojs:import:journal $id
+    i=$((i+5))
+    $contents[$i]=$id
 done < $IDS
+echo $id[*]
