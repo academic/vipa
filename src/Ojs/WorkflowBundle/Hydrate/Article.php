b2519bcb52653890e33496509f2437e9d2b3d979
aybarscengaver <aybarscengaver@gmail.com>
2015-04-15 19:31:16 +0300

    fixed #430

--- src/Ojs/WorkflowBundle/Hydrate/Article.php
+++ src/Ojs/WorkflowBundle/Hydrate/Article.php
@@ -36,12 +36,12 @@ class Article
     public function mapChanges(array $data, ArticleReviewStep &$step)
     {
         $articleRevised = $step->getArticleRevised();
-        if(array_key_exists('article',$data['changes'])){
+        if (array_key_exists('article', $data['changes'])) {
             foreach ($data['changes']['article'] as $key => $article) {
                 $articleRevised['articleData'][$key] = array_merge($articleRevised['articleData'][$key], $article);
             }
         }
-        if(array_key_exists('author',$data['changes'])){
+        if (array_key_exists('author', $data['changes'])) {
             foreach ($data['changes']['author'] as $key => $author) {
                 $articleRevised['authors'][$key - 1] = array_merge($articleRevised['authors'][$key - 1], $author);
             }
@@ -50,28 +50,28 @@ class Article
         if (array_key_exists('citation', $data['changes'])) {
             foreach ($data['changes']['citation'] as $key => $citation) {
                 $parsedCitations = $citationParser->parse($citation)[0];
-                $citation=$this->grepCitation($parsedCitations,$citation);
+                $citation = $this->grepCitation($parsedCitations, $citation);
                 $articleRevised['citation'][$key - 1] = array_merge($articleRevised['citation'][$key - 1], $citation);
             }
         }
-        if(array_key_exists('files',$data['changes'])){
-            foreach ($data['changes']['files'] as  $key=>$file) {
-                if($data['files'][$key]){
-                    $file = array_merge($data['files'][$key],$file);
+        if (array_key_exists('files', $data['changes'])) {
+            foreach ($data['changes']['files'] as $key => $file) {
+                if ($data['files'][$key]) {
+                    $file = array_merge($data['files'][$key], $file);
                 }
                 $articleRevised['files'][$key - 1] = array_merge($articleRevised['files'][$key - 1], $file);
             }
         }
-        if(array_key_exists('author',$data)){
+        if (array_key_exists('author', $data)) {
             $data['author'] = $this->grepAuthors($data['author']);
-            $articleRevised['authors']=array_merge($articleRevised['authors'],$data['author']);
+            $articleRevised['authors'] = array_merge($articleRevised['authors'], $data['author']);
         }
-        if(array_key_exists('citation',$data)){
+        if (array_key_exists('citation', $data)) {
             foreach ($data['citation'] as $key => $citation) {
                 $parsed = $citationParser->parse($citation);
-                if($parsed){
+                if ($parsed) {
                     $parsedCitations = $parsed[0];
-                    $citation=$this->grepCitation($parsedCitations,$citation);
+                    $citation = $this->grepCitation($parsedCitations, $citation);
                     $articleRevised['citation'] = array_merge($articleRevised['citation'], [$citation]);
                 }
             }
@@ -79,7 +79,12 @@ class Article
         $step->setArticleRevised($articleRevised);
     }
 
-    private function grepCitation($parsedCitations,$citation)
+    /**
+     * @param $parsedCitations
+     * @param $citation
+     * @return array
+     */
+    private function grepCitation($parsedCitations, $citation)
     {
         $citation = [
             'type' => $parsedCitations->getType(),
@@ -94,23 +99,23 @@ class Article
         ];
         return $citation;
     }
+
     /**
      * @param array $authors
      * @return array
      */
-    private function grepAuthors(array $authors){
+    private function grepAuthors(array $authors)
+    {
         $author = [];
-        foreach($authors['orcid'] as $key=>$value){
-            $author[$key]=[
-              'orcid'=>$authors['orcid'][$key],
-                'order'=>$authors['order'][$key],
-                'title'=>$authors['title'][$key],
-                'initials'=>$authors['initials'][$key],
-                'firstName'=>$authors['firstName'][$key],
-                'middleName'=>$authors['middleName'][$key],
-                'lastName'=>$authors['lastName'][$key],
-                'email'=>$authors['email'][$key]
-            ];
+        foreach ($authors['orcid'] as $key => $value) {
+            $author[$key] = [];
+            isset($authors['orcid']) && isset($authors['orcid'][$key]) && $author[$key]['orcid'] = $authors['orcid'][$key];
+            isset($authors['order']) && isset($authors['order'][$key]) && $author[$key]['order'] = $authors['order'][$key];
+            isset($authors['title']) && isset($authors['title'][$key]) && $author[$key]['title'] = $authors['title'][$key];
+            isset($authors['initials']) && isset($authors['initials'][$key]) && $author[$key]['initials'] = $authors['initials'][$key];
+            isset($authors['firstName']) && isset($authors['firstName'][$key]) && $author[$key]['firstName'] = $authors['firstName'][$key];
+            isset($authors['lastName']) && isset($authors['lastName'][$key]) && $author[$key]['lastName'] = $authors['lastName'][$key];
+            isset($authors['email']) && isset($authors['email'][$key]) && $author[$key]['email'] = $authors['email'][$key];
         }
         return $author;
     }
