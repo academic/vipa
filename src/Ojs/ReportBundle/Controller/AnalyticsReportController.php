bbfadebc0900808bb9388ed3343d34050e3b1c20
aybarscengaver <aybarscengaver@gmail.com>
2015-05-03 01:26:38 +0300

    readyfortest #503

--- src/Ojs/ReportBundle/Controller/AnalyticsReportController.php
+++ src/Ojs/ReportBundle/Controller/AnalyticsReportController.php
@@ -2,6 +2,7 @@
 
 namespace Ojs\ReportBundle\Controller;
 
+use Ojs\AnalyticsBundle\Document\ObjectViews;
 use Ojs\JournalBundle\Entity\Article;
 use Ojs\UserBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -12,36 +13,17 @@ class AnalyticsReportController extends Controller
     public function indexAction()
     {
         $data = [];
-        $dm = $this->get('doctrine.odm.mongodb.document_manager');
-        /** @var \Doctrine\ORM\EntityManager $em */
-        $em = $this->getDoctrine()->getManager();
 
-        $object_view = $dm->getRepository('OjsAnalyticsBundle:ObjectViews');
-        /** @var User $user */
-        $user = $this->getUser();
+        $journal_service = $this->get('ojs.journal_service');
 
-        $journal = $this->get('ojs.journal_service')->getSelectedJournal();
+        $journal = $journal_service->getSelectedJournal();
 
-        $journal_stats = [];
-        $journal_stats['info'] = $journal;
-        $journal_stats['stats'] = $object_view->findBy(['entity' => 'journal', 'objectId' => $journal->getId()]);
-        $articles = $journal->getArticles()->toArray();
-
-        $data['stats'] = [];
-        $data['stats']['journal'] = $journal_stats;
-
-        $article_stats = [];
-        foreach ($articles as $article) {
-            /** @var Article $_a */
-            $_a = [
-                'info' => $article,
-                'stats' => $object_view->findBy(['entity' => 'article', 'objectId' => $article->getId()]),
-            ];
-            $article_stats[] = $_a;
-        }
-        $data['stats']['article'] = $article_stats;
-
+        $data['journal_stats']  = $journal_service->journalStats($journal);
+        $data['article_stats'] = $journal_service->journalsArticlesStats($journal);
         return $this->render('OjsReportBundle:analytics:index.html.twig', $data);
     }
+
+
+
 
 }
