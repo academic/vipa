1fd95baf2c7b4b478513bf208febdb4de8d01a66
Utku Aydın <utkuaydin34@gmail.com>
2015-05-20 16:45:46 +0300

    Set labels of application forms manually, add new strings and translations, related #594

--- src/Ojs/JournalBundle/Form/InstitutionApplicationType.php
+++ src/Ojs/JournalBundle/Form/InstitutionApplicationType.php
@@ -42,27 +42,28 @@ class InstitutionApplicationType extends AbstractType
 
 
         $builder
-            ->add('name')
-            ->add('slug')
+            ->add('name', null, ['label' => 'institution.name'])
+            ->add('slug', null, ['label' => 'institution.slug'])
             ->add('type','choice',[
+                'label' => 'institution.type',
                 'choices'=>$choices['types']
             ])
-            ->add('about')
-            ->add('address')
-            ->add('lat')
-            ->add('lon')
-            ->add('email')
-            ->add('fax')
-            ->add('phone')
-            ->add('url')
-            ->add('wiki_url')
-            ->add('tags')
+            ->add('about', null, ['label' => 'institution.about'])
+            ->add('address', null, ['label' => 'institution.address'])
+            ->add('lat', null, ['label' => 'institution.lat'])
+            ->add('lon', null, ['label' => 'institution.lon'])
+            ->add('email', null, ['label' => 'institution.email'])
+            ->add('fax', null, ['label' => 'institution.fax'])
+            ->add('phone', null, ['label' => 'institution.phone'])
+            ->add('url', null, ['label' => 'institution.url'])
+            ->add('wiki_url', null, ['label' => 'institution.wiki_url'])
+            ->add('tags', null, ['label' => 'institution.tags'])
             ->add('logo_image','hidden')
             ->add('header_image','hidden')
-
             ->add('country', 'choice', [
                 'choices' => $choices['countries'],
                 'attr' => [
+                    'label' => 'institution.country',
                     'class' => 'select2-element  bridged-dropdown',
                     'data-to'=>'#'.$this->getName().'_city'
                 ]
