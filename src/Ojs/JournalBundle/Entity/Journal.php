0f0fec60b3a0e8fbe42d732f830c5f3331a0117d
Utku Aydın <utkuaydin34@gmail.com>
2015-05-21 17:26:44 +0300

    Bring the lost commit back: Add fields for journal's own contact information

--- src/Ojs/JournalBundle/Entity/Journal.php
+++ src/Ojs/JournalBundle/Entity/Journal.php
@@ -96,6 +96,24 @@ class Journal implements Translatable
     private $url;
 
     /**
+     * @var string
+     * @Expose
+     */
+    private $address;
+
+    /**
+     * @var string
+     * @Expose
+     */
+    private $phone;
+
+    /**
+     * @var string
+     * @Expose
+     */
+    private $email;
+
+    /**
      * @var Location
      * @Expose
      */
@@ -759,6 +777,54 @@ class Journal implements Translatable
         $this->institution = $institution;
 
         return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param string $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param string $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
     }
 
     /**
