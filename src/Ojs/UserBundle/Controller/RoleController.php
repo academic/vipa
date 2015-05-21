211b1ed0311a6ab1e446709adff65738641be00d
Behram ÇELEN <behramcelen@gmail.com>
2015-05-21 05:08:00 +0300

    role entity remove action edit related #606

--- src/Ojs/UserBundle/Controller/RoleController.php
+++ src/Ojs/UserBundle/Controller/RoleController.php
@@ -101,11 +101,9 @@ class RoleController extends Controller
         if (!$entity) {
             throw $this->createNotFoundException($this->get('translator')->trans('Not Found'));
         }
-        $deleteForm = $this->createDeleteForm($id);
 
         return $this->render('OjsUserBundle:Role:show.html.twig', array(
-            'entity' => $entity,
-            'delete_form' => $deleteForm->createView(),
+            'entity' => $entity
         ));
     }
 
@@ -120,12 +118,10 @@ class RoleController extends Controller
             throw $this->createNotFoundException($this->get('translator')->trans('Not Found'));
         }
         $editForm = $this->createEditForm($entity);
-        $deleteForm = $this->createDeleteForm($id);
 
         return $this->render('OjsUserBundle:Role:edit.html.twig', array(
             'entity' => $entity,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
+            'edit_form' => $editForm->createView()
         ));
     }
 
@@ -159,7 +155,6 @@ class RoleController extends Controller
         if (!$entity) {
             throw $this->createNotFoundException($this->get('translator')->trans('Not Found'));
         }
-        $deleteForm = $this->createDeleteForm($id);
         $editForm = $this->createEditForm($entity);
         $editForm->handleRequest($request);
         if ($editForm->isValid()) {
@@ -171,52 +166,22 @@ class RoleController extends Controller
 
         return array(
             'entity' => $entity,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
+            'edit_form' => $editForm->createView()
         );
     }
 
     /**
      * Deletes a Role entity.
+     * @param Role $entity
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
-    public function deleteAction(Request $request, $id)
+    public function deleteAction(Role $entity)
     {
-        $form = $this->createDeleteForm($id);
-        $form->handleRequest($request);
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $entity = $em->getRepository('OjsUserBundle:Role')->find($id);
-            if (!$entity) {
-                throw $this->createNotFoundException($this->get('translator')->trans('Not Found'));
-            }
-            $em->remove($entity);
-            $em->flush();
-        }
+        $this->throw404IfNotFound($entity);
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->remove($entity);
+        $em->flush();
         $this->successFlashBag('successful.remove');
         return $this->redirectToRoute('role');
     }
-
-    /**
-     * Creates a form to delete a Role entity by id.
-     *
-     * @param mixed $id The entity id
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm($id)
-    {
-        $t = $this->get('translator');
-
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('role_delete', array('id' => $id)))
-            ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' =>
-                $t->trans('Delete'),
-                'attr' => array(
-                    'class' => 'button alert',
-                    'onclick' => 'return confirm("' . $t->trans('Are you sure?') . '"); ')
-            ))
-            ->getForm();
-    }
-
 }
