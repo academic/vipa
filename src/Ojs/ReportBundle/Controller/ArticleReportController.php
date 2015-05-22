daf189f34e5261331c1875a6efe832fd68340b18
Behram ÇELEN <behramcelen@gmail.com>
2015-05-22 08:08:52 +0300

    code review for ReportBundle #608

--- src/Ojs/ReportBundle/Controller/ArticleReportController.php
+++ src/Ojs/ReportBundle/Controller/ArticleReportController.php
@@ -9,11 +9,11 @@ class ArticleReportController extends Controller
 
     public function indexAction()
     {
-        /** @var \Doctrine\ORM\EntityManager $em */
+        /*
         $em = $this->getDoctrine()->getManager();
         $dm = $this->get('doctrine.odm.mongodb.document_manager');
         $data = $dm->getRepository('OjsAnalyticsBundle:ObjectViews');
+        */
         return $this->render('OjsReportBundle:article:index.html.twig');
     }
-
 }
