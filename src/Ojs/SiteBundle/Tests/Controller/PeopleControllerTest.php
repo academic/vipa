c310a57256813375b68f01e61c5876cc3f10fcac
Utku Aydın <utkuaydin34@gmail.com>
2015-05-27 02:14:54 +0300

    Create people directory, related #651

---
+++ src/Ojs/SiteBundle/Tests/Controller/PeopleControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Ojs\SiteBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PeopleControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/index');
+    }
+
+    public function testShow()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/show');
+    }
+
+}
