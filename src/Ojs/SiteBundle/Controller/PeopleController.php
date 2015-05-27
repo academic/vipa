3b401d148bbb5413748f04538cc44f3076c5eb09
Utku Aydın <utkuaydin34@gmail.com>
2015-05-27 18:55:07 +0300

    Add pagination support for people directory

--- src/Ojs/SiteBundle/Controller/PeopleController.php
+++ src/Ojs/SiteBundle/Controller/PeopleController.php
@@ -3,20 +3,28 @@
 namespace Ojs\SiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Pagerfanta\Pagerfanta;
+use Pagerfanta\Adapter\ArrayAdapter;
 
 class PeopleController extends Controller
 {
     /**
+     * @param int $page
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction()
+    public function indexAction($page)
     {
         $em = $this->getDoctrine()->getManager();
         $usersWithRoles = $em
             ->getRepository('OjsUserBundle:UserJournalRole')
             ->getUsersWithRoles();
 
-        return $this->render('OjsSiteBundle:People:index.html.twig', ['people' => $usersWithRoles]);
+        $adapter = new ArrayAdapter($usersWithRoles);
+        $pagerfanta = new Pagerfanta($adapter);
+        $pagerfanta->setMaxPerPage(20);
+        $pagerfanta->setCurrentPage($page);
+
+        return $this->render('OjsSiteBundle:People:index.html.twig', ['people' => $usersWithRoles, 'pagerfanta' => $pagerfanta]);
     }
 
     /**
