35b083ec59f8386db130d55309f0194d4a486bbe
aybarscengaver <aybarscengaver@gmail.com>
2015-05-27 13:12:07 +0300

    related #659 membercountupdater completed.

--- src/Ojs/AnalyticsBundle/Updater/MemberCountUpdater.php
+++ src/Ojs/AnalyticsBundle/Updater/MemberCountUpdater.php
@@ -4,15 +4,44 @@
  */
 namespace Ojs\AnalyticsBundle\Updater;
 
-class MemberCountUpdater implements UpdaterInterface
+use Documents\UserRepository;
+use Ojs\UserBundle\Entity\UserJournalRole;
+
+class MemberCountUpdater extends Updater implements UpdaterInterface
 {
     public function update()
     {
         // TODO: Implement update() method.
     }
 
+    /**
+     * @return array
+     */
     public function count()
     {
-        // TODO: Implement count() method.
+        $readerRole = $this->em->getRepository("OjsUserBundle:Role")->findOneBy(['role'=>'ROLE_AUTHOR']);
+        /** @var UserRepository $ue */
+        $ue = $this->em->getRepository('Ojs\UserBundle\Entity\UserJournalRole');
+        $all = $ue->findBy(['roleId'=>$readerRole->getId()]);
+        $journalUsers = [];
+        foreach ($all as $r) {
+            /** @var UserJournalRole $r */
+            if (isset($journalUsers[$r->getJournalId()])
+                and in_array($r->getUserId(), $journalUsers[$r->getJournalId()])
+            ) {
+                continue;
+            }
+            $journalUsers[$r->getJournalId()][] = $r->getUserId();
+        }
+
+        return $journalUsers;
+    }
+
+    /**
+     * @return string
+     */
+    public function getObject()
+    {
+        return 'Ojs\JournalBundle\Entity\Journal';
     }
 }
