f22b65b64ddced815522f022184d093a5f82c9d6
Utku Aydın <utkuaydin34@gmail.com>
2015-05-28 23:52:28 +0300

    Simplify people directory, work in progress, related #651

--- src/Ojs/SiteBundle/Controller/PeopleController.php
+++ src/Ojs/SiteBundle/Controller/PeopleController.php
@@ -10,21 +10,28 @@ class PeopleController extends Controller
 {
     /**
      * @param int $page
+     * @param array $filter
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction($page = 1)
+    public function indexAction($page = 1, $filter = null)
     {
         $em = $this->getDoctrine()->getManager();
-        $usersWithRoles = $em
-            ->getRepository('OjsUserBundle:UserJournalRole')
-            ->getUsersWithRoles();
+        $users = $em->getRepository('OjsUserBundle:User')->findAll();
 
-        $adapter = new ArrayAdapter($usersWithRoles);
+        $adapter = new ArrayAdapter($users);
         $pagerfanta = new Pagerfanta($adapter);
         $pagerfanta->setMaxPerPage(20);
         $pagerfanta->setCurrentPage($page);
         $people = $pagerfanta->getCurrentPageResults();
 
-        return $this->render('OjsSiteBundle:People:index.html.twig', ['people' => $people, 'pagerfanta' => $pagerfanta]);
+        $subjects = $em->getRepository('OjsJournalBundle:Subject')->findAll();
+
+        $data = [
+            'people' => $people,
+            'subjects' => $subjects,
+            'pagerfanta' => $pagerfanta,
+        ];
+
+        return $this->render('OjsSiteBundle:People:index.html.twig', $data);
     }
 }
