852a68b0a29421e3222d52fae0cf5ebd6b6fdc94
Behram ÇELEN <behramcelen@gmail.com>
2015-06-08 14:30:29 +0300

    report bundle permission check via acl #697

--- src/Ojs/ReportBundle/Controller/ArticleReportController.php
+++ src/Ojs/ReportBundle/Controller/ArticleReportController.php
@@ -2,7 +2,8 @@
 
 namespace Ojs\ReportBundle\Controller;
 
- use Ojs\Common\Controller\OjsController as Controller;
+use Ojs\Common\Controller\OjsController as Controller;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class ArticleReportController extends Controller
 {
@@ -14,6 +15,10 @@ class ArticleReportController extends Controller
         $dm = $this->get('doctrine.odm.mongodb.document_manager');
         $data = $dm->getRepository('OjsAnalyticsBundle:ObjectViews');
         */
+        $journal = $this->get('ojs.journal_service')->getSelectedJournal();
+        if(!$this->isGranted('VIEW', $journal, 'report')) {
+            throw new AccessDeniedException("You are not authorized for view this page");
+        }
         return $this->render('OjsReportBundle:article:index.html.twig');
     }
 }
--- src/Ojs/ReportBundle/Controller/UserReportController.php
+++ src/Ojs/ReportBundle/Controller/UserReportController.php
@@ -2,15 +2,18 @@
 
 namespace Ojs\ReportBundle\Controller;
 
- use Ojs\Common\Controller\OjsController as Controller;
+use Ojs\Common\Controller\OjsController as Controller;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class UserReportController extends Controller
 {
 
     public function indexAction()
     {
-        //$em = $this->getDoctrine()->getManager();
-
+        $journal = $this->get('ojs.journal_service')->getSelectedJournal();
+        if(!$this->isGranted('VIEW', $journal, 'report')) {
+            throw new AccessDeniedException("You are not authorized for view this page");
+        }
         return $this->render('OjsReportBundle:user:index.html.twig');
     }
 }
