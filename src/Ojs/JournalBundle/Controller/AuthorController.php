c684fc5782b7a98e1b3ed2b96efbc068824b065f
Behram ÇELEN <behramcelen@gmail.com>
2015-06-08 18:57:40 +0300

    gis

--- src/Ojs/JournalBundle/Controller/AuthorController.php
+++ src/Ojs/JournalBundle/Controller/AuthorController.php
@@ -34,8 +34,12 @@ class AuthorController extends Controller
         $actionColumn = new ActionsColumn("actions", 'actions');
         ActionHelper::setup($this->get('security.csrf.token_manager'), $this->get('translator'));
         $rowAction[] = ActionHelper::showAction('author_show', 'id');
-        $rowAction[] = ActionHelper::editAction('author_edit', 'id');
-        $rowAction[] = ActionHelper::deleteAction('author_delete', 'id');
+        if(!$this->isGranted('EDIT', new Author())) {
+            $rowAction[] = ActionHelper::editAction('author_edit', 'id');
+        }
+        if(!$this->isGranted('DELETE', new Author())) {
+            $rowAction[] = ActionHelper::deleteAction('author_delete', 'id');
+        }
 
         $actionColumn->setRowActions($rowAction);
         $grid->addColumn($actionColumn);
