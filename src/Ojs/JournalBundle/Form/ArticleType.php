aad855edf7dbddd2b5b4fce4a79a5bc014858f30
Behram ÇELEN <behramcelen@gmail.com>
2015-06-11 02:03:17 +0300

    article type on edit form #717

--- src/Ojs/JournalBundle/Form/ArticleType.php
+++ src/Ojs/JournalBundle/Form/ArticleType.php
@@ -44,6 +44,15 @@ class ArticleType extends AbstractType
                 )
             )
             ->add(
+                'articleType',
+                'entity',
+                array(
+                    'label' => 'article.type',
+                    'class' => 'Ojs\JournalBundle\Entity\ArticleTypes',
+                    'required' => false
+                )
+            )
+            ->add(
                 'status',
                 'choice',
                 array(
