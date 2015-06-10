fc1cbfe3558656adade892a7e2ffd10160f9d6c7
Behram ÇELEN <behramcelen@gmail.com>
2015-06-11 01:58:11 +0300

    article type crud actions create related #717

---
+++ src/Ojs/JournalBundle/Form/ArticleTypesType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Ojs\JournalBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ArticleTypesType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description');
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'data_class' => 'Ojs\JournalBundle\Entity\ArticleTypes',
+                'attr' => [
+                    'novalidate' => 'novalidate',
+                    'class' => 'form-validate',
+                ],
+            )
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ojs_journalbundle_articletypes';
+    }
+}
