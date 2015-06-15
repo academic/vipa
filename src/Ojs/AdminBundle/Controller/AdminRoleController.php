d29e6a4414847734f9c3c8e3c4b97cc78147e004
Utku Aydın <utkuaydin34@gmail.com>
2015-06-16 00:17:26 +0300

    Move role administration logic to AdminBundle, related #713

--- src/Ojs/AdminBundle/Controller/AdminRoleController.php
+++ src/Ojs/AdminBundle/Controller/AdminRoleController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Ojs\UserBundle\Controller;
+namespace Ojs\AdminBundle\Controller;
 
 use APY\DataGridBundle\Grid\Column\ActionsColumn;
 use APY\DataGridBundle\Grid\Source\Entity;
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Core\Exception\TokenNotFoundException;
 
-class RoleController extends Controller
+class AdminRoleController extends Controller
 {
     /**
      * Lists all Role entities.
@@ -31,15 +31,15 @@ class RoleController extends Controller
         $actionColumn = new ActionsColumn("actions", "actions");
         $rowAction = [];
 
-        $rowAction[] = $gridAction->showAction('role_show', 'id');
-        $rowAction[] = $gridAction->editAction('role_edit', 'id');
-        $rowAction[] = $gridAction->deleteAction('role_delete', 'id');
+        $rowAction[] = $gridAction->showAction('ojs_admin_role_show', 'id');
+        $rowAction[] = $gridAction->editAction('ojs_admin_role_edit', 'id');
+        $rowAction[] = $gridAction->deleteAction('ojs_admin_role_delete', 'id');
         $actionColumn->setRowActions($rowAction);
         $grid->addColumn($actionColumn);
         $data = [];
         $data['grid'] = $grid;
 
-        return $grid->getGridResponse('OjsUserBundle:Role:index.html.twig', $data);
+        return $grid->getGridResponse('OjsAdminBundle:AdminRole:index.html.twig', $data);
     }
 
     /**
@@ -63,7 +63,7 @@ class RoleController extends Controller
 
             $this->successFlashBag('successful.create');
 
-            return $this->redirectToRoute('role_show', ['id' => $entity->getId()]);
+            return $this->redirectToRoute('ojs_admin_role_show', ['id' => $entity->getId()]);
         }
 
         return array(
@@ -84,7 +84,7 @@ class RoleController extends Controller
             new RoleType(),
             $entity,
             array(
-                'action' => $this->generateUrl('role_create'),
+                'action' => $this->generateUrl('ojs_admin_role_create'),
                 'method' => 'POST',
             )
         );
@@ -105,7 +105,7 @@ class RoleController extends Controller
         $form = $this->createCreateForm($entity);
 
         return $this->render(
-            'OjsUserBundle:Role:new.html.twig',
+            'OjsAdminBundle:AdminRole:new.html.twig',
             array(
                 'entity' => $entity,
                 'form' => $form->createView(),
@@ -129,7 +129,7 @@ class RoleController extends Controller
         }
 
         return $this->render(
-            'OjsUserBundle:Role:show.html.twig',
+            'OjsAdminBundle:AdminRole:show.html.twig',
             array(
                 'entity' => $entity,
             )
@@ -153,7 +153,7 @@ class RoleController extends Controller
         $editForm = $this->createEditForm($entity);
 
         return $this->render(
-            'OjsUserBundle:Role:edit.html.twig',
+            'OjsAdminBundle:AdminRole:edit.html.twig',
             array(
                 'entity' => $entity,
                 'edit_form' => $editForm->createView(),
@@ -174,7 +174,7 @@ class RoleController extends Controller
             new RoleType(),
             $entity,
             array(
-                'action' => $this->generateUrl('role_update', array('id' => $entity->getId())),
+                'action' => $this->generateUrl('ojs_admin_role_update', array('id' => $entity->getId())),
                 'method' => 'PUT',
             )
         );
@@ -212,7 +212,7 @@ class RoleController extends Controller
 
             $this->successFlashBag('successful.update');
 
-            return $this->redirectToRoute('role_edit', ['id' => $id]);
+            return $this->redirectToRoute('ojs_admin_role_edit', ['id' => $id]);
         }
 
         return array(
