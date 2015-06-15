01b9950cf731129b20cb1399e02562228d6dab3b
Utku Aydın <utkuaydin34@gmail.com>
2015-06-15 16:50:49 +0300

    Create AdminBundle, related #713

---
+++ src/Ojs/AdminBundle/AdminBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ojs\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AdminBundle extends Bundle
+{
+}
---
+++ src/Ojs/AdminBundle/Controller/AdminUserController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ojs\AdminBundle\Controller;
+
+use Ojs\Common\Controller\OjsController as Controller;
+
+class AdminUserController extends Controller
+{
+}
