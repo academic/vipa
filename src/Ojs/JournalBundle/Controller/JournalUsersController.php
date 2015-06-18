c69cb9c8365f2a562293d6a72e67b638ec0fa965
Behram ÇELEN <behramcelen@gmail.com>
2015-06-18 09:48:38 +0300

    split journal user create actions & views #714

--- src/Ojs/JournalBundle/Controller/JournalUsersController.php
+++ src/Ojs/JournalBundle/Controller/JournalUsersController.php
@@ -7,6 +7,11 @@ use APY\DataGridBundle\Grid\Source\Entity;
 use Doctrine\ORM\QueryBuilder;
 use Ojs\Common\Controller\OjsController as Controller;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Ojs\UserBundle\Entity\User;
+use Ojs\UserBundle\Form\Type\UserType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * JournalUsers controller.
@@ -54,5 +59,81 @@ class JournalUsersController extends Controller
                 'grid' => $grid
             ]
         );
+    }
+
+    public function newUserAction()
+    {
+        $journal = $this->get('ojs.journal_service')->getSelectedJournal();
+        if (!$this->isGranted('CREATE', $journal, 'userRole')) {
+            throw new AccessDeniedException("You are not authorized for this page!");
+        }
+        $entity = new User();
+        $form = $this->createCreateForm($entity);
+
+        return $this->render(
+            'OjsJournalBundle:JournalUsers:new_user.html.twig',
+            array(
+                'entity' => $entity,
+                'form' => $form->createView(),
+            )
+        );
+    }
+
+    /**
+     * Creates a new User entity.
+     *
+     * @param  Request                   $request
+     * @return RedirectResponse|Response
+     */
+    public function createUserAction(Request $request)
+    {
+        $journal = $this->get('ojs.journal_service')->getSelectedJournal();
+        if (!$this->isGranted('CREATE', $journal, 'userRole')) {
+            throw new AccessDeniedException("You are not authorized for this page!");
+        }
+        $entity = new User();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $factory = $this->get('security.encoder_factory');
+            $encoder = $factory->getEncoder($entity);
+            $password = $encoder->encodePassword($entity->getPassword(), $entity->getSalt());
+            $entity->setPassword($password);
+            $entity->setAvatar($request->get('user_avatar'));
+            $em->persist($entity);
+            $em->flush();
+
+            $this->successFlashBag('successful.create');
+            return $this->redirectToRoute('ujr_new');
+        }
+
+        return $this->render(
+            'OjsAdminBundle:AdminUser:new.html.twig',
+            array(
+                'entity' => $entity,
+                'form' => $form->createView(),
+            )
+        );
+    }
+
+    /**
+     * Creates a form to create a User entity.
+     * @param  User $entity The entity
+     * @return Form The form
+     */
+    private function createCreateForm(User $entity)
+    {
+        $form = $this->createForm(
+            new UserType(),
+            $entity,
+            array(
+                'action' => $this->generateUrl('journal_users_create_user'),
+                'method' => 'POST',
+                'helper' => $this->get('ojs_location.form.helper'),
+            )
+        );
+
+        return $form;
     }
 }
