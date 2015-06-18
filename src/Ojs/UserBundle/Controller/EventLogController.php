2ec2a99e34993f001faa132886e8f98807a41bc4
Utku Aydın <utkuaydin34@gmail.com>
2015-06-18 13:18:13 +0300

    Move event log administration logic to AdminBundle, related #713

--- src/Ojs/UserBundle/Controller/EventLogController.php
+++ src/Ojs/UserBundle/Controller/EventLogController.php
@@ -19,8 +19,7 @@ class EventLogController extends Controller
 {
     /**
      * Lists all EventLog according to user role.
-     *
-     * @todo \Ojs\Common\Params\EventLogParams role level logs classes can expand for every role
+     * @todo Add items to EventLogParams for every role
      *
      * @return mixed
      */
@@ -28,50 +27,32 @@ class EventLogController extends Controller
     {
         /** @var User $user */
         $user = $this->getUser();
+        $logTypes = EventLogParams::editorLevelEventLogs();
 
-        //get eventLog parameters according to user role
-        if ($user->isAdmin()) {
-            $logTypes = EventLogParams::adminLevelEventLogs();
-        } else {
-            //if unlisted user_role.
-            $logTypes = EventLogParams::editorLevelEventLogs();
-        }
         $source = new Entity('OjsUserBundle:EventLog');
-        $ta = $source->getTableAlias();
+        $tableAlias = $source->getTableAlias();
         $source->manipulateQuery(
-            function (QueryBuilder $qb) use ($ta, $logTypes, $user) {
-                $qb->andWhere(
-                    $qb->expr()->in($ta.'.eventInfo', ':logTypes')
-                )
-                    ->setParameters(
-                        [
-                            'logTypes' => $logTypes,
-                            'userId' => $user->getId(),
-                        ]
-                    );
-                if (!$user->isAdmin()) {
-                    $qb->andWhere("$ta.userId = :userId OR $ta.affectedUserId = :userId");
-                }
-
+            function (QueryBuilder $qb) use ($tableAlias, $logTypes, $user) {
+                $expression = $qb->expr()->in($tableAlias.'.eventInfo', ':logTypes');
+                $qb->andWhere($expression)->setParameter('logTypes', $logTypes);
+                $qb->andWhere("$tableAlias.userId = :userId OR $tableAlias.affectedUserId = :userId")
+                    ->setParameter('userId', $user->getId());
                 return $qb;
             }
         );
+
         $grid = $this->get('grid')->setSource($source);
         $gridAction = $this->get('grid_action');
         $actionColumn = new ActionsColumn("actions", "actions");
-        $rowAction = [];
-
         $rowAction[] = $gridAction->showAction('user_eventlog_show', 'id');
         $actionColumn->setRowActions($rowAction);
         $grid->addColumn($actionColumn);
-        $data = [];
-        $data['grid'] = $grid;
 
-        return $grid->getGridResponse('OjsUserBundle:EventLog:index.html.twig', $data);
+        return $grid->getGridResponse('OjsUserBundle:EventLog:index.html.twig', ['grid' => $grid]);
     }
 
     /**
-     * Finds and displays a EventLog entity.
+     * Finds and displays an EventLog entity.
      *
      * @param $id
      * @return Response
@@ -82,60 +63,14 @@ class EventLogController extends Controller
         $user = $this->getUser();
         $em = $this->getDoctrine()->getManager();
         $entity = $em->getRepository('OjsUserBundle:EventLog')->find($id);
-        if (!$entity) {
+
+        if (!$entity)
             throw $this->createNotFoundException('notFound');
-        }
 
-        //if event isn't consider user throw 403
-        if ($entity->getUserId() !== $user->getId() && $entity->getAffectedUserId() !== $user->getId(
-            ) && !$user->isAdmin()
-        ) {
+        // If the event about the user, throw 403
+        if ($entity->getUserId() !== $user->getId() && $entity->getAffectedUserId() !== $user->getId())
             throw $this->createNotFoundException('You have not permission to see this activity.');
-        }
 
-        if ($user->isAdmin()) {
-            $tpl = 'OjsUserBundle:EventLog:admin/show.html.twig';
-        } else {
-            $tpl = 'OjsUserBundle:EventLog:show.html.twig';
-        }
-
-        return $this->render(
-            $tpl,
-            array(
-                'entity' => $entity,
-            )
-        );
-    }
-
-    /**
-     * Removes all EventLog records.
-     * Function only open for admin users.
-     *
-     * @return RedirectResponse
-     */
-    public function flushAction()
-    {
-
-        /**
-         * All entities delete. Function not truncate table only removes all entry, not resets FOREIGN_KEY.
-         *
-         * Later this event can be log another super user log table.
-         *
-         * For Truncating you can use this(http://stackoverflow.com/a/9710383/2438520) link
-         */
-        $em = $this->getDoctrine()->getManager();
-        $entities = $em->getRepository('OjsUserBundle:EventLog')->findAll();
-
-        foreach ($entities as $entity) {
-            $em->remove($entity);
-        }
-        $em->flush();
-
-        $this->get('session')->getFlashBag()->add(
-            'success',
-            $this->get('translator')->trans('All records removed successfully!')
-        );
-
-        return $this->redirect($this->generateUrl('eventlog'));
+        return $this->render('OjsUserBundle:EventLog:show.html.twig', ['entity' => $entity]);
     }
 }
