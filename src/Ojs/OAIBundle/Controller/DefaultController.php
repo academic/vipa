70cae3ba90ecc5194d2e692bb3d09b59d0081154
aybarscengaver <aybarscengaver@gmail.com>
2015-06-23 04:04:46 +0300

    related #721

--- src/Ojs/OAIBundle/Controller/DefaultController.php
+++ src/Ojs/OAIBundle/Controller/DefaultController.php
@@ -2,14 +2,16 @@
 
 namespace Ojs\OAIBundle\Controller;
 
+use Doctrine\ORM\EntityManager;
 use Ojs\Common\Controller\OjsController as Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
     /**
-     * @param  Request  $request
+     * @param  Request $request
      * @return Response
      */
     public function indexAction(Request $request)
@@ -31,10 +33,11 @@ class DefaultController extends Controller
             case 'ListIdentifiers':
                 return $this->listIdentifierAction($request);
                 break;
+            case 'GetRecord':
+                return $this->getRecordAction($request);
+                break;
         }
-        $response = new Response();
-        $response->headers->set('content-type','text/xml');
-        return $this->render('OjsOAIBundle:Default:index.html.twig',[],$response);
+        return $this->response('OjsOAIBundle:Default:index.xml.twig');
     }
 
     /**
@@ -42,11 +45,11 @@ class DefaultController extends Controller
      */
     public function identifyAction()
     {
-        return $this->response("OjsOAIBundle:Default:identify.html.twig");
+        return $this->response("OjsOAIBundle:Default:identify.xml.twig");
     }
 
     /**
-     * @param  Request  $request
+     * @param  Request $request
      * @return Response
      */
     public function recordsAction(Request $request)
@@ -86,11 +89,11 @@ class DefaultController extends Controller
         $records = $qb->getQuery()->getResult();
         $data['records'] = $records;
 
-        return $this->response('OjsOAIBundle:Default:records.html.twig', $data);
+        return $this->response('OjsOAIBundle:Default:records.xml.twig', $data);
     }
 
     /**
-     * @param  Request  $request
+     * @param  Request $request
      * @return Response
      */
     public function listSetsAction(Request $request)
@@ -129,7 +132,7 @@ class DefaultController extends Controller
         $sets = $qb->getQuery()->getResult();
         $data['records'] = $sets;
 
-        return $this->response('OjsOAIBundle:Default:sets.html.twig', $data);
+        return $this->response('OjsOAIBundle:Default:sets.xml.twig', $data);
     }
 
     /**
@@ -143,15 +146,63 @@ class DefaultController extends Controller
     /**
      * @return Response
      */
-    public function listIdentifierAction()
+    public function listIdentifierAction(Request $request)
     {
-        return new Response();
+        $data = [];
+        /** @var \Doctrine\ORM\EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
+        $qb = $em->createQueryBuilder();
+        $qb->select("a")
+            ->from("OjsJournalBundle:Article", 'a');
+        $set = $request->get("set", false);
+        if ($set) {
+            $qb->join("a.journal","j","WITH");
+            $qb->where(
+                $qb->expr()->gte('j.slug', ':slug')
+            )
+                ->setParameter('slug', $set);
+        }
+        $data['articles'] = $qb->getQuery()->getResult();
+        return $this->response("OjsOAIBundle:Default:identifiers.xml.twig", $data);
     }
 
-    public function response($template, $data=[])
+
+    public function getRecordAction(Request $request)
+    {
+        $data = [];
+        $identifier = $request->get('identifier');
+        $base_host = $this->container->getParameter("base_host");
+        preg_match_all('~oai:'.$base_host.':((\barticle\b)|(\brecord\b))/(\d+)~',$identifier,$matches);
+        if(!isset($matches[4]) || !isset($matches[4][0])){
+            throw new NotFoundHttpException("Record not found.");
+        }
+        $id = $matches[4][0];
+        /** @var EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
+        $qb = $em->createQueryBuilder();
+        $qb->select("a")
+            ->from("OjsJournalBundle:Article","a");
+        $qb->where(
+            $qb->expr()->eq("a.id",":id")
+        )
+            ->setParameter("id",$id);
+        $data['record'] = $qb->getQuery()->getOneOrNullResult();
+        if(!$data['record']){
+            throw new NotFoundHttpException("Record not found");
+        }
+        return $this->response('OjsOAIBundle:Default:record.xml.twig',$data);
+    }
+
+    /**
+     * Xml response
+     * @param $template
+     * @param array $data
+     * @return Response
+     */
+    public function response($template, $data = [])
     {
         $response = new Response();
-        $response->headers->set('content-type','text/xml');
-        return $this->render($template,$data,$response);
+        $response->headers->set('content-type', 'text/xml');
+        return $this->render($template, $data, $response);
     }
 }
