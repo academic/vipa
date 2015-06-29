262f522999378ac69c85bbe68f888f4bfb7715e9
Behram ÇELEN <behramcelen@gmail.com>
2015-06-29 06:36:45 +0300

    article file entity form translation support related #742

---
+++ src/Ojs/JournalBundle/Entity/ArticleFileTranslation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ojs\JournalBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Translatable\Entity\MappedSuperclass\AbstractPersonalTranslation;
+
+class ArticleFileTranslation extends AbstractPersonalTranslation
+{
+    protected $object;
+}
