7833ec78f0a6c3edda947b31dcf260e2ec923d6f
aybarscengaver <aybarscengaver@gmail.com>
2015-07-13 23:19:34 +0300

    Wrong database name

--- tools/stats_batch_import.sh
+++ tools/stats_batch_import.sh
@@ -6,7 +6,7 @@ i=0
 while read id
 do
     echo "Adding $id stats"
-    php dpstastImporter.php $id root:root@10.61.11.29/dpupdated root:root@10.61.11.29/dpstats ojs 10.61.11.29
+    php dpstastImporter.php $id root:root@10.61.11.29/dergipark root:root@10.61.11.29/dpstats ojs 10.61.11.29
     i=$((i+1))
     contents[$i]=$id
     res=$[$i%10]
