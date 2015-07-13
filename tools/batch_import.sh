025f69d55472a33218309c4bd7c6bc23bb9be8b7
aybarscengaver <aybarscengaver@gmail.com>
2015-07-13 15:29:11 +0300

    wrong database name

--- tools/batch_import.sh
+++ tools/batch_import.sh
@@ -6,7 +6,7 @@ i=0
 while read id
 do
     echo "Adding $id"
-    php ../app/console ojs:import:journal $id root:root@10.61.11.29/dbupdated
+    php ../app/console ojs:import:journal $id root:root@10.61.11.29/dpupdated
     i=$((i+1))
     contents[$i]=$id
     res=$[$i%10]
