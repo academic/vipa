4617d6ee54d4a75636794e19ffa30ff176cafd62
aybarscengaver <aybarscengaver@gmail.com>
2015-07-13 23:16:25 +0300

    batch import for stats

---
+++ tools/stats_batch_import.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+IDS=dergipark.csv
+
+contents=()
+i=0
+while read id
+do
+    echo "Adding $id stats"
+    php dpstastImporter.php $id root:root@10.61.11.29/dpupdated root:root@10.61.11.29/dpstats ojs 10.61.11.29
+    i=$((i+1))
+    contents[$i]=$id
+    res=$[$i%10]
+    if [ $res  -eq "0" ]; then
+        echo "\nwaiting...\n"
+        sleep 10
+    fi
+done < $IDS
