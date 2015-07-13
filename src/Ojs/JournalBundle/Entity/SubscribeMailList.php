1686c74903e24b6f392b71cf463bfa44c1017435
Behram ÇELEN <behramcelen@gmail.com>
2015-07-13 14:53:52 +0300

    subscribe mail list entity and orm file create #149

---
+++ src/Ojs/JournalBundle/Entity/SubscribeMailList.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Ojs\JournalBundle\Entity;
+
+use APY\DataGridBundle\Grid\Mapping as GRID;
+use Gedmo\Translatable\Translatable;
+use Ojs\Common\Entity\GenericEntityTrait;
+
+/**
+ * SubscribeMailList
+ */
+class SubscribeMailList implements Translatable
+{
+    use GenericEntityTrait;
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var integer
+     */
+    private $journalId;
+
+    /**
+     *
+     * @var Journal
+     */
+    private $journal;
+
+    /**
+     * @var string
+     */
+    private $mail;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set journal
+     * @param  Journal $journal
+     * @return Issue
+     */
+    public function setJournal($journal)
+    {
+        $this->journal = $journal;
+
+        return $this;
+    }
+
+    /**
+     * Get journal
+     *
+     * @return Journal
+     */
+    public function getJournal()
+    {
+        return $this->journal;
+    }
+
+    /**
+     * Set journalId
+     *
+     * @param  integer $journalId
+     * @return Issue
+     */
+    public function setJournalId($journalId)
+    {
+        $this->journalId = $journalId;
+
+        return $this;
+    }
+
+    /**
+     * Get journalId
+     *
+     * @return integer
+     */
+    public function getJournalId()
+    {
+        return $this->journalId;
+    }
+
+    /**
+     * Set mail
+     *
+     * @param  string $mail
+     * @return $this
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    /**
+     * Get mail
+     *
+     * @return string
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+}
