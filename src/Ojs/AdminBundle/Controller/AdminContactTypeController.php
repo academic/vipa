0597f1104f104af5cbd937b5fc7b8824960044b9
Behram ÇELEN <behramcelen@gmail.com>
2015-07-22 20:37:17 +0300

    contact type form translation bugfix #788

--- src/Ojs/AdminBundle/Controller/AdminContactTypeController.php
+++ src/Ojs/AdminBundle/Controller/AdminContactTypeController.php
@@ -66,6 +66,7 @@ class AdminContactTypeController extends Controller
         $form->handleRequest($request);
         if ($form->isValid()) {
             $em = $this->getDoctrine()->getManager();
+            $entity->setTranslatableLocale($request->getDefaultLocale());
             $em->persist($entity);
             $em->flush();
             $this->successFlashBag('successful.create');
