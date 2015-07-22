b35a1a3255e03df27a7d96d51cd2f9d64e6528e8
aybarscengaver <aybarscengaver@gmail.com>
2015-07-22 19:44:11 +0300

    related #791

--- src/Ojs/ReportBundle/Controller/UserReportController.php
+++ src/Ojs/ReportBundle/Controller/UserReportController.php
@@ -2,6 +2,10 @@
 
 namespace Ojs\ReportBundle\Controller;
 
+use Elastica\Aggregation\DateHistogram;
+use Elastica\Aggregation\Terms;
+use Elastica\Aggregation\ValueCount;
+use Elastica\Query;
 use Ojs\Common\Controller\OjsController as Controller;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
@@ -15,6 +19,39 @@ class UserReportController extends Controller
             throw new AccessDeniedException("You are not authorized for view this page");
         }
 
-        return $this->render('OjsReportBundle:user:index.html.twig');
+        $results = $this->get('fos_elastica.index.search.user')->search($this->getStatsQuery());
+        $datedResult = $this->get('fos_elastica.index.search.user')->search($this->getDatedStatsQuery());
+        $data = [];
+        $data['aggs'] = $results->getAggregations();
+        $data['datedAggs'] = $datedResult->getAggregations();
+        return $this->render('OjsReportBundle:user:index.html.twig',$data);
+    }
+
+
+    private function getStatsQuery()
+    {
+        $query = new Query(new Query\MatchAll());
+        $titleAggregation = new Terms('title');
+        $titleAggregation->setField('title');
+
+        $query->addAggregation($titleAggregation);
+
+        $genderAggregation = new Terms('gender');
+        $genderAggregation->setField('gender');
+
+        $query->addAggregation($genderAggregation);
+
+        $query->setSize(0);
+        return $query;
+    }
+
+    private function getDatedStatsQuery(){
+        $query = new Query(new Query\MatchAll());
+        $dateHistogram = new DateHistogram('dateHistogram','created','month');
+
+        $query->addAggregation($dateHistogram);
+        $query->setSize(0);
+
+        return $query;
     }
 }
