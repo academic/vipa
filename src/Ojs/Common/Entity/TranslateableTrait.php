0f3fdbfa2d9eddad112e340694238b0575821ef5
aybarscengaver <aybarscengaver@gmail.com>
2015-07-23 21:09:34 +0300

    article reports related #791

--- src/Ojs/Common/Entity/TranslateableTrait.php
+++ src/Ojs/Common/Entity/TranslateableTrait.php
@@ -12,10 +12,30 @@ trait TranslateableTrait
     public function setTranslatableLocale($locale)
     {
         $this->locale = $locale;
+        return $this;
     }
 
     public function getTranslatableLocale()
     {
         return $this->locale;
+    }
+
+    /**
+     * Alias for getTranslatableLocale
+     * @return mixed
+     */
+    public function getLocale()
+    {
+        return $this->getTranslatableLocale();
+    }
+
+    /**
+     * Alias for setTranslatableLocale
+     * @param $locale
+     * @return TranslateableTrait
+     */
+    public function setLocale($locale)
+    {
+        return $this->setTranslatableLocale($locale);
     }
 }
--- src/Ojs/ReportBundle/Controller/ArticleReportController.php
+++ src/Ojs/ReportBundle/Controller/ArticleReportController.php
@@ -2,7 +2,13 @@
 
 namespace Ojs\ReportBundle\Controller;
 
+use Elastica\Aggregation\DateHistogram;
+use Elastica\Aggregation\Terms;
+use Elastica\Filter\Term;
+use Elastica\Query;
+use Elastica\Query\Filtered;
 use Ojs\Common\Controller\OjsController as Controller;
+use Ojs\JournalBundle\Entity\Journal;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class ArticleReportController extends Controller
@@ -19,7 +25,48 @@ class ArticleReportController extends Controller
         if (!$this->isGranted('VIEW', $journal, 'report')) {
             throw new AccessDeniedException("You are not authorized for view this page");
         }
+        $results = $this->get('fos_elastica.index.search.articles')->search($this->getStatsQuery($journal));
+        $counter = $this->get('fos_elastica.index.search.articles')->count($this->getCountQuery($journal));
+        $data = [];
+        $data['aggs'] = $results->getAggregations();
+        $data['total'] = $counter;
 
-        return $this->render('OjsReportBundle:article:index.html.twig');
+        return $this->render('OjsReportBundle:article:index.html.twig',$data);
+    }
+
+    private function getStatsQuery(Journal $journal)
+    {
+        $filter = new Term();
+        $filter->setTerm('journal.id',$journal->getId());
+        $filterQuery = new Filtered();
+        $filterQuery->setFilter($filter);
+        $query = new Query($filterQuery);
+
+        $dateHistogram = new DateHistogram('dateHistogram','created','month');
+        $dateHistogram->setFormat("YYYY-MM-dd");
+        $query->addAggregation($dateHistogram);
+
+        $genderAggregation = new Terms('language');
+        $genderAggregation->setField('locale');
+
+        $query->addAggregation($genderAggregation);
+
+        $query->setSize(0);
+
+
+
+        return $query;
+    }
+
+
+    private function getCountQuery($journal)
+    {
+        $filter = new Term();
+        $filter->setTerm('journal.id',$journal->getId());
+        $filterQuery = new Query\Filtered();
+        $filterQuery->setFilter($filter);
+        $query = new Query($filterQuery);
+        $query->setSize(0);
+        return $query;
     }
 }
