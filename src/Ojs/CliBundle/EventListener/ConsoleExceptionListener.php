162266100aef199454a29d2eedee718cfa54a2e3
Utku Aydın <utkuaydin34@gmail.com>
2015-07-23 19:11:35 +0300

    Add a listener for console errors and log them

---
+++ src/Ojs/CliBundle/EventListener/ConsoleExceptionListener.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Ojs\CliBundle\EventListener;
+
+use Symfony\Component\Console\Event\ConsoleExceptionEvent;
+use Psr\Log\LoggerInterface;
+
+class ConsoleExceptionListener
+{
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function onConsoleException(ConsoleExceptionEvent $event)
+    {
+        $command = $event->getCommand();
+        $exception = $event->getException();
+
+        $message = sprintf(
+            '%s: %s (uncaught exception) at %s line %s while running console command `%s`',
+            get_class($exception),
+            $exception->getMessage(),
+            $exception->getFile(),
+            $exception->getLine(),
+            $command->getName()
+        );
+
+        $trace = $exception->getTraceAsString();
+
+        $this->logger->error($message, array('exception' => $exception));
+        $this->logger->error($trace, array('exception' => $exception));
+    }
+}
