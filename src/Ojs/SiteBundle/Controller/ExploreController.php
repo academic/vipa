73fdfff5a5ea2948c35a6f7a4a51977685c33432
Utku Aydın <utkuaydin34@gmail.com>
2015-07-27 18:52:21 +0300

    Create an early version for explore page and present all journals in a grid there, related #766 Require salvattore as bower dependency Add logo and header paths for journals to elasticsearch index Change assetic.yml's indentation

--- src/Ojs/SiteBundle/Controller/ExploreController.php
+++ src/Ojs/SiteBundle/Controller/ExploreController.php
@@ -2,61 +2,33 @@
 
 namespace Ojs\SiteBundle\Controller;
 
+use Doctrine\ORM\EntityManager;
+use Elastica\Query;
+use Ojs\JournalBundle\Entity\Institution;
+use Ojs\JournalBundle\Entity\InstitutionRepository;
+use Pagerfanta\Adapter\ElasticaAdapter;
+use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ExploreController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request, $page = 1)
     {
-        $data = array();
-        $data['page'] = 'explore';
-        $em = $this->getDoctrine()->getManager();
+        $journalSearcher = $this->get('fos_elastica.index.search.journal');
+        $journalQuery = new Query("*");
 
-        $data['institution_types'] = $em->getRepository('OjsJournalBundle:InstitutionTypes')
-            /* TODO: order by name + only has a journal in it */
-            ->findAll();
+        $adapter = new ElasticaAdapter($journalSearcher, $journalQuery);
+        $pagerfanta = new Pagerfanta($adapter);
+        $pagerfanta->setMaxPerPage(20);
+        $pagerfanta->setCurrentPage($page);
+        $journals = $pagerfanta->getCurrentPageResults();
 
-        return $this->render('OjsSiteBundle:Explore:explore_index.html.twig', $data);
-    }
+        $data = [
+            'journals' => $journals,
+            'pagerfanta' => $pagerfanta
+        ];
 
-    public function institutionsIndexAction()
-    {
-        /** @var EntityManager $em */
-        $em = $this->getDoctrine()->getManager();
-        /** @var InstitutionRepository $repo */
-        $repo = $em->getRepository('OjsJournalBundle:Institution');
-        $data['entities'] = $repo->getAllWithDefaultTranslation();
-        $data['page'] = 'institution';
-
-        return $this->render('OjsSiteBundle::Institution/institutions_index.html.twig', $data);
-    }
-
-
-    public function journalsIndexAction(Request $request, $page, $institution = null)
-    {
-        $searchManager = $this->get('ojs_search_manager');
-        $searchManager->setPage($page);
-        $filter = $request->get('filter', []);
-        if ($institution) {
-            /** @var Institution $institutionObj */
-            $institutionObj = $this->getDoctrine()->getManager()->getRepository(
-                'OjsJournalBundle:Institution'
-            )->findOneBy(['slug' => $institution]);
-            $filter['institution'] = $institutionObj->getId();
-            $data['institutionObject'] = $institutionObj;
-        }
-        if (!empty($filter)) {
-            $searchManager->addFilters($filter);
-        }
-        $result = $searchManager->searchJournal()->getResult();
-        $data['result'] = $result;
-        $data['total_count'] = $searchManager->getCount();
-        $data['page'] = 'journals';
-        $data['current_page'] = $page;
-        $data['page_count'] = $searchManager->getPageCount();
-        $data['aggregations'] = $searchManager->getAggregations();
-        $data['filter'] = $filter;
-
-        return $this->render('OjsSiteBundle::Journal/journals_index.html.twig', $data);
+        return $this->render('OjsSiteBundle:Explore:index.html.twig', $data);
     }
 }
