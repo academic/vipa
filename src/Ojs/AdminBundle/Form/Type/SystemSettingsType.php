ade755303abc7d080ee61c70dc99226fbd994bce
Utku Aydın <utkuaydin34@gmail.com>
2015-08-02 20:12:49 +0300

    Use the plural word, related #828

--- src/Ojs/AdminBundle/Form/Type/SystemSettingsType.php
+++ src/Ojs/AdminBundle/Form/Type/SystemSettingsType.php
@@ -6,7 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class SystemSettingType extends AbstractType
+class SystemSettingsType extends AbstractType
 {
 
     /**
