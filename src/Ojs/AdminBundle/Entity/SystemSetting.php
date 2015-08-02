2d95686fdb52e3d1541527330fcbd003a2120421
Utku Aydın <utkuaydin34@gmail.com>
2015-08-02 20:01:10 +0300

    Create a system settings page and allow disabling registrations and applications, related #828

---
+++ src/Ojs/AdminBundle/Entity/SystemSetting.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Ojs\AdminBundle\Entity;
+
+/**
+ * AdminSystemSetting
+ */
+class SystemSetting
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $value;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    public function __construct($name = null, $value = null, $description = null)
+    {
+        $this->setName($name);
+        $this->setValue($value);
+        $this->setDescription($description);
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return SystemSetting
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set value
+     *
+     * @param string $value
+     *
+     * @return SystemSetting
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+}
+
