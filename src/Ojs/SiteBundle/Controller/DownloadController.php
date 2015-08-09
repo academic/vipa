41f14fa33d3cb3495c2e9813375475d634dceb7c
Ismail BASKIN <ismailbaskin1@gmail.com>
2015-08-10 00:39:43 +0300

    improve download methods

--- src/Ojs/SiteBundle/Controller/DownloadController.php
+++ src/Ojs/SiteBundle/Controller/DownloadController.php
@@ -7,24 +7,42 @@ use Ojs\JournalBundle\Entity\IssueFile;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
-use Symfony\Component\HttpFoundation\Response;
 
 class DownloadController extends Controller
 {
-    private function getUploadDirectory()
-    {
-        return 'uploads';
-    }
-
     public function articleFileAction(ArticleFile $articleFile)
     {
-        $path = $this->getUploadDirectory() . '/articlefiles/' . $articleFile->getFile();
-        return new BinaryFileResponse($path);
+        $fileManager = $this->get('jb_fileuploader.file_history.manager');
+        $rootDir = $this->getParameter('kernel.root_dir');
+        $assetHelper = $this->get('templating.helper.assets');
+        $fileHistory = $fileManager->findOneByFileName($articleFile->getFile());
+
+        $path = $rootDir.'/../web'.$fileManager->getUrl($fileHistory);
+        $path = preg_replace('/\?'.$assetHelper->getVersion().'$/', '', $path);
+        $response = new BinaryFileResponse($path);
+        $response->setContentDisposition(
+            ResponseHeaderBag::DISPOSITION_INLINE,
+            preg_replace('/[[:^print:]]/', '_', $fileHistory->getOriginalName())
+        );
+
+        return $response;
     }
 
     public function issueFileAction(IssueFile $issueFile)
     {
-        $path = $this->getUploadDirectory() . '/journalfiles/' . $issueFile->getFile();
-        return new BinaryFileResponse($path);
+        $fileManager = $this->get('jb_fileuploader.file_history.manager');
+        $rootDir = $this->getParameter('kernel.root_dir');
+        $assetHelper = $this->get('templating.helper.assets');
+        $fileHistory = $fileManager->findOneByFileName($issueFile->getFile());
+
+        $path = $rootDir.'/../web'.$fileManager->getUrl($fileHistory);
+        $path = preg_replace('/\?'.$assetHelper->getVersion().'$/', '', $path);
+        $response = new BinaryFileResponse($path);
+        $response->setContentDisposition(
+            ResponseHeaderBag::DISPOSITION_INLINE,
+            preg_replace('/[[:^print:]]/', '_', $fileHistory->getOriginalName())
+        );
+
+        return $response;
     }
 }
