0aea290643ca2c194a016aec75e83693e2587e5f
Utku Aydın <utkuaydin34@gmail.com>
2015-08-10 19:34:00 +0300

    Create the controller for AdminAnnouncement entity and add the entity to ACL, related #874

--- src/Ojs/AdminBundle/Entity/AdminAnnouncement.php
+++ src/Ojs/AdminBundle/Entity/AdminAnnouncement.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace Ojs\AdminBundle\Entity;
+
 use Ojs\CmsBundle\Entity\Announcement;
+use APY\DataGridBundle\Grid\Mapping\Source;
 
 /**
  * AdminAnnouncement
+ * @Source(columns="id, title, content")
  */
 class AdminAnnouncement extends Announcement
 {
---
+++ src/Ojs/AdminBundle/Form/Type/AnnouncementType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Ojs\AdminBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnnouncementType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('title', 'text')->add('content', 'textarea');
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(['data_class' => 'Ojs\AdminBundle\Entity\AdminAnnouncement']);
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ojs_admin_announcement';
+    }
+}
