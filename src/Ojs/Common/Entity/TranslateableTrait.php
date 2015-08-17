00f655ea75a3392aff9f900b37b60f7c7450490e
Behram ÇELEN <behramcelen@gmail.com>
2015-08-18 01:25:59 +0300

    TranslateableTrait locale variable remove

--- src/Ojs/Common/Entity/TranslateableTrait.php
+++ src/Ojs/Common/Entity/TranslateableTrait.php
@@ -23,41 +23,6 @@ trait TranslateableTrait
     protected $defaultLocale;
 
     /**
-     * Translateable locale field
-     */
-    protected $locale;
-
-    public function setTranslatableLocale($locale)
-    {
-        $this->locale = $locale;
-        return $this;
-    }
-
-    public function getTranslatableLocale()
-    {
-        return $this->locale;
-    }
-
-    /**
-     * Alias for getTranslatableLocale
-     * @return mixed
-     */
-    public function getLocale()
-    {
-        return $this->getTranslatableLocale();
-    }
-
-    /**
-     * Alias for setTranslatableLocale
-     * @param $locale
-     * @return TranslateableTrait
-     */
-    public function setLocale($locale)
-    {
-        return $this->setTranslatableLocale($locale);
-    }
-
-    /**
      * @return mixed
      */
     public function getCurrentLocale()
