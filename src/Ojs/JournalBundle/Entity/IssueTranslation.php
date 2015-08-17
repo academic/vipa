80611528579b524b96b077f555a3cbf3ab02e87a
Behram ÇELEN <behramcelen@gmail.com>
2015-08-17 16:56:12 +0300

    issue entity new object translation system integration, bugfixes

--- src/Ojs/JournalBundle/Entity/IssueTranslation.php
+++ src/Ojs/JournalBundle/Entity/IssueTranslation.php
@@ -3,9 +3,55 @@
 namespace Ojs\JournalBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Gedmo\Translatable\Entity\MappedSuperclass\AbstractPersonalTranslation;
+use Prezent\Doctrine\Translatable\Annotation as Prezent;
+use Prezent\Doctrine\Translatable\Entity\AbstractTranslation;
 
-class IssueTranslation extends AbstractPersonalTranslation
+class IssueTranslation extends AbstractTranslation
 {
-    protected $object;
+    /**
+     * @Prezent\Translatable(targetEntity="Ojs\JournalBundle\Entity\Issue")
+     */
+    protected $translatable;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
 }
