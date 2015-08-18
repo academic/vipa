df10fa4a9484771d5e25ddd6b5e6a3b89e2d7a33
Hüseyin Mert <huseyinin@gmail.com>
2015-08-19 01:10:26 +0300

    getImageOptions removed #887

--- src/Ojs/SiteBundle/Twig/CommonExtension.php
+++ src/Ojs/SiteBundle/Twig/CommonExtension.php
@@ -6,7 +6,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Liip\ImagineBundle\Imagine\Filter\FilterManager;
 use Ojs\Common\Services\OrcidService;
 use Ojs\UserBundle\Entity\User;
-use Symfony\Component\HttpFoundation\Request;
 
 class CommonExtension extends \Twig_Extension
 {
@@ -66,54 +65,11 @@ class CommonExtension extends \Twig_Extension
     public function getFunctions()
     {
         return [
-            new \Twig_SimpleFunction('getImageOptions', [$this, 'getImageOptions']),
             new \Twig_SimpleFunction('orcidLoginUrl', [$this, 'orcidLoginUrl']),
         ];
     }
 
 
-
-    /**
-     * @param $entity
-     * @param $type
-     * @param  null        $filter
-     * @return array|mixed
-     */
-    public function getImageOptions($entity, $type, $filter = null)
-    {
-        $options = $entity->{'get'.ucfirst($type).'Options'}();
-        $options = json_decode($options, true);
-        if (
-            !$options ||
-            ((array_key_exists('height', $options) && $options['height'] < 1) ||
-                (array_key_exists('width', $options) && $options['width'] < 1))
-        ) {
-            $defaultConfig = [
-                'height' => 0,
-                'width' => 0,
-                'x' => 0,
-                'y' => 0,
-            ];
-
-            if ($filter) {
-                $config = $this->filterManager->getFilterConfiguration()->get($filter);
-                if (isset($config['filters']) && isset($config['filters']['crop']) && isset($config['filters']['crop']['size'])) {
-                    $size = $config['filters']['crop']['size'];
-                    $defaultConfig['height'] = $size[1];
-                    $defaultConfig['width'] = $size[0];
-                }
-                if (isset($config['filters']) && isset($config['filters']['crop']) && isset($config['filters']['crop']['start'])) {
-                    $start = $config['filters']['crop']['start'];
-                    $defaultConfig['x'] = $start[0];
-                    $defaultConfig['y'] = $start[1];
-                }
-            }
-
-            return $defaultConfig;
-        }
-
-        return $options;
-    }
 
     /**
      * @return string
