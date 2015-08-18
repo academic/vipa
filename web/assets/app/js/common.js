9c56d43d4647af01414f259ed0de89286f8a2bbb
Hüseyin Mert <huseyinin@gmail.com>
2015-08-19 02:54:54 +0300

    js optimization #887

--- web/assets/app/js/common.js
+++ web/assets/app/js/common.js
@@ -1,98 +1,3 @@
-// to serialize form data as Json object
-$.fn.serializeObject = function () {
-    var o = {};
-    var a = this.serializeArray();
-    $.each(a, function () {
-        if (o[this.name] !== undefined) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
-        } else {
-            o[this.name] = this.value || '';
-        }
-    });
-    return o;
-};
-
-$.fn.setTypeForHook = function () {
-    this.each(function () {
-        this.type = 'source';
-    });
-    return this;
-};
-
-
-/*
- * SIDEBAR MENU
- * ------------
- * This is a custom plugin for the sidebar menu. It provides a tree view.
- *
- * Usage:
- * $(".sidebar).tree();
- *
- * Note: This plugin does not accept any options. Instead, it only requires a class
- *       added to the element that contains a sub-menu.
- *
- * When used with the sidebar, for example, it would look something like this:
- * <ul class='sidebar-menu'>
- *      <li class="treeview active">
- *          <a href="#>Menu</a>
- *          <ul class='treeview-menu'>
- *              <li class='active'><a href=#>Level 1</a></li>
- *          </ul>
- *      </li>
- * </ul>
- *
- * Add .active class to <li> elements if you want the menu to be open automatically
- * on page load. See above for an example.
- */
-(function ($) {
-    "use strict";
-
-    $.fn.tree = function () {
-
-        return this.each(function () {
-            var btn = $(this).children("a").first();
-            var menu = $(this).children(".treeview-menu").first();
-            var isActive = $(this).hasClass('active');
-
-            //initialize already active menus
-            if (isActive) {
-                menu.show();
-                btn.children(".fa-angle-left").first().removeClass("fa-angle-left").addClass("fa-angle-down");
-            }
-            //Slide open or close the menu on link click
-            btn.click(function (e) {
-                e.preventDefault();
-                if (isActive) {
-                    //Slide up to close menu
-                    menu.slideUp();
-                    isActive = false;
-                    btn.children(".fa-angle-down").first().removeClass("fa-angle-down").addClass("fa-angle-left");
-                    btn.parent("li").removeClass("active");
-                } else {
-                    //Slide down to open menu
-                    menu.slideDown();
-                    isActive = true;
-                    btn.children(".fa-angle-left").first().removeClass("fa-angle-left").addClass("fa-angle-down");
-                    btn.parent("li").addClass("active");
-                }
-            });
-
-            /* Add margins to submenu elements to give it a tree look */
-            menu.find("li > a").each(function () {
-                var pad = parseInt($(this).css("margin-left")) + 10;
-
-                $(this).css({"margin-left": pad + "px"});
-            });
-
-        });
-
-    };
-
-}(jQuery));
-
 $(document).ready(function () {
     if ($(".maskissn").length) {
         $(".maskissn").inputmask({
@@ -100,9 +5,6 @@ $(document).ready(function () {
             definitions: {'#': {validator: "[0-9]", cardinality: 1}, 'M': {validator: "[0-9X]", cardinality: 1}}
         });
     }
-    $(".contrastColor").each(function () {
-        $(".contrastColor").css("color", (parseInt($(this).css("backgroundColor"), 16) > 0xffffff / 2) ? 'black' : 'white');
-    });
     $(".validate-form").validationEngine({
         promptPosition: 'inline', validateNonVisibleFields: true,
         updatePromptsPosition: true
@@ -145,23 +47,12 @@ $(document).ready(function () {
     $('.select2-element').select2();
 
 
-    $('a[title]').tooltip();
+    $('a[title],[rel="tooltip"]').tooltip();
     $(".panel-heading.toggle-body").click(function () {
         $(this).next(".panel-body").slideToggle();
     });
 
 
-    //Loads the correct sidebar on window load,
-    //collapses the sidebar on window resize.
-
-    $(window).bind("load resize", function () {
-        //console.log($(this).width());
-        if ($(this).width() < 768) {
-            $('div.sidebar-collapse').addClass('collapse');
-        } else {
-            $('div.sidebar-collapse').removeClass('collapse');
-        }
-    });
     if ($('#issuetree').length > 0 && typeof alternateData != "undefined") {
         $('#issuetree').treeview({
             showTags: true,
@@ -172,29 +63,6 @@ $(document).ready(function () {
             }
         });
     }
-
-    var $btnSets = $('#responsive'),
-        $btnLinks = $btnSets.find('a');
-
-    $btnLinks.click(function (e) {
-        e.preventDefault();
-        $(this).siblings('a.active').removeClass("active");
-        $(this).addClass("active");
-        var index = $(this).index();
-        $("div.user-menu>div.user-menu-content").removeClass("active");
-        $("div.user-menu>div.user-menu-content").eq(index).addClass("active");
-    });
-
-    $("[rel='tooltip']").tooltip();
-
-    $('.view').hover(
-        function () {
-            $(this).find('.caption').slideDown(250); //.fadeIn(250)
-        },
-        function () {
-            $(this).find('.caption').slideUp(250); //.fadeOut(205)
-        }
-    );
 
 
     function deleteSubmit(link) {
