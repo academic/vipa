f497f6b11235596054a5193ce015fd1de076ebcf
Behram ÇELEN <behramcelen@gmail.com>
2015-08-24 03:48:06 +0300

    mail template entity validation check :lock: related #824

--- src/Ojs/JournalBundle/Form/Type/MailTemplateType.php
+++ src/Ojs/JournalBundle/Form/Type/MailTemplateType.php
@@ -38,8 +38,8 @@ class MailTemplateType extends AbstractType
         $resolver->setDefaults(
             array(
                 'data_class' => 'Ojs\JournalBundle\Entity\MailTemplate',
+                'cascade_validation' => true,
                 'attr' => [
-                    'novalidate' => 'novalidate',
                     'class' => 'form-validate',
                 ],
             )
