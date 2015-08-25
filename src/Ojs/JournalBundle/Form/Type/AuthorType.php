a3648484d1162c4451dbdb855bc37c2d3a5d4aa5
Behram ÇELEN <behramcelen@gmail.com>
2015-08-26 00:01:01 +0300

    article submission show errors inline

--- src/Ojs/JournalBundle/Form/Type/AuthorType.php
+++ src/Ojs/JournalBundle/Form/Type/AuthorType.php
@@ -39,7 +39,7 @@ class AuthorType extends AbstractType
         $resolver->setDefaults(
             array(
                 'data_class' => 'Ojs\JournalBundle\Entity\Author',
-                'cascade_validatio' => true,
+                'cascade_validation' => true,
                 'attr' => [
                     'class' => 'form-validate',
                 ],
