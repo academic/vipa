e00ad4ec6afb939d88654f6f4d781b74906cc1a7
Utku Aydın <utkuaydin34@gmail.com>
2015-08-30 16:04:17 +0300

    Improve users field type

--- src/Ojs/UserBundle/Resources/public/js/user.js
+++ src/Ojs/UserBundle/Resources/public/js/user.js
@@ -1,29 +1,38 @@
 $(document).ready(function () {
-    $('.usersType').select2({
-        ajax: {
-            data: function (params) {
-                return {
-                    q: params.term
-                };
-            },
-            processResults: function (data) {
-                return {
-                    results: data
-                };
-            },
-            cache: true
-        },
-        templateResult: function (user) {
-            if (user.loading) {
-                return user.text;
-            }
-            return user.username + ' ( ' + user.first_name + ' ' + user.last_name + ' ~ ' + user.email + ' )';
-        },
-        templateSelection: function (user) {
-            if(!user.username) {
-                return user.text;
-            }
-            return user.username + ' ( ' + user.first_name + ' ' + user.last_name + ' ~ ' + user.email + ' )';
+    $.fn.usersType = function(action) {
+        if(action){
+            this.select2(action);
+            return this;
         }
-    });
+        this.select2({
+            ajax: {
+                data: function (params) {
+                    return {
+                        q: params.term
+                    };
+                },
+                processResults: function (data) {
+                    return {
+                        results: data
+                    };
+                },
+                cache: true
+            },
+            templateResult: function (user) {
+                if (user.loading) {
+                    return user.text;
+                }
+                return user.username + ' ( ' + user.first_name + ' ' + user.last_name + ' ~ ' + user.email + ' )';
+            },
+            templateSelection: function (user) {
+                if(!user.username) {
+                    return user.text;
+                }
+                return user.username + ' ( ' + user.first_name + ' ' + user.last_name + ' ~ ' + user.email + ' )';
+            }
+        });
+        return this;
+    };
+
+    $('.usersType').usersType();
 });
