3346716373ae1687b17a3f8da4e1fcbd7fa27f8a
Utku Aydın <utkuaydin34@gmail.com>
2015-09-02 22:20:49 +0300

    Add icon support, related #973

--- src/Ojs/JournalBundle/Menu/MenuBuilder.php
+++ src/Ojs/JournalBundle/Menu/MenuBuilder.php
@@ -30,32 +30,39 @@ class MenuBuilder extends ContainerAware
         }
 
         $items = [
-            // [field, label, options]
-            ['submissionSettings', 'title.journal_settings_submission', array('route' => 'ojs_journal_settings_submission')],
-            ['mailSettings',       'title.journal_settings_mail',       array('route' => 'ojs_journal_settings_mail')],
-            ['checklist',          'title.submission_checklists',       array('route' => 'ojs_journal_checklist_index')],
-            ['file',               'title.submission_files',            array('route' => 'ojs_journal_file_index')],
-            ['userRole',           'title.users',                       array('route' => 'ojs_journal_user_index')],
-            ['index',              'title.journal_indexes',             array('route' => 'ojs_journal_index_index')],
-            ['articles',           'title.articles',                    array('route' => 'ojs_journal_article_index')],
-            ['sections',           'title.journal_sections',            array('route' => 'ojs_journal_section_index')],
-            ['issues',             'title.issues',                      array('route' => 'ojs_journal_issue_index')],
-            ['contacts',           'title.contacts',                    array('route' => 'ojs_journal_journal_contact_index')],
-            ['issues',             'title.issues',                      array('route' => 'ojs_journal_issue_index')],
-            ['mailTemplates',      'title.title.mail_templates',        array('route' => 'ojs_journal_mail_template_index')],
-            ['designs',            'title.designs',                     array('route' => 'ojs_journal_design_index')],
-            ['theme',              'title.themes',                      array('route' => 'ojs_journal_theme_index')],
-            ['boards',             'title.boards',                      array('route' => 'ojs_journal_board_index')],
-            ['steps',              'workflow.steps',                    array('route' => 'okul_bilisim_workflow_step_index')],
-            ['announcements',      'title.announcements',               array('route' => 'ojs_journal_announcement_index')],
-            ['pages',              'title.pages',                       array('route' => 'ojs_journal_page_index')],
-            ['posts',              'title.posts',                       array('route' => 'ojs_journal_post_index')],
+            // [field, label, route, icon]
+            ['submissionSettings', 'title.journal_settings_submission', 'ojs_journal_settings_submission',   'paper-plane'],
+            ['mailSettings',       'title.journal_settings_mail',       'ojs_journal_settings_mail',         'envelope'],
+            ['checklist',          'title.submission_checklists',       'ojs_journal_checklist_index',       'list'],
+            ['file',               'title.submission_files',            'ojs_journal_file_index',            'file'],
+            ['userRole',           'title.users',                       'ojs_journal_user_index',            'key'],
+            ['index',              'title.journals_indexes',            'ojs_journal_index_index',           'sitemap'],
+            ['issues',             'title.issues',                      'ojs_journal_issue_index',           'newspaper-o'],
+            ['sections',           'title.journal_sections',            'ojs_journal_section_index',         'folder'],
+            ['articles',           'title.articles',                    'ojs_journal_article_index',         'file-text'],
+            ['contacts',           'title.contacts',                    'ojs_journal_journal_contact_index', 'users'],
+            ['mailTemplates',      'title.title.mail_templates',        'ojs_journal_mail_template_index',   'envelope'],
+            ['design',             'title.designs',                     'ojs_journal_design_index',          'bars'],
+            ['theme',              'title.themes',                      'ojs_journal_theme_index',           'paint-brush'],
+            ['boards',             'title.boards',                      'ojs_journal_board_index',           'object-group'],
+            ['steps',              'workflow.steps',                    'okul_bilisim_workflow_step_index',  'random'],
+            ['announcements',      'title.announcements',               'ojs_journal_announcement_index',    'bullhorn'],
+            ['pages',              'title.pages',                       'ojs_journal_page_index',            'file'],
+            ['posts',              'title.posts',                       'ojs_journal_post_index',            'file-o'],
         ];
 
         foreach ($items as $item) {
-            $item[2]['routeParameters'] = ['journalId' => $journalId];
-            if ($checker->isGranted('VIEW', $journal, $item[0])) {
-                $menu->addChild($item[1], $item[2]);
+            $field = $item[0];
+            $label = $item[1];
+            $path  = $item[2];
+            $icon  = $item[3];
+
+            if ($checker->isGranted('VIEW', $journal, $field)) {
+                $menu->addChild($label, [
+                    'route'           => $path,
+                    'routeParameters' => ['journalId' => $journalId],
+                    'extras'          => ['icon'      => $icon]
+                ]);
             }
         }
 
