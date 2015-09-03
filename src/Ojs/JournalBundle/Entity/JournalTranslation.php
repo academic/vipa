b7ac9d6cc91dc3a336a56dc19edd83dac8741c02
Behram ÇELEN <behramcelen@gmail.com>
2015-09-03 03:35:43 +0300

    journal entity crud show template display integration :art: #990

--- src/Ojs/JournalBundle/Entity/JournalTranslation.php
+++ src/Ojs/JournalBundle/Entity/JournalTranslation.php
@@ -3,11 +3,13 @@
 namespace Ojs\JournalBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Ojs\CoreBundle\Entity\DisplayTrait;
 use Prezent\Doctrine\Translatable\Annotation as Prezent;
 use Prezent\Doctrine\Translatable\Entity\AbstractTranslation;
 
 class JournalTranslation extends AbstractTranslation
 {
+    use DisplayTrait;
     /**
      * @Prezent\Translatable(targetEntity="Ojs\JournalBundle\Entity\Journal")
      */
