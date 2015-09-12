3605957bce1436539467d696ddd20e07eb21acd6
Behram ÇELEN <behramcelen@gmail.com>
2015-09-12 23:54:17 +0300

    journal dashboard top left menu knpmenubundle integration :art: fixes #1044

--- src/Ojs/JournalBundle/JournalEvents.php
+++ src/Ojs/JournalBundle/JournalEvents.php
@@ -5,5 +5,6 @@ namespace Ojs\JournalBundle;
 final class JournalEvents
 {
     const LEFT_MENU_INITIALIZED = 'ojs.journal.event.left_menu_initialized';
+    const TOP_LEFT_MENU_INITIALIZED = 'ojs.journal.event.top_left_menu_initialized';
     const ARTICLE_LIST_INITIALIZED = 'ojs.journal.event.article.list';
 }
