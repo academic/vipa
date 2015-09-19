5afb4a92716560a732c4c63c643ee0cdb0132da8
Hüseyin Mert <huseyinin@gmail.com>
2015-09-20 01:13:34 +0300

    Admin Events #989

---
+++ src/Ojs/AdminBundle/Events/AdminEvents.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ojs\AdminBundle\Event;
+
+
+final class AdminEvents
+{
+    const USER_PASS_CHANGE = 'ojs.admin.user.password.change';
+    const USER_CHANGE = 'ojs.admin.user.change';
+    const JOURNAL_CONTACT_CHANGE = 'ojs.admin.journal.contact.change';
+    const JOURNAL_APPLICATION_HAPPEN = 'ojs.admin.journal.application.happen';
+    const JOURNAL_CHANGE = 'ojs.admin.journal.change';
+    const PUBLISHER_APPLICATION_HAPPEN = 'ojs.admin.publisher.application.happen';
+    const PUBLISHER_MANAGER_CHANGE = 'ojs.admin.publisher.manager.change';
+    const PUBLISHER_CHANGE = 'ojs.admin.publisher.change';
+    const SUBJECT_CHANGE = 'ojs.admin.subject.change';
+    const SETTINGS_CHANGE = 'ojs.admin.settings.change';
+}
