9ca13eba23a017fd78635d0898a54630eee5dd84
Behram ÇELEN <behramcelen@gmail.com>
2015-09-29 10:49:11 +0300

    journal contact entity api integration with full support, :bug: fixes related #844

--- src/Ojs/ApiBundle/Tests/Controller/ContactRestControllerTest.php
+++ src/Ojs/ApiBundle/Tests/Controller/ContactRestControllerTest.php
@@ -6,9 +6,109 @@ use Ojs\CoreBundle\Tests\BaseTestCase;
 
 class ContactRestControllerTest extends BaseTestCase
 {
-    public function testGetContacts()
+    public function testGetContactsAction()
     {
-        $response = $this->apiRequest('/api/contacts');
+        $url = $this->router->generate('api_1_get_contacts');
+        $this->client->request('GET', $url);
+        $response = $this->client->getResponse();
+         var_dump($response->getContent());
         $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testNewContactAction()
+    {
+        $content = [
+            'title' => '',
+            'fullName' => 'PHPUnit Test Full Name Field en - POST',
+            'address' => 'PHPUnit Test Address Field en - POST',
+            'phone' => 'PHPUnit Test Phone Field en - POST',
+            'email' => 'PHPUnit Test Email Field en - POST',
+            'tags' => ['phpunit'],
+            'contactType' => 4,
+            'journal' => 1,
+            'country' => 216,
+            'city' => 1419
+        ];
+        $url = $this->router->generate('api_1_get_contacts');
+        $this->client->request(
+            'POST',
+            $url,
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($content)
+        );
+        $response = $this->client->getResponse();
+        var_dump($response->getContent());
+        $this->assertEquals(201, $response->getStatusCode());
+    }
+
+    public function testGetContactAction()
+    {
+        $url = $this->router->generate('api_1_get_contact', ['id'=> 1]);
+        $this->client->request('GET', $url);
+        $response = $this->client->getResponse();
+        var_dump($response->getContent());
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testPutContactAction()
+    {
+        $content = [
+            'title' => '',
+            'fullName' => 'PHPUnit Test Full Name Field en - PUT',
+            'address' => 'PHPUnit Test Address Field en - PUT',
+            'phone' => 'PHPUnit Test Phone Field en - PUT',
+            'email' => 'PHPUnit Test Email Field en - PUT',
+            'tags' => ['phpunit', 'PUT'],
+            'contactType' => 4,
+            'journal' => 1,
+            'country' => 216,
+            'city' => 1419
+        ];
+        $url = $this->router->generate('api_1_put_contact', ['id' => 550]);
+        $this->client->request(
+            'PUT',
+            $url,
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($content)
+        );
+        $response = $this->client->getResponse();
+        var_dump($response->getContent());
+        $this->assertEquals(201, $response->getStatusCode());
+    }
+
+    public function testPatchContactAction()
+    {
+        $content = [
+            'address' => 'PHPUnit Test Address Field en - PUT',
+            'email' => 'PHPUnit Test Email Field en - PUT',
+        ];
+        $url = $this->router->generate('api_1_patch_contact', ['id' => 1]);
+        $this->client->request(
+            'PATCH',
+            $url,
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($content)
+        );
+        $response = $this->client->getResponse();
+        var_dump($response->getContent());
+        $this->assertEquals(204, $response->getStatusCode());
+    }
+
+    public function testDeleteContactAction()
+    {
+        $url = $this->router->generate('api_1_delete_contact', ['id' => 1]);
+        $this->client->request(
+            'DELETE',
+            $url
+        );
+        $response = $this->client->getResponse();
+        var_dump($response->getContent());
+        $this->assertEquals(204, $response->getStatusCode());
     }
 }
