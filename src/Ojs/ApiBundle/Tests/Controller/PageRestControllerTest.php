d7578346e73b18d79219e9addc5cc588a1285ebe
Behram ÇELEN <behramcelen@gmail.com>
2015-09-29 11:32:22 +0300

    page entity api system tests full support related #844

---
+++ src/Ojs/ApiBundle/Tests/Controller/PageRestControllerTest.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Ojs\ApiBundle\Tests\Controller;
+
+use Ojs\CoreBundle\Tests\BaseTestCase;
+
+class PageRestControllerTest extends BaseTestCase
+{
+    public function testGetPagesAction()
+    {
+        $url = $this->router->generate('api_1_get_pages');
+        $this->client->request('GET', $url);
+        $response = $this->client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testNewPageAction()
+    {
+        $content = [
+            'translations' => [
+                'en' => [
+                    'title' => 'PHPUnit Test Title Field en - POST',
+                    'body' => 'PHPUnit Test Body Field en - POST',
+                ]
+            ],
+            'tags' => ['phpunit', 'post']
+        ];
+        $url = $this->router->generate('api_1_get_pages');
+        $this->client->request(
+            'POST',
+            $url,
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($content)
+        );
+        $response = $this->client->getResponse();
+        $this->assertEquals(201, $response->getStatusCode());
+    }
+
+    public function testGetPageAction()
+    {
+        $url = $this->router->generate('api_1_get_page', ['id'=> 1]);
+        $this->client->request('GET', $url);
+        $response = $this->client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testPutPageAction()
+    {
+        $content = [
+            'translations' => [
+                'en' => [
+                    'title' => 'PHPUnit Test Title Field en - PUT',
+                    'body' => 'PHPUnit Test Body Field en - PUT',
+                ]
+            ],
+            'tags' => ['phpunit', 'put']
+        ];
+        $url = $this->router->generate('api_1_put_page', ['id' => 550]);
+        $this->client->request(
+            'PUT',
+            $url,
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($content)
+        );
+        $response = $this->client->getResponse();
+        $this->assertEquals(201, $response->getStatusCode());
+    }
+
+    public function testPatchPageAction()
+    {
+        $content = [
+            'translations' => [
+                'tr' => [
+                    'title' => 'PHPUnit Test Title Field TR - PATCH',
+                    'body' => 'PHPUnit Test Body Field TR - PATCH',
+                ]
+            ]
+        ];
+        $url = $this->router->generate('api_1_patch_page', ['id' => 1]);
+        $this->client->request(
+            'PATCH',
+            $url,
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($content)
+        );
+        $response = $this->client->getResponse();
+        $this->assertEquals(204, $response->getStatusCode());
+    }
+
+    public function testDeletePageAction()
+    {
+        $url = $this->router->generate('api_1_delete_page', ['id' => 1]);
+        $this->client->request(
+            'DELETE',
+            $url
+        );
+        $response = $this->client->getResponse();
+        $this->assertEquals(204, $response->getStatusCode());
+    }
+}
