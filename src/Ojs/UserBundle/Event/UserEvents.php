7ef4bc1238c20370950924f72ff9eea121f0e5a6
Utku Aydın <utkuaydin34@gmail.com>
2015-10-15 13:13:26 +0300

    Change user event values

--- src/Ojs/UserBundle/Event/UserEvents.php
+++ src/Ojs/UserBundle/Event/UserEvents.php
@@ -3,11 +3,11 @@ namespace Ojs\UserBundle\Event;
 
 final class UserEvents
 {
-    const USER_INFO_CHANGE = 'ojs.user.info.change';
-    const USER_PASSWORD_CHANGE = 'ojs.user.password.change';
-    const USER_PASSWORD_RESET = 'ojs.user.password.reset.happen';
-    const USER_REGISTER = 'ojs.user.register.happen';
-    const USER_LOGIN = 'ojs.user.login.happen';
-    const USER_LOGOUT = 'ojs.user.logout.happen';
+    const USER_INFO_CHANGE = 'ojs.user.info_changed';
+    const USER_PASSWORD_CHANGE = 'ojs.user.password_changed';
+    const USER_PASSWORD_RESET = 'ojs.user.password_reset';
+    const USER_REGISTER = 'ojs.user.registered';
+    const USER_LOGIN = 'ojs.user.logged_in';
+    const USER_LOGOUT = 'ojs.user.logged_out';
 }
 
