ba9cec1eb16ac9dfa326756ae9e08e8a896eb1eb
Utku Aydın <utkuaydin34@gmail.com>
2015-10-15 15:24:07 +0300

    Add formatMilliseconds method to StringHelper

--- src/Ojs/CoreBundle/Helper/StringHelper.php
+++ src/Ojs/CoreBundle/Helper/StringHelper.php
@@ -114,4 +114,17 @@ class StringHelper {
 
         return round($bytes, $precision) . ' ' . $units[$pow];
     }
+
+    public static function formatMilliseconds($milliseconds) {
+        $seconds = floor($milliseconds / 1000);
+        $minutes = floor($seconds / 60);
+        $hours = floor($minutes / 60);
+        $milliseconds = $milliseconds % 1000;
+        $seconds = $seconds % 60;
+        $minutes = $minutes % 60;
+
+        $format = '%u:%02u:%02u.%03u';
+        $time = sprintf($format, $hours, $minutes, $seconds, $milliseconds);
+        return rtrim($time, '0');
+    }
 } 
