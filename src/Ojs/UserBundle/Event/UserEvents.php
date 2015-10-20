c28bea8cdc98119e03ddfecbfd91dae054e7b7d8
Behram ÇELEN <behramcelen@gmail.com>
2015-10-20 14:09:50 +0300

    fos user bundle already have this type events related #1230

--- src/Ojs/UserBundle/Event/UserEvents.php
+++ src/Ojs/UserBundle/Event/UserEvents.php
@@ -4,10 +4,7 @@ namespace Ojs\UserBundle\Event;
 
 final class UserEvents
 {
-    const USER_INFO_CHANGE = 'ojs.user.info_changed';
-    const USER_PASSWORD_CHANGE = 'ojs.user.password_changed';
     const USER_PASSWORD_RESET = 'ojs.user.password_reset';
-    const USER_REGISTER = 'ojs.user.registered';
     const USER_LOGIN = 'ojs.user.logged_in';
     const USER_LOGOUT = 'ojs.user.logged_out';
 }
