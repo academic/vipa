e06a7a0e2e5d8c5f63cd33abd4dfa810475b77d6
Behram ÇELEN <behramcelen@gmail.com>
2015-10-28 17:39:59 +0200

    dispatch user profile show event and minor :bug: fixes

--- src/Ojs/CoreBundle/Events/TwigEvents.php
+++ src/Ojs/CoreBundle/Events/TwigEvents.php
@@ -5,4 +5,6 @@ namespace Ojs\CoreBundle\Events;
 class TwigEvents
 {
     const OJS_USER_PROFILE_EDIT_TABS = 'ojs.user.profile.edit.tabs';
+    const OJS_USER_PROFILE_PUBLIC_VIEW = 'ojs.user.profile.public.view';
+    const OJS_USER_PROFILE_PUBLIC_VIEW_SCRIPT = 'ojs.user.profile.public.view.script';
 }
