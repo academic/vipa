2a5215f2fc8c1024d6e6540be7b1ba17e9aa54b6
Behram ÇELEN <behramcelen@gmail.com>
2015-11-04 00:34:25 +0200

    show article stats on pie for users fixes #1272

--- src/Ojs/JournalBundle/Controller/DashboardController.php
+++ src/Ojs/JournalBundle/Controller/DashboardController.php
@@ -8,6 +8,29 @@ class DashboardController extends OjsController
 {
     public function indexAction()
     {
-        return $this->render('OjsJournalBundle:Dashboard:dashboard.html.twig');
+        return $this->render('OjsJournalBundle:Dashboard:dashboard.html.twig', [
+            'articlePieStats' => $this->getArticlePieStats()
+        ]);
+    }
+
+    /**
+     * @return array
+     */
+    private function getArticlePieStats()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $userArticles = $em->getRepository('OjsJournalBundle:Article')->findBy([
+           'submitterUser' => $this->getUser()
+        ]);
+        $stats = [];
+        foreach($userArticles as $article){
+            $articleStatus = $article->getStatusText();
+            if(isset($stats[$articleStatus])){
+                $stats[$articleStatus]++;
+            }else{
+                $stats[$articleStatus] = 1;
+            }
+        }
+        return $stats;
     }
 }
