2b2b11a1febc3c104d2184816685c9f9f57772c2
Behram ÇELEN <behramcelen@gmail.com>
2015-11-03 17:12:01 +0200

    dispatch twig event on user page article table render event :art: fixes #1270

--- src/Ojs/CoreBundle/Events/TwigEvents.php
+++ src/Ojs/CoreBundle/Events/TwigEvents.php
@@ -7,4 +7,5 @@ class TwigEvents
     const OJS_USER_PROFILE_EDIT_TABS = 'ojs.user.profile.edit.tabs';
     const OJS_USER_PROFILE_PUBLIC_VIEW = 'ojs.user.profile.public.view';
     const OJS_USER_PROFILE_PUBLIC_VIEW_SCRIPT = 'ojs.user.profile.public.view.script';
+    const OJS_USER_ARTICLE_ACTIONS = 'ojs.core.user.article.actions';
 }
