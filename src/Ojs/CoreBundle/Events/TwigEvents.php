177d81835f416aecf5330c06b92d595d1f05fedd
Utku Aydın <utkuaydin34@gmail.com>
2015-11-09 15:27:47 +0200

    Dispatch an event when displaying citations in submission preview page

--- src/Ojs/CoreBundle/Events/TwigEvents.php
+++ src/Ojs/CoreBundle/Events/TwigEvents.php
@@ -8,4 +8,5 @@ class TwigEvents
     const OJS_USER_PROFILE_PUBLIC_VIEW = 'ojs.user.profile.public.view';
     const OJS_USER_PROFILE_PUBLIC_VIEW_SCRIPT = 'ojs.user.profile.public.view.script';
     const OJS_USER_ARTICLE_ACTIONS = 'ojs.core.user.article.actions';
+    const OJS_SUBMISSION_CITATION_VIEW = 'ojs.submision.citation.view';
 }
