6a2dbb6ce7539b0a4d0df0a5ed09c4a3dacb2533
Behram ÇELEN <behramcelen@gmail.com>
2015-11-17 12:06:54 +0200

    show only boards own members on boards show process related #1319

--- src/Ojs/JournalBundle/Controller/BoardController.php
+++ src/Ojs/JournalBundle/Controller/BoardController.php
@@ -20,6 +20,7 @@ use Symfony\Component\Security\Core\Exception\TokenNotFoundException;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Ojs\JournalBundle\Event\JournalEvent;
 use Ojs\JournalBundle\Event\JournalEvents;
+use Doctrine\ORM\QueryBuilder;
 
 /**
  * Board controller.
@@ -174,8 +175,16 @@ class BoardController extends Controller
             ->refreshToken('ojs_journal_board'.$board->getId());
 
         $source = new Entity('OjsJournalBundle:BoardMember');
-
+        $alias = $source->getTableAlias();
+        $source->manipulateQuery(
+            function (QueryBuilder $query) use ($alias, $board) {
+                $query
+                    ->andWhere($alias.'.board = :board')
+                    ->setParameter('board', $board);
+            }
+        );
         $membersGrid = $this->get('grid')->setSource($source);
+
         $gridAction = $this->get('grid_action');
 
         $actionColumn = new ActionsColumn("actions", 'actions');
