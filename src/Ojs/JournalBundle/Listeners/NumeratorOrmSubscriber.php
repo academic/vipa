010596b9219851a8973c9a29a301963c8168673a
Utku Aydın <utkuaydin34@gmail.com>
2015-11-19 22:27:22 +0200

    Create NumeratorHelper and move article numeration logic to there

--- src/Ojs/JournalBundle/Listeners/NumeratorOrmSubscriber.php
+++ src/Ojs/JournalBundle/Listeners/NumeratorOrmSubscriber.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\Events;
 use Doctrine\ORM\NoResultException;
 use Ojs\JournalBundle\Entity\Article;
 use Ojs\JournalBundle\Entity\Numerator;
+use Ojs\JournalBundle\Helper\NumeratorHelper;
 
 class NumeratorOrmSubscriber implements EventSubscriber
 {
@@ -22,25 +23,7 @@ class NumeratorOrmSubscriber implements EventSubscriber
         $entityManager = $args->getObjectManager();
 
         if ($entity instanceof Article) {
-            $journal = $entity->getJournal();
-            try {
-                $numerator = $entityManager
-                    ->getRepository('OjsJournalBundle:Numerator')
-                    ->getArticleNumerator($journal);
-                $last = $numerator->getLast() + 1;
-                $numerator->setLast($last);
-                $entity->setNumerator($last);
-            } catch (NoResultException $exception) {
-                $numerator = new Numerator();
-                $numerator->setJournal($journal);
-                $numerator->setType('article');
-                $numerator->setLast(1);
-                $entity->setNumerator(1);
-            }
-
-            $entityManager->persist($entity);
-            $entityManager->persist($numerator);
-            $entityManager->flush();
+            NumeratorHelper::numerateArticle($entity, $entityManager);
         }
     }
 }
