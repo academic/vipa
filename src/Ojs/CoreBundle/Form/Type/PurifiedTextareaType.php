afd6cfd91838c3784894a97987826509dae0356f
Hüseyin Mert <huseyinin@gmail.com>
2015-11-28 21:58:57 +0200

    purifier bundle enabled

---
+++ src/Ojs/CoreBundle/Form/Type/PurifiedTextareaType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ojs\CoreBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PurifiedTextareaType extends AbstractType
+{
+    private $purifierTransformer;
+
+    public function __construct(DataTransformerInterface $purifierTransformer)
+    {
+        $this->purifierTransformer = $purifierTransformer;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addViewTransformer($this->purifierTransformer);
+    }
+
+    public function getParent()
+    {
+        return 'textarea';
+    }
+
+    public function setDefault(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'compound' => false,
+        ));
+    }
+
+    public function getName()
+    {
+        return 'purified_textarea';
+    }
+}
