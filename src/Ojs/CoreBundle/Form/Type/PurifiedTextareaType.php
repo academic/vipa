c4d76c52973d9adfc41ff608e86a79b92eeadcee
Behram ÇELEN <behramcelen@gmail.com>
2015-12-03 16:10:45 +0200

    revise purified text type form service related #1236

--- src/Ojs/CoreBundle/Form/Type/PurifiedTextareaType.php
+++ src/Ojs/CoreBundle/Form/Type/PurifiedTextareaType.php
@@ -4,19 +4,11 @@ namespace Ojs\CoreBundle\Form\Type;
 
 use Ojs\CoreBundle\Form\DataTransformer\PurifiedTextareaTransformer;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\DataTransformerInterface;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PurifiedTextareaType extends AbstractType
 {
-    private $purifierTransformer;
-
-    public function __construct(DataTransformerInterface $purifierTransformer)
-    {
-        $this->purifierTransformer = $purifierTransformer;
-    }
-
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->addViewTransformer(new PurifiedTextareaTransformer(), true);
