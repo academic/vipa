7c933e2511230d18de6a8f223c0174a7d9c0cf6e
Behram ÇELEN <behramcelen@gmail.com>
2015-12-03 04:18:28 +0200

    remove unused user rest controller related #1080, rollback changed dev. env. configs

--- web/app_dev.php
+++ web/app_dev.php
@@ -22,7 +22,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
  **/
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-//Debug::enable();
+Debug::enable();
 
 require_once __DIR__.'/../app/AppKernel.php';
 
