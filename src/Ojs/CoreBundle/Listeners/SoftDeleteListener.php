9a19698651b78c09195932f1ce0de6b5773dae8e
Utku Aydın <utkuaydin34@gmail.com>
2015-12-23 01:33:41 +0200

    Make articles deletable

--- src/Ojs/CoreBundle/Listeners/SoftDeleteListener.php
+++ src/Ojs/CoreBundle/Listeners/SoftDeleteListener.php
@@ -5,31 +5,33 @@ namespace Ojs\CoreBundle\Listeners;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Mapping\ClassMetadataInfo;
 use Ojs\CoreBundle\Exception\ChildNotEmptyException;
+use Ojs\JournalBundle\Entity\Article;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 
 class SoftDeleteListener
 {
     private $excludedParents = [
         'Prezent\Doctrine\Translatable\Entity\AbstractTranslation',
-        'Ojs\AnalyticsBundle\Entity\Statistic'
+        'Ojs\AnalyticsBundle\Entity\Statistic',
     ];
 
     public function preSoftDelete(LifecycleEventArgs $args)
     {
-        $this->checkRelations($args);
+        if (!$args->getEntity() instanceof Article) {
+            $this->checkRelations($args);
+        }
     }
 
     protected function checkRelations(LifecycleEventArgs $args)
     {
         $entity = $args->getEntity();
         $entityManager = $args->getEntityManager();
-
         $mappings = $entityManager->getClassMetadata(get_class($entity))->getAssociationMappings();
+
         foreach ($mappings as $mapping) {
-            $type = $mapping['type'];
-            if ($type === ClassMetadataInfo::ONE_TO_MANY || $type === ClassMetadataInfo::MANY_TO_MANY) {
+            if ($mapping['type'] === ClassMetadataInfo::ONE_TO_MANY || $mapping['type'] === ClassMetadataInfo::MANY_TO_MANY) {
                 $targetEntityMeta = $entityManager->getClassMetadata($mapping['targetEntity']);
-                if($targetEntityMeta->reflClass->getParentClass()){
+                if ($targetEntityMeta->reflClass->getParentClass()) {
                     if (in_array($targetEntityMeta->reflClass->getParentClass()->name, $this->excludedParents)) {
                         continue;
                     }
@@ -37,9 +39,11 @@ class SoftDeleteListener
 
                 $accessor = PropertyAccess::createPropertyAccessor();
                 $value = $accessor->getValue($entity, $mapping['fieldName']);
+
                 if (count($value) === 0) {
                     continue;
                 }
+
                 $exception = new ChildNotEmptyException();
                 $exception->setEntity($entity);
                 $exception->setMapping($mapping);
@@ -50,6 +54,8 @@ class SoftDeleteListener
 
     public function preRemove(LifecycleEventArgs $args)
     {
-        $this->checkRelations($args);
+        if (!$args->getEntity() instanceof Article) {
+            $this->checkRelations($args);
+        }
     }
 }
