8125c0b1ec0d02b4f5554c7ffce07867dc1fc572
Behram ÇELEN <behramcelen@gmail.com>
2015-12-28 12:17:17 +0200

    remove deprecated api journal users function

--- src/Ojs/ApiBundle/Controller/Admin/JournalRestController.php
+++ src/Ojs/ApiBundle/Controller/Admin/JournalRestController.php
@@ -266,41 +266,4 @@ class JournalRestController extends FOSRestController
         }
         return $entity;
     }
-
-    /**
-     * @todo if action not using we can remove this
-     * @ApiDoc(
-     *  resource=true,
-     *  description="Get Specific Journal Of Users Action",
-     *  parameters={
-     *      {
-     *          "name"="page",
-     *          "dataType"="integer",
-     *          "required"="true",
-     *          "description"="offset page"
-     *      },
-     *      {
-     *          "name"="limit",
-     *          "dataType"="integer",
-     *          "required"="true",
-     *          "description"="limit"
-     *      }
-     *  }
-     * )
-     * @Get("/journal/{id}/users")
-     *
-     * @param  Request $request
-     * @param $id
-     * @return mixed
-     */
-    public function getJournalUsersAction(Request $request, $id)
-    {
-        $limit = $request->get('limit');
-        $page = (int) $request->get('page'); // page is not a mandotary parameter
-        if (empty($limit)) {
-            throw new HttpException(400, 'Missing parameter : limit');
-        }
-
-        return $this->get('ojs.journal_service')->getUsers($id, $page, $limit);
-    }
 }
