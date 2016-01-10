9bd187fd87dcba9a7062c1c2d0d0f182abb5c6b6
Utku Aydın <utkuaydin34@gmail.com>
2016-01-10 17:35:12 +0200

    add php doc

--- src/Ojs/JournalBundle/Entity/JournalItemInterface.php
+++ src/Ojs/JournalBundle/Entity/JournalItemInterface.php
@@ -6,7 +6,14 @@ interface JournalItemInterface
 {
     public function getId();
 
+    /**
+     * @return Journal
+     */
     public function getJournal();
 
+    /**
+     * @param Journal $journal
+     * @return Journal
+     */
     public function setJournal(Journal $journal);
 }
