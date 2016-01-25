5f40c3b420120d8ab927efaf106c7cc4d50e8350
Behram ÇELEN <behramcelen@gmail.com>
2016-01-25 03:06:22 +0200

    create admin journal theme entity

---
+++ src/Ojs/AdminBundle/Entity/AdminJournalTheme.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Ojs\AdminBundle\Entity;
+
+use APY\DataGridBundle\Grid\Mapping as GRID;
+use Ojs\CoreBundle\Entity\GenericEntityTrait;
+use Prezent\Doctrine\Translatable\Annotation as Prezent;
+
+/**
+ * AdminJournalTheme
+ * @GRID\Source(columns="id,title")
+ */
+class AdminJournalTheme
+{
+    use GenericEntityTrait;
+
+    /**
+     * @var integer
+     * @GRID\Column(title="id")
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $css;
+
+    /**
+     * @var boolean
+     */
+    private $public = true;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCss()
+    {
+        return $this->css;
+    }
+
+    /**
+     * @param string $css
+     */
+    public function setCss($css)
+    {
+        $this->css = $css;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isPublic()
+    {
+        return $this->public;
+    }
+
+    /**
+     * @param boolean $public
+     */
+    public function setPublic($public)
+    {
+        $this->public = $public;
+    }
+}
