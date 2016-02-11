f9442c93c2705d2e79b64496c1088a2b17bb8d46
Behram ÇELEN <behramcelen@gmail.com>
2016-02-11 04:23:35 +0200

    enable delete service check on journal theme

--- src/Ojs/JournalBundle/Controller/JournalThemeController.php
+++ src/Ojs/JournalBundle/Controller/JournalThemeController.php
@@ -278,6 +278,7 @@ class JournalThemeController extends Controller
         if ($token != $request->get('_token')) {
             throw new TokenNotFoundException("Token Not Found!");
         }
+        $this->get('ojs_core.delete.service')->check($entity);
         $em->remove($entity);
         $em->flush();
         $this->successFlashBag('successful.remove');
