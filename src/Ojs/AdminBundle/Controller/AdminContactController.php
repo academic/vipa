1ac1d4f003c5aacea03c2cec3de6750f84ac2708
Behram ÇELEN <behramcelen@gmail.com>
2016-02-11 06:18:57 +0200

    enable delete service check on admin contacts

--- src/Ojs/AdminBundle/Controller/AdminContactController.php
+++ src/Ojs/AdminBundle/Controller/AdminContactController.php
@@ -326,7 +326,7 @@ class AdminContactController extends Controller
         if ($token != $request->get('_token')) {
             throw new TokenNotFoundException("Token Not Found!");
         }
-        
+        $this->get('ojs_core.delete.service')->check($entity);
         $em->remove($entity);
         $em->flush();
         $this->successFlashBag('successful.remove');
