c2474b7dd8b266d8730d44c8568ad4ecb63d59d9
Behram ÇELEN <behramcelen@gmail.com>
2016-02-13 01:28:33 +0200

    enable delete service check on publisher application entity

--- src/Ojs/AdminBundle/Controller/AdminPublisherApplicationController.php
+++ src/Ojs/AdminBundle/Controller/AdminPublisherApplicationController.php
@@ -139,7 +139,7 @@ class AdminPublisherApplicationController extends Controller
         if ($token != $request->get('_token')) {
             throw new TokenNotFoundException("Token Not Found!");
         }
-
+        $this->get('ojs_core.delete.service')->check($entity);
         $em->remove($entity);
         $em->flush();
 
