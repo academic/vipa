6192e689c4cc074dc6cc796d861e181a5b8e3e0a
Behram ÇELEN <behramcelen@gmail.com>
2016-02-20 14:12:37 +0200

    create mail event chain service, and add to compiler pass

---
+++ src/Ojs/CoreBundle/Events/Chain/MailEventChain.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Ojs\CoreBundle\Events\Chain;
+
+use Ojs\CoreBundle\Events\MailEventsInterface;
+
+class MailEventChain
+{
+    private $mailEvents;
+
+    public function __construct()
+    {
+        $this->mailEvents = array();
+    }
+
+    public function addMailEvent(MailEventsInterface $mailEvent)
+    {
+        $this->mailEvents[] = $mailEvent;
+    }
+}
