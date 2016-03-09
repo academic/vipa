73a11bc349bb4af410cea908c330ecc89f997468
Utku Aydın <utkuaydin34@gmail.com>
2016-03-09 20:55:18 +0200

    Seperate actions for DOI and ID shortlinks

--- src/Ojs/SiteBundle/Controller/ShortLinkController.php
+++ src/Ojs/SiteBundle/Controller/ShortLinkController.php
@@ -3,15 +3,29 @@
 namespace Ojs\SiteBundle\Controller;
 
 use Ojs\CoreBundle\Controller\OjsController as Controller;
+use Ojs\JournalBundle\Entity\Article;
 
 class ShortLinkController extends Controller
 {
-    public function articleAction($identifier)
+    public function articleIdAction(Article $article)
+    {
+        return $this->redirectToRoute(
+            'ojs_article_page',
+            [
+                'publisher' => $article->getJournal()->getPublisher()->getSlug(),
+                'slug' => $article->getJournal()->getSlug(),
+                'article_id' => $article->getId(),
+                'issue_id'   => $article->getIssue()->getId()
+            ]
+        );
+    }
+
+    public function articleDoiAction($doi)
     {
         $repo = $this->getDoctrine()->getRepository('OjsJournalBundle:Article');
-        $article = is_numeric($identifier) ?
-            $repo->find(intval($identifier)) :
-            $repo->findOneBy(['doi' => $identifier]);
+        $article = $repo->findOneBy(['doi' => $doi]);
+
+        $this->throw404IfNotFound($article);
         $this->throw404IfNotFound($article->getIssue());
 
         return $this->redirectToRoute(
