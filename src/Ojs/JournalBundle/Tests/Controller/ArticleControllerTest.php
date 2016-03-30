da400dd2c348cc452c22a356477da984ba28a051
Behram ÇELEN <behramcelen@gmail.com>
2016-03-31 02:52:34 +0300

    this actions deprecated very long time ago

--- src/Ojs/JournalBundle/Tests/Controller/ArticleControllerTest.php
+++ src/Ojs/JournalBundle/Tests/Controller/ArticleControllerTest.php
@@ -5,68 +5,8 @@ namespace Ojs\JournalBundle\Tests\Controller;
 use Ojs\CoreBundle\Tests\BaseTestCase;
 
 /**
- * @todo new article, update article, delete article  and show article
+ *
  */
 class ArticleControllerTest extends BaseTestCase
 {
-
-    public function testIndex()
-    {
-        $this->logIn();
-        $this->assertTrue($this->isAccessible(['article']));
-    }
-
-    public function testArticleJournal()
-    {
-        $this->logIn();
-        $this->assertTrue($this->isAccessible(['article_journal', ['journalId' => 1]]));
-    }
-
-    public function testArticleIssue()
-    {
-        $this->logIn();
-        $this->assertTrue($this->isAccessible(['article_issue', ['issueId' => 1]]));
-    }
-
-    public function testArticleShow()
-    {
-        $this->logIn();
-        $this->assertTrue($this->isAccessible(['article_show', ['id' => 1]]));
-    }
-
-    public function testArticleNew()
-    {
-        $this->logIn();
-        $this->assertTrue($this->isAccessible(['article_new']));
-    }
-
-    public function testCitation()
-    {
-        $this->logIn();
-        $this->assertTrue($this->isAccessible(['citation', ['id' => 1]]));
-    }
-
-    public function testArticleEdit()
-    {
-        $this->logIn();
-        $this->assertTrue($this->isAccessible(['article_edit', ['id' => '1']]));
-    }
-
-    public function testArticleDelete()
-    {
-        $this->logIn();
-        $this->assertTrue($this->isAccessible(['article_delete', ['id' => 2]], [], 'DELETE', true));
-    }
-
-    public function testStatus()
-    {
-        $client = $this->client;
-        $this->logIn('admin', array('ROLE_SUPER_ADMIN'));
-
-        $client->request('GET', '/admin/article/');
-        $this->assertTrue($client->getResponse()->isSuccessful());
-
-        $client->request('GET', '/admin/article/new');
-        $this->assertTrue($client->getResponse()->isSuccessful());
-    }
 }
