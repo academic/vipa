84da5fd33b752acf4afc682b16febb8370be725c
Utku Aydın <utkuaydin34@gmail.com>
2016-05-14 13:52:32 +0300

    Enable Symfony's debug bundle for dev and test environments

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -69,6 +69,7 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new h4cc\AliceFixturesBundle\h4ccAliceFixturesBundle();
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
         }
 
         $accessor = PropertyAccess::createPropertyAccessor();
