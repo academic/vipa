db4a69a2c621530ab9a2f6d17e7494eec3273b62
Utku Aydın <utkuaydin34@gmail.com>
2016-05-15 14:51:03 +0300

    Merge "is published" and "is in press" fields into one visibility field

---
+++ src/Ojs/CoreBundle/Params/IssueVisibilityStatuses.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ojs\CoreBundle\Params;
+
+class IssueVisibilityStatuses
+{
+    const NOT_PUBLISHED = 0;
+    const PUBLISHED = 1;
+    const IN_PRESS = 2;
+}
--- src/Ojs/JournalBundle/Form/Type/IssueType.php
+++ src/Ojs/JournalBundle/Form/Type/IssueType.php
@@ -3,8 +3,12 @@
 namespace Ojs\JournalBundle\Form\Type;
 
 use Ojs\CoreBundle\Params\IssueDisplayModes;
+use Ojs\CoreBundle\Params\IssueVisibilityStatuses;
+use Ojs\JournalBundle\Entity\Issue;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class IssueType extends AbstractType
@@ -17,88 +21,168 @@ class IssueType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('translations', 'a2lix_translations', [
-                'fields' => [
-                    'title' => [],
-                    'description' => [
-                        'required' => false,
-                    ]
+            ->add(
+                'translations',
+                'a2lix_translations',
+                [
+                    'fields' => [
+                        'title'       => [],
+                        'description' => [
+                            'required' => false,
+                        ],
+                    ],
                 ]
-            ])
-            ->add('volume', 'integer', array('label' => 'volume', 'required' => false))
-            ->add('number', 'integer', array('label' => 'number', 'required' => false))
-            ->add('display_mode', 'choice', [
-                'choices' => [
-                    'display_mode.all' => IssueDisplayModes::SHOW_ALL,
-                    'display_mode.title' => IssueDisplayModes::SHOW_TITLE,
-                    'display_mode.volume_and_number' => IssueDisplayModes::SHOW_VOLUME_AND_NUMBER,
-                ],
-                'choices_as_values' => true,
-                'label' => 'display_mode',
-                'required' => false
-            ])
-            ->add('special', 'checkbox', [
-                    'label' => 'special.issue',
+            )
+            ->add('volume', 'integer', ['label' => 'volume', 'required' => false])
+            ->add('number', 'integer', ['label' => 'number', 'required' => false])
+            ->add(
+                'display_mode',
+                'choice',
+                [
+                    'choices'           => [
+                        'display_mode.all'               => IssueDisplayModes::SHOW_ALL,
+                        'display_mode.title'             => IssueDisplayModes::SHOW_TITLE,
+                        'display_mode.volume_and_number' => IssueDisplayModes::SHOW_VOLUME_AND_NUMBER,
+                    ],
+                    'choices_as_values' => true,
+                    'label'             => 'display_mode',
+                    'required'          => false,
+                ]
+            )
+            ->add(
+                'visibility',
+                'choice',
+                [
+                    'choices'           => [
+                        'issue.visibility.not_published' => IssueVisibilityStatuses::NOT_PUBLISHED,
+                        'issue.visibility.published'     => IssueVisibilityStatuses::PUBLISHED,
+                        'issue.visibility.in_press'      => IssueVisibilityStatuses::IN_PRESS,
+                    ],
+                    'choices_as_values' => true,
+                    'label'             => 'published',
+                    'required'          => true,
+                    'mapped'            => false,
+                ]
+            )
+            ->add(
+                'special',
+                'checkbox',
+                [
+                    'label'    => 'special.issue',
                     'required' => false,
                 ]
             )
-            ->add('supplement', 'checkbox', array(
-                    'label' => 'issue.supplement',
-                'required' => false,
-                )
+            ->add(
+                'supplement',
+                'checkbox',
+                [
+                    'label'    => 'issue.supplement',
+                    'required' => false,
+                ]
             )
-            ->add('inPress', 'checkbox', [
-                'label'    => 'issue.in_press_desc',
-                'required' => false,
-            ])
-            ->add('year', 'text', array('label' => 'year'))
+            ->add('year', 'text', ['label' => 'year'])
             ->add(
                 'datePublished',
                 'collot_datetime',
-                array(
-                    'label' => 'issue.publishdate',
-                    'date_format' => 'dd-MM-yyyy',
+                [
+                    'label'         => 'issue.publishdate',
+                    'date_format'   => 'dd-MM-yyyy',
                     'pickerOptions' => [
-                        'format' => 'dd-mm-yyyy',
-                        'startView' => 'month',
-                        'minView' => 'month',
-                        'todayBtn' => 'true',
+                        'format'         => 'dd-mm-yyyy',
+                        'startView'      => 'month',
+                        'minView'        => 'month',
+                        'todayBtn'       => 'true',
                         'todayHighlight' => 'true',
-                        'autoclose' => 'true',
+                        'autoclose'      => 'true',
                     ],
-                )
+                ]
             )
             ->add('tags', 'tags', ['label' => 'tags'])
-            ->add('published', 'checkbox', [
-                'label' => 'published.and.public',
-                'required' => false
-            ])
-            ->add('full_file', 'jb_file_ajax', array(
-                'endpoint' => 'issuefiles',
-                'label' => 'issue.full_file',
-                'required' => false
-            ))
-            ->add('cover', 'jb_crop_image_ajax', array(
-                'endpoint' => 'journal',
-                'img_width' => 200,
-                'img_height' => 300,
-                'crop_options' => array(
-                    'aspect-ratio' => 200 / 300,
-                    'maxSize' => "[200, 300]"
-                ),
-                'label' => 'issue.cover', 'required' => false
-            ))
-            ->add('header', 'jb_crop_image_ajax', array(
-                'endpoint' => 'journal',
-                'img_width' => 960,
-                'img_height' => 200,
-                'crop_options' => array(
-                    'aspect-ratio' => 960 / 200,
-                    'maxSize' => "[960, 200]"
-                ),
-                'label' => 'issue.header', 'required' => false
-            ))
-        ;
+            ->add(
+                'full_file',
+                'jb_file_ajax',
+                [
+                    'endpoint' => 'issuefiles',
+                    'label'    => 'issue.full_file',
+                    'required' => false,
+                ]
+            )
+            ->add(
+                'cover',
+                'jb_crop_image_ajax',
+                [
+                    'endpoint'     => 'journal',
+                    'img_width'    => 200,
+                    'img_height'   => 300,
+                    'crop_options' => [
+                        'aspect-ratio' => 200 / 300,
+                        'maxSize'      => "[200, 300]",
+                    ],
+                    'label'        => 'issue.cover',
+                    'required'     => false,
+                ]
+            )
+            ->add(
+                'header',
+                'jb_crop_image_ajax',
+                [
+                    'endpoint'     => 'journal',
+                    'img_width'    => 960,
+                    'img_height'   => 200,
+                    'crop_options' => [
+                        'aspect-ratio' => 960 / 200,
+                        'maxSize'      => "[960, 200]",
+                    ],
+                    'label'        => 'issue.header',
+                    'required'     => false,
+                ]
+            );
+
+        $builder->addEventListener(FormEvents::POST_SET_DATA, self::postSetDataCallback());
+        $builder->addEventListener(FormEvents::POST_SUBMIT, self::postSubmitCallback());
+    }
+
+    /**
+     * @return \Closure
+     */
+    public static function postSetDataCallback()
+    {
+        return function (FormEvent $event) {
+            /** @var Issue $issue */
+            $issue = $event->getData();
+            $form = $event->getForm();
+
+            $visibility = IssueVisibilityStatuses::NOT_PUBLISHED;
+
+            if ($issue->getInPress() && $issue->isPublished()) {
+                $visibility = IssueVisibilityStatuses::IN_PRESS;
+            } else if ($issue->isPublished()) {
+                $visibility = IssueVisibilityStatuses::PUBLISHED;
+            }
+
+            $form->get('visibility')->setData($visibility);
+        };
+    }
+
+    /**
+     * @return \Closure
+     */
+    public static function postSubmitCallback()
+    {
+        return function (FormEvent $event) {
+            $form = $event->getForm();
+            $issue = $event->getData();
+
+            $visibility = $form->get('visibility')->getData();
+
+            $issue->setInPress($visibility == IssueVisibilityStatuses::IN_PRESS);
+            $issue->setPublished(
+                $visibility == IssueVisibilityStatuses::IN_PRESS ||
+                $visibility == IssueVisibilityStatuses::PUBLISHED
+            );
+
+            $event->setData($issue);
+        };
     }
 
     /**
@@ -107,14 +191,14 @@ class IssueType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(
-            array(
-                'data_class' => 'Ojs\JournalBundle\Entity\Issue',
+            [
+                'data_class'         => 'Ojs\JournalBundle\Entity\Issue',
                 'cascade_validation' => true,
-                'novalidate' => 'novalidate',
-                'attr' => [
+                'novalidate'         => 'novalidate',
+                'attr'               => [
                     'class' => 'form-validate',
                 ],
-            )
+            ]
         );
     }
 
