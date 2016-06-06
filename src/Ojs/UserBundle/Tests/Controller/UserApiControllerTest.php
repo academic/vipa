53a5a59c6a910512035197e66aef8dc14b9061d9
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-07 02:08:00 +0300

    UserApiController Tests added.

--- src/Ojs/UserBundle/Tests/Controller/UserApiControllerTest.php
+++ src/Ojs/UserBundle/Tests/Controller/UserApiControllerTest.php
@@ -4,12 +4,25 @@ namespace Ojs\UserBundle\Tests\Controller;
 
 use Ojs\CoreBundle\Tests\BaseTestSetup as BaseTestCase;
 
-class UserControllerTest extends BaseTestCase
+class UserApiControllerTest extends BaseTestCase
 {
     public function testApiKey()
     {
         $client = $this->client;
-        $crawler = $client->request('GET', '/user/api/key');
+        $crawler = $client->request('GET', '/user/apikey', array(), array(), array(
+            'PHP_AUTH_USER' => 'admin',
+            'PHP_AUTH_PW' => 'admin',
+        ));
         $this->assertStatusCode(200,$client);
+    }
+
+    public function testregenerateAPI()
+    {
+        $client = $this->client;
+        $crawler = $client->request('GET', '/user/apikey/regenerate', array(), array(), array(
+            'PHP_AUTH_USER' => 'admin',
+            'PHP_AUTH_PW' => 'admin',
+        ));
+        $this->assertStatusCode(302, $client);
     }
 }
