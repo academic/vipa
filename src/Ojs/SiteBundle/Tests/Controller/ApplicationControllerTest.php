d3a1660fca44ebf8932a39112990b8761f19e62d
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-06 20:17:26 +0300

    Application Controller tests added.

--- src/Ojs/SiteBundle/Tests/Controller/ApplicationControllerTest.php
+++ src/Ojs/SiteBundle/Tests/Controller/ApplicationControllerTest.php
@@ -2,50 +2,27 @@
 
 namespace Ojs\SiteBundle\Tests\Controller;
 
-use Ojs\CoreBundle\Tests\BaseTestCase;
+use Ojs\CoreBundle\Tests\BaseTestSetup as BaseTestCase;
 
 class ApplicationControllerTest extends BaseTestCase
 {
-    const APPLY_JOURNAL = 'ojs_apply_journal';
-    const APPLY_PUBLISHER = 'ojs_apply_publisher';
-    const APPLY_INSTITUTE_SUCCESS = 'ojs_apply_institute_success';
-    const APPLY_JOURNAL_SUCCESS = 'ojs_apply_journal_success';
-
-    public function testJournal()
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful($url)
     {
-        $url                = $this->router->generate(self::APPLY_JOURNAL);
-        $crawler            = $this->client->request('GET', $url);
-        $response           = $this->client->getResponse();
-
-        $this->assertEquals(200, $response->getStatusCode());
+        $client = $this->client;
+        $crawler = $client->request('GET', $url);
+        $this->assertStatusCode(200, $client);
     }
 
-    public function testPublisher()
+    public function urlProvider()
     {
-        $url                = $this->router->generate(self::APPLY_PUBLISHER);
-        $crawler            = $this->client->request('GET', $url);
-        $response           = $this->client->getResponse();
-
-        $this->assertEquals(200, $response->getStatusCode());
-    }
-
-    public function testInstituteSuccess()
-    {
-        $url                = $this->router->generate(self::APPLY_INSTITUTE_SUCCESS);
-        $crawler            = $this->client->request('GET', $url);
-        $response           = $this->client->getResponse();
-
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertContains('Success', $response->getContent());
-    }
-
-    public function testJournalSuccess()
-    {
-        $url                = $this->router->generate(self::APPLY_JOURNAL_SUCCESS);
-        $crawler            = $this->client->request('GET', $url);
-        $response           = $this->client->getResponse();
-
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertContains('Success', $response->getContent());
+        return array(
+            array('/apply/journal'),
+            array('/apply/publisher'),
+            array('/apply/journal/success'),
+            array('/apply/publisher/success'),
+        );
     }
 }
