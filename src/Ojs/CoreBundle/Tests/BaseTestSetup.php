caf63668e5dc12dd5ae624dc1cd9cd28c0ff3f52
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-06 20:09:11 +0300

    New base  testcase added

---
+++ src/Ojs/CoreBundle/Tests/BaseTestSetup.php
@@ -0,0 +1,33 @@
+<?php
+namespace Ojs\CoreBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Client;
+use Doctrine\ORM\EntityManager;
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+
+abstract class BaseTestSetup extends WebTestCase
+{
+    /** @var  Client */
+    protected $client;
+
+    /** @var  EntityManager */
+    protected $em;
+    
+    protected function setUp()
+    {
+        static::$kernel = static::createKernel();
+        static::$kernel->boot();
+        $this->em = static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+
+        $baseUrl = static::$kernel->getContainer()->getParameter('base_host');
+        $this->client = static::makeClient(array(),array('HTTP_HOST' => $baseUrl));
+    }
+
+    protected function tearDown()
+    {
+        $this->em->close();
+        unset($this->client, $this->em);
+    }
+}
