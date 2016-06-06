86de10390d6d84ceb461da62d5303f56c8078a68
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-07 01:52:33 +0300

    MultipleMailController Tests added.

--- src/Ojs/UserBundle/Tests/Controller/MultipleMailControllerTest.php
+++ src/Ojs/UserBundle/Tests/Controller/MultipleMailControllerTest.php
@@ -2,8 +2,28 @@
 
 namespace Ojs\UserBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Ojs\CoreBundle\Tests\BaseTestSetup as BaseTestCase;
 
-class MultipleMailControllerTest extends WebTestCase
+class MultipleMailControllerTest extends BaseTestCase
 {
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful($url)
+    {
+        $client = $this->client;
+        $crawler = $client->request('GET', $url, array(), array(), array(
+            'PHP_AUTH_USER' => 'admin',
+            'PHP_AUTH_PW' => 'admin',
+        ));
+        $this->assertStatusCode(200, $client);
+    }
+
+    public function urlProvider()
+    {
+        return array(
+            array('/user/multiple_mails'),
+            array('/user/multiple_mails/add'),
+        );
+    }
 }
