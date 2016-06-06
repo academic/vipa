56e4a19d31eca1c80912c42812c29aff81901d76
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-06 20:23:52 +0300

    UserController Test added.

--- src/Ojs/SiteBundle/Tests/Controller/UserControllerTest.php
+++ src/Ojs/SiteBundle/Tests/Controller/UserControllerTest.php
@@ -2,100 +2,32 @@
 
 namespace Ojs\SiteBundle\Tests\Controller;
 
-use Ojs\CoreBundle\Tests\BaseTestCase;
+use Ojs\CoreBundle\Tests\BaseTestSetup as BaseTestCase;
 
 class UserControllerTest extends BaseTestCase
 {
-    const PROFILE = 'ojs_site_people_index';
-    const EDIT_PROFILE = 'ojs_user_edit_profile';
-    const CUSTOM_FIELD = 'ojs_user_custom_field';
-    const CUSTOM_FIELD_CREATE = 'ojs_user_custom_field_create';
-    const CUSTOM_FIELD_DELETE = 'ojs_user_custom_field_delete';
-    const CONNECTED_ACCOUNT = 'ojs_user_connected_account';
-    const CONNECTED_ACCOUNT_ADD = 'ojs_user_connected_account_add';
-
-    public function testProfile()
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful($url)
     {
-        $routeParameters    = ['slug' => 'admin'];
-        $url                = $this->router->generate(self::PROFILE,$routeParameters);
-        $crawler            = $this->client->request('GET', $url);
-        $response           = $this->client->getResponse();
-
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertContains('admin', $response->getContent());
+        $client = $this->client;
+        $crawler = $client->request('GET', $url, array(), array(), array(
+            'PHP_AUTH_USER' => 'admin',
+            'PHP_AUTH_PW' => 'admin',
+            ));
+        $this->assertStatusCode(200, $client);
     }
 
-    public function testEditProfile()
+    public function urlProvider()
     {
-        $this->login();
-        $url        = $this->router->generate(self::EDIT_PROFILE);
-        $crawler    = $this->client->request('GET', $url);
-        $response   = $this->client->getResponse();
-
-        $this->assertEquals(200, $response->getStatusCode());
-
-        $form = $crawler->filter('form[name=ojs_userbundle_updateuser]')->form();
-        $form['ojs_userbundle_updateuser[username]'] = 'admin2';
-        $form['ojs_userbundle_updateuser[firstName]'] = 'admin2';
-        $form['ojs_userbundle_updateuser[email]'] = 'root@localhost.com';
-        $crawler = $this->client->submit($form);
-
-        $usernameVal = $crawler->filter('#ojs_userbundle_updateuser_username')->attr('value');
-        $emailVal = $crawler->filter('#ojs_userbundle_updateuser_email')->attr('value');
-        $this->assertEquals('admin2',$usernameVal);
-        $this->assertEquals('root@localhost.com',$emailVal);
-
-    }
-
-    public function testCustomField()
-    {
-        $this->logIn();
-        $url                = $this->router->generate(self::CUSTOM_FIELD);
-        $crawler            = $this->client->request('GET', $url);
-        $response           = $this->client->getResponse();
-
-        $this->assertEquals(200, $response->getStatusCode());
-    }
-
-    public function testCreateCustomField()
-    {
-        $this->logIn();
-        $url                = $this->router->generate(self::CUSTOM_FIELD_CREATE);
-        $crawler            = $this->client->request('GET', $url);
-        $response           = $this->client->getResponse();
-
-        $this->assertEquals(200, $response->getStatusCode());
-
-        $form = $crawler->filter('form[name=ojs_userbundle_customfieldtype]')->form();
-        $form['ojs_userbundle_customfieldtype[label]'] = 'labeltry';
-        $form['ojs_userbundle_customfieldtype[value]'] = 'valuetry';
-        $crawler = $this->client->submit($form);
-        $this->assertTrue($this->client->getResponse()->isRedirect());
-        $this->client->followRedirect();
-        $this->assertContains(
-            'labeltry',
-            $this->client->getResponse()->getContent()
+        return array(
+            array('/@admin'),
+            array('/user/update'),
+            array('/user/custom_field'),
+            array('/user/custom_field/create'),
+            array('/user/connected_accounts'),
+            array('/user/connected_accounts/add'),
         );
     }
-
-    public function testDeleteCustomField()
-    {
-        $this->logIn();
-        $url                = $this->router->generate(self::CUSTOM_FIELD_DELETE,['id' => 3]);
-        $crawler            = $this->client->request('GET', $url);
-        $response           = $this->client->getResponse();
-
-        $this->assertEquals(302, $response->getStatusCode());
-    }
-
-    public function testConnectedAccount()
-    {
-        $this->logIn();
-        $url                = $this->router->generate(self::CONNECTED_ACCOUNT);
-        $crawler            = $this->client->request('GET',$url);
-        $response            = $this->client->getResponse();
-
-        $this->assertEquals(200,$response->getStatusCode());
-    }
-
 }
