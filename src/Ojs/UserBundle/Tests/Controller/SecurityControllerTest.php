f52f59613ee43e65ddafc732d2a27b5f7ce07bf4
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-07 01:54:04 +0300

    SecurityController Tests added.

--- src/Ojs/UserBundle/Tests/Controller/SecurityControllerTest.php
+++ src/Ojs/UserBundle/Tests/Controller/SecurityControllerTest.php
@@ -2,71 +2,21 @@
 
 namespace Ojs\UserBundle\Tests\Controller;
 
-use Ojs\CoreBundle\Tests\BaseTestCase;
-use Symfony\Bundle\SwiftmailerBundle\DataCollector\MessageDataCollector;
+use Ojs\CoreBundle\Tests\BaseTestSetup as BaseTestCase;
 
 class SecurityControllerTest extends BaseTestCase
 {
-    public function testRegister()
-    {
-        //$this->client->enableProfiler();
-
-        $this->isAccessible(['fos_user_registration_register']);
-        $form = $this->crawler->selectButton('Register')->form();
-        $form['ojs_user_register[username]'] = "test-user";
-        $form['ojs_user_register[email]'] = "test@user.com";
-        $form['ojs_user_register[firstName]'] = "Aybars";
-        $form['ojs_user_register[lastName]'] = "Cengaver";
-        $form['ojs_user_register[password][first]'] = "cengaver";
-        $form['ojs_user_register[password][second]'] = "cengaver";
-        $this->client->submit($form);
-        $result = $this->client->followRedirect();
-        $this->assertEquals(1, $result->filter('html:contains("Success")')->count());
-        /** @var MessageDataCollector $mailCollector */
-        //  $mailCollector = $this->client->getProfile()->getCollector('swiftmailer');
-        //  $this->assertEquals(1,$mailCollector->getMessageCount());
-    }
-
     public function testLogin()
     {
-        $this->isAccessible(['login']);
-        $form = $this->crawler->selectButton('Sign In')->form();
-        $form['_username'] = 'admin';
-        $form['_password'] = 'admin';
-        $result = $this->client->submit($form);
-        $this->assertEquals(1, $result->filter('html:contains("/user")')->count());
+        $client = $this->client;
+        $crawler = $client->request('GET', '/login');
+        $this->assertStatusCode(200, $client);
     }
 
-    public function testUnconfirmedLogin()
+    public function testPasswordCreate()
     {
-        $this->isAccessible(['login']);
-        $form = $this->crawler->selectButton('Sign In')->form();
-        $form['_username'] = 'test-user';
-        $form['_password'] = 'cengaver';
-        $this->client->submit($form);
-        $result = $this->client->followRedirect();
-        $this->assertEquals(1, $result->filter('html:contains("User account is locked.")')->count());
-    }
-
-    public function testRegenerateAPI()
-    {
-        $this->logIn();
-        $this->client->request('GET', '/user/apikey/regenerate', [], [], ['HTTP_ACCEPT' => 'application/json']);
-        $response = $this->client->getResponse();
-        $this->assertEquals(200, $response->getStatusCode());
-        $content = json_decode($response->getContent());
-        $this->assertEquals(true, $content->status);
-    }
-
-    public function testCreatePassword()
-    {
-        $this->logIn();
-        $this->isAccessible(['user_create_password']);
-        $form = $this->crawler->selectButton('Create')->form();
-        $form['ojs_userbundle_createpassword[password][first]'] = 'admin';
-        $form['ojs_userbundle_createpassword[password][second]'] = 'admin';
-        $this->client->submit($form);
-        $result = $this->client->followRedirect();
-        $this->assertEquals(1, $result->filter('html:contains("Terms of Service")')->count());
+        $client = $this->client;
+        $crawler = $client->request('GET', '/password/create');
+        $this->assertStatusCode(302, $client);
     }
 }
