c400a130f50404ab94698035022bb9205cc07e71
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-06 20:23:27 +0300

    PeopleController Test added.

--- src/Ojs/SiteBundle/Tests/Controller/PeopleControllerTest.php
+++ src/Ojs/SiteBundle/Tests/Controller/PeopleControllerTest.php
@@ -2,16 +2,15 @@
 
 namespace Ojs\SiteBundle\Tests\Controller;
 
-use Ojs\CoreBundle\Tests\BaseTestCase;
+use Ojs\CoreBundle\Tests\BaseTestSetup as BaseTestCase;
 
 class PeopleControllerTest extends BaseTestCase
 {
     public function testIndex()
     {
-        $url = $this->router->generate('ojs_site_people_index');
-        $this->client->request('GET', $url);
-        $response = $this->client->getResponse();
-        $this->assertEquals(200, $response->getStatusCode());
+        $client = $this->client;
+        $crawler = $client->request('GET', '/people');
+        $this->assertStatusCode(200, $client);
     }
-    
+
 }
