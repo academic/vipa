d2f24ae6ef914219d155bb9af48a0068be381688
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-06 20:26:51 +0300

    ShortLinkController Test added.

---
+++ src/Ojs/SiteBundle/Tests/Controller/ShortLinkControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Ojs\SiteBundle\Tests\Controller;
+
+use Ojs\CoreBundle\Tests\BaseTestSetup as BaseTestCase;
+
+class ShortLinkControllerTest extends BaseTestCase
+{
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful($url)
+    {
+        $client = $this->client;
+        $crawler = $client->request('GET', $url);
+        $this->assertStatusCode(302, $client);
+    }
+
+    public function urlProvider()
+    {
+        return array(
+            array('/a/1'),
+            array('/doi/10.5281/zenodo.14791'),
+        );
+    }
+}
