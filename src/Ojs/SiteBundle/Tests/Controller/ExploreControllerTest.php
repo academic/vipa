4dcc3d7ba3b274fde809af1c5ae160df8d076e2e
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-06 20:20:42 +0300

    Explore Controller Test added.

--- src/Ojs/SiteBundle/Tests/Controller/ExploreControllerTest.php
+++ src/Ojs/SiteBundle/Tests/Controller/ExploreControllerTest.php
@@ -2,28 +2,14 @@
 
 namespace Ojs\SiteBundle\Tests\Controller;
 
-use Ojs\CoreBundle\Tests\BaseTestCase;
+use Ojs\CoreBundle\Tests\BaseTestSetup as BaseTestCase;
 
 class ExploreControllerTest extends BaseTestCase
 {
-    const EXPLORE = 'ojs_site_explore_index';
-    const EXPLORE_PUBLISHER = 'ojs_site_explore_publisher';
-
     public function testIndex()
     {
-        $url                = $this->router->generate(self::EXPLORE);
-        $crawler            = $this->client->request('GET', $url);
-        $response           = $this->client->getResponse();
-
-        $this->assertEquals(200, $response->getStatusCode());
-    }
-
-    public function testPublisher()
-    {
-        $url                = $this->router->generate(self::EXPLORE_PUBLISHER);
-        $crawler            = $this->client->request('GET', $url);
-        $response           = $this->client->getResponse();
-
-        $this->assertEquals(200, $response->getStatusCode());
+        $client = $this->client;
+        $crawler = $client->request('GET', '/explore/journal');
+        $this->assertStatusCode(200, $client);
     }
 }
