6baf004b0155beb071e2fa671ff113fafcdc91ff
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-10 09:09:37 +0300

    SectionController Test Added

---
+++ src/Ojs/JournalBundle/Tests/Controller/SectionControllerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Ojs\JournalBundle\Tests\Controller;
+
+use Ojs\CoreBundle\Tests\BaseTestSetup as BaseTestCase;
+
+class SectionControllerTest extends BaseTestCase
+{
+    public function testIndex()
+    {
+        $this->logIn();
+        $client = $this->client;
+        $client->request('GET', '/journal/1/section/');
+
+        $this->assertStatusCode(200, $client);
+    }
+
+    public function testNew()
+    {
+        $this->logIn();
+        $client = $this->client;
+        $client->request('GET', '/journal/1/section/new');
+
+        $this->assertStatusCode(200, $client);
+    }
+
+    public function testShow()
+    {
+        $this->logIn();
+        $client = $this->client;
+        $client->request('GET', '/journal/1/section/1/show');
+
+        $this->assertStatusCode(200, $client);
+    }
+
+    public function testEdit()
+    {
+        $this->logIn();
+        $client = $this->client;
+        $client->request('GET', '/journal/1/section/1/edit');
+
+        $this->assertStatusCode(200, $client);
+    }
+}
