19c7db52844c3e65881803c014f53ed1db08a985
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-10 08:46:41 +0300

    ManagerController Test added

---
+++ src/Ojs/JournalBundle/Tests/Controller/ManagerControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Ojs\JournalBundle\Tests\Controller;
+
+use Ojs\CoreBundle\Tests\BaseTestSetup as BaseTestCase;
+
+class ManagerControllerTest extends BaseTestCase
+{
+    public function testJournalSettings()
+    {
+        $this->logIn();
+        $client = $this->client;
+        $client->request('GET', '/journal/1/settings/');
+
+        $this->assertStatusCode(200, $client);
+    }
+
+    public function testJournalSettingsSubmission()
+    {
+        $this->logIn();
+        $client = $this->client;
+        $client->request('GET', '/journal/1/settings/submission');
+
+        $this->assertStatusCode(200, $client);
+    }
+
+    public function testUserIndex()
+    {
+        $this->logIn();
+        $client = $this->client;
+        $client->request('GET', '/user');
+
+        $this->assertStatusCode(200, $client);
+    }
+}
