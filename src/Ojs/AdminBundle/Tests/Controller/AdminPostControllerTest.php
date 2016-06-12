d860abd4317772564cb63bc4e2c277c2f7bbb41a
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-12 10:02:43 +0300

    AdminPostController tests added

---
+++ src/Ojs/AdminBundle/Tests/Controller/AdminPostControllerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Ojs\AdminBundle\Tests\Controller;
+
+use Ojs\CoreBundle\Tests\BaseTestSetup as BaseTestCase;
+
+class AdminPostControllerTest extends BaseTestCase
+{
+    public function testIndex()
+    {
+        $this->logIn();
+        $client = $this->client;
+        $client->request('GET', '/admin/post/');
+
+        $this->assertStatusCode(200, $client);
+    }
+
+    public function testNew()
+    {
+        $this->logIn();
+        $client = $this->client;
+        $client->request('GET', '/admin/post/new');
+
+        $this->assertStatusCode(200, $client);
+    }
+
+    public function testShow()
+    {
+        $this->logIn();
+        $client = $this->client;
+        $client->request('GET', '/admin/post/1/show');
+
+        $this->assertStatusCode(200, $client);
+    }
+
+    public function testEdit()
+    {
+        $this->logIn();
+        $client = $this->client;
+        $client->request('GET', '/admin/post/1/edit');
+
+        $this->assertStatusCode(200, $client);
+    }
+}
