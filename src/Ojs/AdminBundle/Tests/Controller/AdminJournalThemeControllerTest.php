3864899cb083171e64860c5aafaf30713f91bc21
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-20 03:44:20 +0300

    adminjournalthemcontroller tests updated

--- src/Ojs/AdminBundle/Tests/Controller/AdminJournalThemeControllerTest.php
+++ src/Ojs/AdminBundle/Tests/Controller/AdminJournalThemeControllerTest.php
@@ -23,4 +23,22 @@ class AdminJournalThemeControllerTest extends BaseTestCase
 
         $this->assertStatusCode(200, $client);
     }
+
+    public function testShow()
+    {
+        $this->logIn();
+        $client = $this->client;
+        $client->request('GET', '/admin/journal-theme/1/show');
+
+        $this->assertStatusCode(200, $client);
+    }
+
+    public function testEdit()
+    {
+        $this->logIn();
+        $client = $this->client;
+        $client->request('GET', '/admin/journal-theme/1/edit');
+
+        $this->assertStatusCode(200, $client);
+    }
 }
