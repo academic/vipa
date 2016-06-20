16ca23d7e13c1bcef8304a859e412bdc240846ec
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-20 03:42:37 +0300

    adminindexcontroller tests updated

--- src/Ojs/AdminBundle/Tests/Controller/AdminIndexControllerTest.php
+++ src/Ojs/AdminBundle/Tests/Controller/AdminIndexControllerTest.php
@@ -23,4 +23,22 @@ class AdminIndexControllerTest extends BaseTestCase
 
         $this->assertStatusCode(200, $client);
     }
+
+    public function testShow()
+    {
+        $this->logIn();
+        $client = $this->client;
+        $client->request('GET', '/admin/index/1/show');
+
+        $this->assertStatusCode(200, $client);
+    }
+
+    public function testEdit()
+    {
+        $this->logIn();
+        $client = $this->client;
+        $client->request('GET', '/admin/index/1/edit');
+
+        $this->assertStatusCode(200, $client);
+    }
 }
