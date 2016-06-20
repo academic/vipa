ad9479d6cc0fef101db42aebbdbc2e7a9c5e1a0e
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-20 03:49:52 +0300

    journalthemecontroller tests added

--- src/Ojs/JournalBundle/Tests/Controller/JournalThemeControllerTest.php
+++ src/Ojs/JournalBundle/Tests/Controller/JournalThemeControllerTest.php
@@ -24,6 +24,24 @@ class JournalThemeControllerTest extends BaseTestCase
         $this->assertStatusCode(200, $client);
     }
 
+    public function testShow()
+    {
+        $this->logIn();
+        $client = $this->client;
+        $client->request('GET', '/journal/1/theme/1/show');
+
+        $this->assertStatusCode(200, $client);
+    }
+
+    public function testEdit()
+    {
+        $this->logIn();
+        $client = $this->client;
+        $client->request('GET', '/journal/1/theme/1/edit');
+
+        $this->assertStatusCode(200, $client);
+    }
+
     public function testGlobalThemes()
     {
         $this->logIn();
