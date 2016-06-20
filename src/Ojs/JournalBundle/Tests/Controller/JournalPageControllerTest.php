7ee03081dda76cf70bd60b80461825d7920320ce
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-20 03:49:09 +0300

    journalpagecontroller tests added

--- src/Ojs/JournalBundle/Tests/Controller/JournalPageControllerTest.php
+++ src/Ojs/JournalBundle/Tests/Controller/JournalPageControllerTest.php
@@ -23,4 +23,22 @@ class JournalPageControllerTest extends BaseTestCase
 
         $this->assertStatusCode(200, $client);
     }
+
+    public function testShow()
+    {
+        $this->logIn();
+        $client = $this->client;
+        $client->request('GET', '/journal/1/page/1/show');
+
+        $this->assertStatusCode(200, $client);
+    }
+
+    public function testEdit()
+    {
+        $this->logIn();
+        $client = $this->client;
+        $client->request('GET', '/journal/1/page/1/edit');
+
+        $this->assertStatusCode(200, $client);
+    }
 }
