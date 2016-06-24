28bee684494216dd779b6da522cc6aa400eb8551
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-24 03:24:39 +0300

    updated adminarticletypecontroller test

--- src/Ojs/AdminBundle/Tests/Controller/AdminArticleTypeControllerTest.php
+++ src/Ojs/AdminBundle/Tests/Controller/AdminArticleTypeControllerTest.php
@@ -19,9 +19,22 @@ class AdminArticleTypeControllerTest extends BaseTestCase
     {
         $this->logIn();
         $client = $this->client;
-        $client->request('GET', '/admin/article-type/new');
+        $crawler = $client->request('GET', '/admin/article-type/new');
 
         $this->assertStatusCode(200, $client);
+
+        $form = $crawler->filter('form[name=article_types]')->form();
+        $form['article_types[translations]['.$this->locale.'][name]'] = 'Article Name';
+        $form['article_types[translations]['.$this->locale.'][description]'] = 'Article Description';
+
+        $client->submit($form);
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $client->followRedirect();
+
+        $this->assertContains(
+            'Article Name',
+            $this->client->getResponse()->getContent()
+        );
     }
 
     public function testShow()
@@ -37,8 +50,29 @@ class AdminArticleTypeControllerTest extends BaseTestCase
     {
         $this->logIn();
         $client = $this->client;
-        $client->request('GET', '/admin/article-type/1/edit');
+        $crawler = $client->request('GET', '/admin/article-type/2/edit');
 
         $this->assertStatusCode(200, $client);
+        $form = $crawler->filter('form[name=article_types]')->form();
+        $form['article_types[translations]['.$this->locale.'][name]'] = 'Article Name Edit';
+        $form['article_types[translations]['.$this->locale.'][description]'] = 'Article Description';
+
+        $client->submit($form);
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $client->followRedirect();
+        $this->assertContains(
+            'Article Name Edit',
+            $this->client->getResponse()->getContent()
+        );
+    }
+
+    public function testDelete()
+    {
+        $this->logIn();
+        $client = $this->client;
+        $token = $client->getContainer()->get('security.csrf.token_manager')->getToken('ojs_admin_article_type2');
+        $client->request('DELETE', '/admin/article-type/2/delete', array('_token' => $token));
+
+        $this->assertStatusCode(302, $client);
     }
 }
