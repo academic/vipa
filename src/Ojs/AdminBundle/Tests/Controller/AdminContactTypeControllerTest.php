bbd6e0dd8f552758c9d8e8e4314d5040572a315e
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-24 13:26:12 +0300

    updated admincontacttypecontoler tests

--- src/Ojs/AdminBundle/Tests/Controller/AdminContactTypeControllerTest.php
+++ src/Ojs/AdminBundle/Tests/Controller/AdminContactTypeControllerTest.php
@@ -19,9 +19,23 @@ class AdminContactTypeControllerTest extends BaseTestCase
     {
         $this->logIn();
         $client = $this->client;
-        $client->request('GET', '/admin/contact-type/new');
+        $crawler = $client->request('GET', '/admin/contact-type/new');
 
         $this->assertStatusCode(200, $client);
+
+
+        $form = $crawler->filter('form[name=contact_types]')->form();
+        $form['contact_types[translations]['.$this->locale.'][name]'] = 'Contact Name';
+        $form['contact_types[translations]['.$this->locale.'][description]'] = 'Contact Description';
+
+        $client->submit($form);
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $client->followRedirect();
+
+        $this->assertContains(
+            'Contact Name',
+            $this->client->getResponse()->getContent()
+        );
     }
 
     public function testShow()
@@ -37,8 +51,31 @@ class AdminContactTypeControllerTest extends BaseTestCase
     {
         $this->logIn();
         $client = $this->client;
-        $client->request('GET', '/admin/contact-type/1/edit');
+        $crawler = $client->request('GET', '/admin/contact-type/2/edit');
 
         $this->assertStatusCode(200, $client);
+
+        $form = $crawler->filter('form[name=contact_types]')->form();
+        $form['contact_types[translations]['.$this->locale.'][name]'] = 'Contact Name Edit';
+        $form['contact_types[translations]['.$this->locale.'][description]'] = 'Contact Description';
+
+        $client->submit($form);
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $client->followRedirect();
+
+        $this->assertContains(
+            'Contact Name Edit',
+            $this->client->getResponse()->getContent()
+        );
+    }
+
+    public function testDelete()
+    {
+        $this->logIn();
+        $client = $this->client;
+        $token = $client->getContainer()->get('security.csrf.token_manager')->getToken('ojs_admin_contact_type2');
+        $client->request('DELETE', '/admin/contact-type/2/delete', array('_token' => $token));
+
+        $this->assertStatusCode(302, $client);
     }
 }
