3e99506bfe3041cff3b4a77e5d8108f9f9269974
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-28 05:22:51 +0300

    updated tests adminjournalapplicationfile

--- src/Ojs/AdminBundle/Tests/Controller/AdminJournalApplicationFileControllerTest.php
+++ src/Ojs/AdminBundle/Tests/Controller/AdminJournalApplicationFileControllerTest.php
@@ -3,6 +3,7 @@
 namespace Ojs\AdminBundle\Tests\Controller;
 
 use Ojs\CoreBundle\Tests\BaseTestSetup as BaseTestCase;
+use Ojs\JournalBundle\Entity\JournalApplicationFile;
 
 class AdminJournalApplicationFileControllerTest extends BaseTestCase
 {
@@ -19,9 +20,26 @@ class AdminJournalApplicationFileControllerTest extends BaseTestCase
     {
         $this->logIn();
         $client = $this->client;
-        $client->request('GET', '/admin/journal-application-file/new');
+        $crawler = $client->request('GET', '/admin/journal-application-file/new');
 
         $this->assertStatusCode(200, $client);
+
+        $form = $crawler->filter('form[name=journal_application_file]')->form();
+        $form['journal_application_file[title]'] = 'Journal File Title - phpunit';
+        $form['journal_application_file[file]'] = '81a2/8eca/ea43/5771db9974ddc.';
+        $form['journal_application_file[detail]'] = 'Detail - phpunit';
+        $form['journal_application_file[locale]'] = 'tr';
+        $form['journal_application_file[visible]'] = '1';
+        $client->submit($form);
+
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $client->followRedirect();
+
+        $this->assertContains(
+            'Journal File Title - phpunit',
+            $client->getResponse()->getContent()
+        );
+
     }
 
     public function testShow()
@@ -37,8 +55,26 @@ class AdminJournalApplicationFileControllerTest extends BaseTestCase
     {
         $this->logIn();
         $client = $this->client;
-        $client->request('GET', '/admin/journal-application-file/1/edit');
+        $crawler = $client->request('GET', '/admin/journal-application-file/1/edit');
 
         $this->assertStatusCode(200, $client);
+
+        $form = $crawler->filter('form[name=journal_application_file]')->form();
+        $form['journal_application_file[title]'] = 'Journal File Edit Title - phpunit';
+        $form['journal_application_file[detail]'] = 'Detail - phpunit';
+        $form['journal_application_file[locale]'] = 'tr';
+        $form['journal_application_file[visible]'] = '1';
+        $form['journal_application_file[required]'] = '1';
+        $client->submit($form);
+
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $client->followRedirect();
+
+        $this->assertContains(
+            'Journal File Edit Title - phpunit',
+            $client->getResponse()->getContent()
+        );
     }
+
+
 }
