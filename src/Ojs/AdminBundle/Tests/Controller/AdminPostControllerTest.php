cfd78dddb03e7dfcb037a32b41bc8da9ebecdd44
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-29 05:55:03 +0300

    updated test adminpost controller

--- src/Ojs/AdminBundle/Tests/Controller/AdminPostControllerTest.php
+++ src/Ojs/AdminBundle/Tests/Controller/AdminPostControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Ojs\AdminBundle\Tests\Controller;
 
+use Ojs\AdminBundle\Entity\AdminPost;
 use Ojs\CoreBundle\Tests\BaseTestSetup as BaseTestCase;
 
 class AdminPostControllerTest extends BaseTestCase
@@ -19,9 +20,22 @@ class AdminPostControllerTest extends BaseTestCase
     {
         $this->logIn();
         $client = $this->client;
-        $client->request('GET', '/admin/post/new');
+        $crawler = $client->request('GET', '/admin/post/new');
 
         $this->assertStatusCode(200, $client);
+
+        $form = $crawler->filter('form[name=admin_post]')->form();
+        $form['admin_post[translations]['.$this->locale.'][title]'] = 'Post Title - phpunit';
+        $form['admin_post[translations]['.$this->locale.'][content]'] = 'Post content - phpunit';
+        
+        $crawler = $client->submit($form);
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $client->followRedirect();
+        $this->assertContains(
+            'Post Title - phpunit',
+            $this->client->getResponse()->getContent()
+        );
+
     }
 
     public function testShow()
@@ -37,8 +51,44 @@ class AdminPostControllerTest extends BaseTestCase
     {
         $this->logIn();
         $client = $this->client;
-        $client->request('GET', '/admin/post/1/edit');
+        $crawler = $client->request('GET', '/admin/post/1/edit');
 
         $this->assertStatusCode(200, $client);
+
+
+        $form = $crawler->filter('form[name=admin_post]')->form();
+        $form['admin_post[translations]['.$this->locale.'][title]'] = 'Post Edit Title - phpunit';
+        $form['admin_post[translations]['.$this->locale.'][content]'] = 'Post content - phpunit';
+
+        $crawler = $client->submit($form);
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $client->followRedirect();
+        $this->assertContains(
+            'Post Edit Title - phpunit',
+            $this->client->getResponse()->getContent()
+        );
+    }
+
+    public function testDelete()
+    {
+        $em = $this->em;
+
+        $entity = new AdminPost();
+        $entity->setCurrentLocale($this->locale);
+        $entity->setTitle('Post Title Delete - phpunit');
+        $entity->setContent('content delete phpunit');
+
+        $em->persist($entity);
+        $em->flush();
+
+        $id = $entity->getId();
+
+        $this->logIn();
+        $client = $this->client;
+        $token = $this->generateToken('ojs_admin_post'.$id);
+        $client->request('DELETE', '/admin/post/'.$id.'/delete', array('_token' => $token));
+
+        $this->assertStatusCode(302, $client);
+
     }
 }
