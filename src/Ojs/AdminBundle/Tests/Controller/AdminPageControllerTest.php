e17992ca24ae3f8e9a6ca89ef73b2c9d676daefe
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-29 05:18:17 +0300

    updated tests adminpage

--- src/Ojs/AdminBundle/Tests/Controller/AdminPageControllerTest.php
+++ src/Ojs/AdminBundle/Tests/Controller/AdminPageControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Ojs\AdminBundle\Tests\Controller;
 
+use Ojs\AdminBundle\Entity\AdminPage;
 use Ojs\CoreBundle\Tests\BaseTestSetup as BaseTestCase;
 
 class AdminPageControllerTest extends BaseTestCase
@@ -19,9 +20,23 @@ class AdminPageControllerTest extends BaseTestCase
     {
         $this->logIn();
         $client = $this->client;
-        $client->request('GET', '/admin/page/new');
+        $crawler = $client->request('GET', '/admin/page/new');
 
         $this->assertStatusCode(200, $client);
+
+        $form = $crawler->filter('form[name=admin_page]')->form();
+        $form['admin_page[translations]['.$this->locale.'][title]'] = 'Page Title - phpunit';
+        $form['admin_page[translations]['.$this->locale.'][body]'] = 'body - phpunit';
+        $form['admin_page[visible]'] = '1';
+        $client->submit($form);
+
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $client->followRedirect();
+
+        $this->assertContains(
+            'Page Title - phpunit',
+            $client->getResponse()->getContent()
+        );
     }
 
     public function testShow()
@@ -37,8 +52,47 @@ class AdminPageControllerTest extends BaseTestCase
     {
         $this->logIn();
         $client = $this->client;
-        $client->request('GET', '/admin/page/1/edit');
+        $crawler = $client->request('GET', '/admin/page/1/edit');
 
         $this->assertStatusCode(200, $client);
+
+        $form = $crawler->filter('form[name=admin_page]')->form();
+        $form['admin_page[translations]['.$this->locale.'][title]'] = 'Page Edit Title - phpunit';
+        $form['admin_page[translations]['.$this->locale.'][body]'] = 'body - phpunit';
+        $form['admin_page[visible]'] = '1';
+        $client->submit($form);
+
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $client->followRedirect();
+
+        $this->assertContains(
+            'Page Edit Title - phpunit',
+            $client->getResponse()->getContent()
+        );
+
+
+    }
+
+    public function  testDelete()
+    {
+        $em = $this->em;
+
+        $entity = new AdminPage();
+        $entity->setCurrentLocale($this->locale);
+        $entity->setTitle('Title Delete - phpunit');
+        $entity->setBody('Body');
+        $entity->setVisible(true);
+
+        $em->persist($entity);
+        $em->flush();
+
+        $id = $entity->getId();
+
+        $this->logIn();
+        $client = $this->client;
+        $token = $this->generateToken('ojs_admin_page'.$id);
+        $client->request('DELETE', '/admin/page/'.$id.'/delete', array('_token' => $token));
+
+        $this->assertStatusCode(302, $client);
     }
 }
