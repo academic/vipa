62f2ce8b2984b0a94e36fe37bd22a0b62381f935
Ahmet YAZBAHAR <ahmetyazbahar1@gmail.com>
2016-06-29 08:31:59 +0300

    updated tests adminpublisherdesign controller

--- src/Ojs/AdminBundle/Tests/Controller/AdminPublisherDesignControllerTest.php
+++ src/Ojs/AdminBundle/Tests/Controller/AdminPublisherDesignControllerTest.php
@@ -3,6 +3,7 @@
 namespace Ojs\AdminBundle\Tests\Controller;
 
 use Ojs\CoreBundle\Tests\BaseTestSetup as BaseTestCase;
+use Ojs\JournalBundle\Entity\PublisherDesign;
 
 class AdminPublisherDesignControllerTest extends BaseTestCase
 {
@@ -37,8 +38,49 @@ class AdminPublisherDesignControllerTest extends BaseTestCase
     {
         $this->logIn();
         $client = $this->client;
-        $client->request('GET', '/admin/publisher-design/1/edit');
+        $crawler = $client->request('GET', '/admin/publisher-design/1/edit');
 
         $this->assertStatusCode(200, $client);
+
+        $form = $crawler->filter('form[name=ojs_adminbundle_publisher_design]')->form();
+
+        $form['ojs_adminbundle_publisher_design[publisher]'] = '1';
+        $form['ojs_adminbundle_publisher_design[title]'] = 'Publisher Design - phpunit';
+        $form['ojs_adminbundle_publisher_design[editableContent]'] = 'html{}';
+        $form['ojs_adminbundle_publisher_design[public]'] = '1';
+
+        $crawler = $client->submit($form);
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $client->followRedirect();
+        $this->assertContains(
+            'Publisher Design - phpunit',
+            $this->client->getResponse()->getContent()
+        );
+    }
+    
+    public function testDelete()
+    {
+        $em = $this->em;
+
+        $entity = new PublisherDesign();
+        $entity->setTitle('Publisher Design Delete Title');
+        $entity->setContent('content');
+        $entity->setEditableContent('editable');
+        $entity->setPublic(false);
+
+        $publisher = $em->getRepository('OjsJournalBundle:Publisher')->find(1);
+        $entity->setPublisher($publisher);
+
+        $em->persist($entity);
+        $em->flush();
+
+        $id = $entity->getId();
+
+        $this->logIn();
+        $client = $this->client;
+        $token = $this->generateToken('ojs_admin_publisher_design'.$id);
+        $client->request('DELETE', '/admin/publisher-design/'.$id.'/delete', array('_token' => $token));
+
+        $this->assertStatusCode(302, $client);
     }
 }
