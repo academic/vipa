83a05e42552059b97fb06659e7a7417de3a2d505
Ahmet Yazbahar <ahmetyazbahar1@gmail.com>
2016-07-10 21:41:38 +0300

    updated test admin user controller

--- src/Ojs/AdminBundle/Tests/Controller/AdminUserControllerTest.php
+++ src/Ojs/AdminBundle/Tests/Controller/AdminUserControllerTest.php
@@ -19,9 +19,31 @@ class AdminUserControllerTest extends BaseTestCase
     {
         $this->logIn();
         $client = $this->client;
-        $client->request('GET', '/admin/user/new');
+        $crawler = $client->request('GET', '/admin/user/new');
 
         $this->assertStatusCode(200, $client);
+
+        $form = $crawler->filter('form[name=user]')->form();
+        $form['user[username]'] = 'UserPhpunit';
+        $form['user[password]'] = 'phpunitPassword';
+        $form['user[email]'] = 'email@ojs.io';
+        $form['user[about]'] = 'User About - phpunit';
+        $form['user[title]'] = '1';
+        $form['user[firstName]'] = 'First Name - phpunit';
+        $form['user[lastName]'] = 'Last Name - phpunit';
+        $form['user[enabled]'] = '1';
+        $form['user[subjects]'] = ['2'];
+        $form['user[avatar]'] = '';
+        $form['user[country]'] = '225';
+
+        $crawler = $client->submit($form);
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $client->followRedirect();
+        $this->assertContains(
+            'First Name - phpunit',
+            $this->client->getResponse()->getContent()
+        );
+
     }
 
     public function testEdit()
