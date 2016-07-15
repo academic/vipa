8a30553fb0375837301e1ac82df7ee3ac0a73f26
Behram ÇELEN <behramcelen@gmail.com>
2016-07-15 10:51:04 +0300

    separately phpunit file test logic support

--- etc/bin/parallel_phpunit.sh
+++ etc/bin/parallel_phpunit.sh
@@ -1,1 +1,3 @@
+export IS_PHPUNIT_FASTEST=1;
 find src/* -name "*Test.php" | ./bin/fastest --before="./etc/bin/initORM.php test" --verbose --preserve-order --process=3 "bin/phpunit -c app {};"
+unset IS_PHPUNIT_FASTEST;
