e4d9ea1c3910ec4285952ce48cbdcf49e657c86d
Behram ÇELEN <behramcelen@gmail.com>
2016-07-20 04:18:00 +0300

    prevent duplicate article view increase on article, issue and journal view function, use cookie

--- web/assets/app/js/plugins/analytics/analytics.js
+++ web/assets/app/js/plugins/analytics/analytics.js
@@ -1,5 +1,32 @@
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
 var analytics = {
     view: function (entity, id, token) {
+        var viewCookieName = "view_"+entity+"_"+id;
+        if(getCookie(viewCookieName) == 1){
+            return false;
+        }
+        setCookie(viewCookieName, 1, 7);
         var path = 'api_1_' + entity + '_view';
         var url = Routing.generate(path, {
             id: id,
