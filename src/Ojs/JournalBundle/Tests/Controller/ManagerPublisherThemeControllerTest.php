e296c964916267d939a09bf3ce0f59a9f45c2732
Ahmet Yazbahar <ahmetyazbahar1@gmail.com>
2016-07-21 09:56:24 +0300

    updated  manager publisher theme controller tests

--- src/Ojs/JournalBundle/Tests/Controller/ManagerPublisherThemeControllerTest.php
+++ src/Ojs/JournalBundle/Tests/Controller/ManagerPublisherThemeControllerTest.php
@@ -19,9 +19,22 @@ class ManagerPublisherThemeControllerTest extends BaseTestCase
     {
         $this->logIn();
         $client = $this->client;
-        $client->request('GET', '/publisher/1/theme/new');
+        $crawler = $client->request('GET', '/publisher/1/theme/new');
 
         $this->assertStatusCode(200, $client);
+
+        $form = $crawler->filter('form[name=publisher_theme]')->form();
+        $form['publisher_theme[title]'] = 'Publisher Theme Title - phpunit';
+        $form['publisher_theme[public]'] = '1';
+        $form['publisher_theme[css]'] = 'css';
+
+        $client->submit($form);
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $client->followRedirect();
+        $this->assertContains(
+            'Publisher Theme Title - phpunit',
+            $this->client->getResponse()->getContent()
+        );
     }
 
     public function testShow()
@@ -37,8 +50,34 @@ class ManagerPublisherThemeControllerTest extends BaseTestCase
     {
         $this->logIn();
         $client = $this->client;
-        $client->request('GET', '/publisher/1/theme/1/edit');
+        $id = $this->sampleObjectLoader->loadPublisherTheme();
+        $crawler = $client->request('GET', '/publisher/1/theme/' . $id . '/edit');
 
         $this->assertStatusCode(200, $client);
+
+        $form = $crawler->filter('form[name=publisher_theme]')->form();
+        $form['publisher_theme[title]'] = 'Publisher Theme Edit Title - phpunit';
+        $form['publisher_theme[public]'] = '1';
+        $form['publisher_theme[css]'] = 'css';
+
+        $client->submit($form);
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $client->followRedirect();
+        $this->assertContains(
+            'Publisher Theme Edit Title - phpunit',
+            $this->client->getResponse()->getContent()
+        );
+    }
+
+    public function testDelete()
+    {
+        $id = $this->sampleObjectLoader->loadPublisherTheme();
+
+        $this->logIn();
+        $client = $this->client;
+        $token = $this->generateToken('ojs_publisher_manager_theme' . $id);
+        $client->request('DELETE', '/publisher/1/theme/' . $id . '/delete', array('_token' => $token));
+
+        $this->assertStatusCode(302, $client);
     }
 }
