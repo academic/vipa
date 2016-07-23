10714f22ead97d46199740dde21e13371e71362a
Ahmet Yazbahar <ahmetyazbahar1@gmail.com>
2016-07-23 11:42:10 +0300

    move actions related  to journal from site controller, related #1413

--- src/Ojs/SiteBundle/Tests/Controller/SiteControllerTest.php
+++ src/Ojs/SiteBundle/Tests/Controller/SiteControllerTest.php
@@ -20,39 +20,11 @@ class SiteControllerTest extends BaseTestCase
         $this->assertStatusCode(200,$client);
     }
 
-    public function testJournalIndex()
-    {
-        $client = static::makeClient(array(),array('HTTP_HOST' => 'www.ojs.dev'));
-        $client->request('GET','/intro');
-        $this->assertStatusCode(200, $client);
-    }
-
     public function testJournalArticles()
     {
         $client = static::makeClient(array(),array('HTTP_HOST' => 'www.ojs.dev'));
         $client->request('GET','/intro/articles');
         $this->assertStatusCode(200, $client);
-    }
-
-    public function testJournalBoard()
-    {
-        $client = static::makeClient(array(),array('HTTP_HOST' => 'www.ojs.dev'));
-        $client->request('GET','/intro/board');
-        $this->assertStatusCode(200,$client);
-    }
-
-    public function testLastArticles()
-    {
-        $client = static::makeClient(array(),array('HTTP_HOST' => 'www.ojs.dev'));
-        $client->request('GET','/intro/last');
-        $this->assertStatusCode(200,$client);
-    }
-
-    public function testArchiveIndex()
-    {
-        $client = static::makeClient(array(),array('HTTP_HOST' => 'www.ojs.dev'));
-        $client->request('GET','/intro/archive');
-        $this->assertStatusCode(200,$client);
     }
 
     public function testAnnouncementIndex()
@@ -62,26 +34,10 @@ class SiteControllerTest extends BaseTestCase
         $this->assertStatusCode(200,$client);
     }
 
-    /**
-    public function testSubscribe()
-    {
-        $client = static::makeClient(array(),array('HTTP_HOST' => 'www.ojs.dev'));
-        $client->request('GET','/intro/subscribe');
-        $this->assertStatusCode(200,$client);
-    }
-     */
-
     public function TestIssuePage()
     {
         $client = static::makeClient(array(),array('HTTP_HOST' => 'www.ojs.dev'));
         $client->request('GET','/intro/issue/1');
-        $this->assertStatusCode(200,$client);
-    }
-
-    public function TestJournalContacts()
-    {
-        $client = static::makeClient(array(),array('HTTP_HOST' => 'www.ojs.dev'));
-        $client->request('GET','/intro/contacts');
         $this->assertStatusCode(200,$client);
     }
 
