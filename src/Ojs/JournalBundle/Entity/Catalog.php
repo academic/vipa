fcffb9980d9e635e7a8880cca63b54667acfd12e
Behram ÇELEN <behramcelen@gmail.com>
2016-10-04 01:21:46 +0300

    create catalog entity, orm and repo files

---
+++ src/Ojs/JournalBundle/Entity/Catalog.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Ojs\JournalBundle\Entity;
+
+/**
+ * Class Catalog
+ * @package Ojs\JournalBundle\Entity
+ */
+class Catalog
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $domain;
+
+
+    /**
+     * Catalog constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDomain()
+    {
+        return $this->domain;
+    }
+
+    /**
+     * @param string $domain
+     *
+     * @return $this
+     */
+    public function setDomain($domain)
+    {
+        $this->domain = $domain;
+
+        return $this;
+    }
+}
---
+++ src/Ojs/JournalBundle/Entity/CatalogRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ojs\JournalBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class CatalogRepository
+ * @package Ojs\JournalBundle\Entity
+ */
+class CatalogRepository extends EntityRepository
+{
+}
