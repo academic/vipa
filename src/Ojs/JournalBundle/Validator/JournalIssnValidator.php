de952894e88694db73782699443c6672e055deb8
Ahmet Yazbahar <ahmetyazbahar1@gmail.com>
2016-10-29 02:20:21 +0300

    added journal issn validator

---
+++ src/Ojs/JournalBundle/Validator/JournalIssnValidator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Ojs\JournalBundle\Validator;
+
+use Doctrine\ORM\EntityManager;
+use Ojs\JournalBundle\Entity\Journal;
+use Ojs\JournalBundle\Validator\Constraints\JournalIssn;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+/**
+ * @Annotation
+ */
+class JournalIssnValidator extends ConstraintValidator
+{
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    /**
+     * @JournalIssnValidator() constructor.
+     * @param EntityManager $em
+     */
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * @param mixed $value
+     * @param Constraint|JournalIssn $constraint
+     * @return mixed
+     */
+    public function validate($value, Constraint $constraint)
+    {
+        if ($this->em->getRepository(Journal::class)->findBy(['issn' => $value, 'eissn' => $value])) {
+            $this->context->addViolation($constraint->message);
+        }
+    }
+}
