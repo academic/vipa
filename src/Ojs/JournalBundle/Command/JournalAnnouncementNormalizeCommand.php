47b103dee4322ff8541f60a0aad3ecb802e840fd
Ahmet Yazbahar <ahmetyazbahar1@gmail.com>
2016-11-26 10:06:49 +0300

    removed unused namespaces

--- src/Ojs/JournalBundle/Command/JournalAnnouncementNormalizeCommand.php
+++ src/Ojs/JournalBundle/Command/JournalAnnouncementNormalizeCommand.php
@@ -4,14 +4,11 @@ namespace Ojs\JournalBundle\Command;
 
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Query\ResultSetMapping;
-use Ojs\JournalBundle\Entity\Author;
 use Ojs\JournalBundle\Entity\JournalAnnouncement;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Ojs\JournalBundle\Entity\JournalAnnouncementTranslation;
 
