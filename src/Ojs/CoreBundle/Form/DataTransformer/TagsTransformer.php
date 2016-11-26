193ed932668d29a0bad8fb5db9f7598bf4c93830
Utku Aydın <utkuaydin34@gmail.com>
2016-11-26 18:14:52 +0300

    Change tag delimiter and clean up the code

--- src/Ojs/CoreBundle/Form/DataTransformer/TagsTransformer.php
+++ src/Ojs/CoreBundle/Form/DataTransformer/TagsTransformer.php
@@ -5,29 +5,20 @@ namespace Ojs\CoreBundle\Form\DataTransformer;
 use Symfony\Component\Form\DataTransformerInterface;
 
 /**
- *
  * Class TagsTransformer
  * @package Ojs\CoreBundle\Form\DataTransformer
  */
 class TagsTransformer implements DataTransformerInterface
 {
-    private $delimiter = ',';
+    private $delimiter = ';';
+
     public function transform($tagsData)
     {
-        if(empty($tagsData)) {
-            return null;
-        }
-        $parts = explode($this->delimiter, $tagsData);
-
-        return $parts;
+        return empty($tagsData) ? null : explode($this->delimiter, $tagsData);
     }
-
 
     public function reverseTransform($values = null)
     {
-        if(empty($values) || !is_array($values)){
-            return null;
-        }
-        return implode($this->delimiter, $values);
+        return empty($values) || !is_array($values) ? null : implode($this->delimiter, $values);
     }
 }
