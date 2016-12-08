151fa64aa4e4f768cdc3c2ce995071400f70747d
Ahmet Yazbahar <ahmetyazbahar1@gmail.com>
2016-12-08 20:32:16 +0300

    added  journal edit param to journal index mail template

--- src/Ojs/JournalBundle/Listeners/JournalIndexMailer.php
+++ src/Ojs/JournalBundle/Listeners/JournalIndexMailer.php
@@ -25,11 +25,11 @@ class JournalIndexMailer extends AbstractJournalItemMailer
      */
     public function onJournalIndexPostCreate(JournalItemEvent $itemEvent)
     {
-        $getMailEvent = $this->ojsMailer->getEventByName(JournalIndexEvents::POST_CREATE, null, $itemEvent->getItem()->getJournal());
+        $getMailEvent = $this->ojsMailer->getEventByName(JournalIndexEvents::POST_CREATE.'.to.users', null, $itemEvent->getItem()->getJournal());
         if(!$getMailEvent){
-            return;
+            goto sendmailtoadmins;
         }
-        $mailUsers = array_merge($this->ojsMailer->getJournalRelatedUsers(), $this->ojsMailer->getAdminUsers());
+        $mailUsers = $this->ojsMailer->getJournalRelatedUsers();
         /** @var User $user */
         foreach ($mailUsers as $user) {
             $transformParams = [
@@ -38,6 +38,33 @@ class JournalIndexMailer extends AbstractJournalItemMailer
                 'receiver.username' => $user->getUsername(),
                 'receiver.fullName' => $user->getFullName(),
                 'journal'           => (string)$itemEvent->getItem()->getJournal(),
+            ];
+            $template = $this->ojsMailer->transformTemplate($getMailEvent->getTemplate(), $transformParams);
+            $this->ojsMailer->sendToUser(
+                $user,
+                $getMailEvent->getSubject(),
+                $template
+            );
+        }
+
+
+
+        sendmailtoadmins:
+        
+        $getMailEvent = $this->ojsMailer->getEventByName(JournalIndexEvents::POST_CREATE.'.to.admins', null, $itemEvent->getItem()->getJournal());
+        if(!$getMailEvent){
+            return;
+        }
+        $mailUsers =  $this->ojsMailer->getAdminUsers();
+        /** @var User $user */
+        foreach ($mailUsers as $user) {
+            $transformParams = [
+                'index'             => (string)$itemEvent->getItem(),
+                'done.by'           => $this->ojsMailer->currentUser()->getUsername(),
+                'receiver.username' => $user->getUsername(),
+                'receiver.fullName' => $user->getFullName(),
+                'journal'           => (string)$itemEvent->getItem()->getJournal(),
+                'journal.edit'      => $this->router->generate('ojs_admin_journal_edit', ['id' => $itemEvent->getItem()->getJournal()->getId()])
             ];
             $template = $this->ojsMailer->transformTemplate($getMailEvent->getTemplate(), $transformParams);
             $this->ojsMailer->sendToUser(
