a02b17c4c938c5d4fd6c262b8565ec44520432f9
Behram ÇELEN <behramcelen@gmail.com>
2017-04-28 04:56:19 +0300

    ojs to vipa transform for lowercase for src/ dir

--- src/Vipa/AdminBundle/Resources/public/js/dashboard.js
+++ src/Vipa/AdminBundle/Resources/public/js/dashboard.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
     $('#quick_switch_switch').click(function(e) {
         e.preventDefault();
         var id = $('#quick_switch_journal').val();
-        window.location.href = Routing.generate('ojs_journal_dashboard_index', {'journalId': id});
+        window.location.href = Routing.generate('vipa_journal_dashboard_index', {'journalId': id});
     });
 });
